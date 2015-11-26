dcffff096f9369555c1e4ad2b1a0c9ab451173f6
nKast <nkastellanos@gmail.com>
2015-11-26 19:56:01 +0200

    [Windows Store 8.x] Locks the swapChain to the native orientation
    Tells to the system to not rotate the swapChain automatically.
    The program is responsible to rotate camera,UI, Input, etc.
    The behavior is somewhat similar to when using
    DrawingSurfaceBackgroundGrid  on WP8.
    Although, touch input behave differently between the two platforms,
    on WP8 you have to rotate touch using the same 2D rotation matrix you
    would use for drawing UI elements. On win8 touch input is allready
    rotated by the system.

--- MonoGame.Framework/GraphicsDeviceManager.cs
+++ MonoGame.Framework/GraphicsDeviceManager.cs
@@ -381,5 +381,16 @@ namespace Microsoft.Xna.Framework
             _strategy.ApplyChanges();
         }
 
+        public bool LockToNativeOrientation 
+        {
+#if (W81 || WP81)
+            get { return ((ConcreteGraphicsDeviceManager)_strategy)._lockToNativeOrientation; }
+            set { ((ConcreteGraphicsDeviceManager)_strategy)._lockToNativeOrientation = value; }
+#else
+            get { return false; }
+            set { throw new PlatformNotSupportedException(); }
+#endif
+        }
+
     }
 }
