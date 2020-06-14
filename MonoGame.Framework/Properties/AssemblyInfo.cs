fd772a43c431818d8af6ad9998ee8fd4ad5c3291
Nick Kastellanos <nkastellanos@gmail.com>
2020-06-14 11:10:07 +0300

    Add MG.9000 projects

--- MonoGame.Framework/Properties/AssemblyInfo.cs
+++ MonoGame.Framework/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 
 // Allow the content pipeline assembly to access
 // some of our internal helper methods that it needs.
-[assembly: InternalsVisibleTo("MonoGame.Framework.Content.Pipeline")]
+//[assembly: InternalsVisibleTo("MonoGame.Framework.Content.Pipeline")]
 [assembly: InternalsVisibleTo("MonoGame.Framework.Net")]
 
 //Tests projects need access too
