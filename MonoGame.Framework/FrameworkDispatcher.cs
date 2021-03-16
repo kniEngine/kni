ac526c0e53ccc56db66fb141619d45302607de6f
Nick Kastellanos <nkastellanos@gmail.com>
2021-03-16 19:53:54 +0200

    Implement FrameworkDispacher
    -Remove Initialize(). Dispatcher doesn't need to initialize itself.
    -Remove DoUpdate()
    -Add OnUpdate event to dispatch 'Update' message.
    -Convert to Singleton

--- MonoGame.Framework/FrameworkDispatcher.cs
+++ MonoGame.Framework/FrameworkDispatcher.cs
@@ -3,7 +3,17 @@
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
+using Microsoft.Xna.Platform;
 using Microsoft.Xna.Framework.Audio;
+
+namespace Microsoft.Xna.Platform
+{
+    public interface IFrameworkDispatcher
+    {
+        event Action OnUpdate;
+        void Update();
+    }
+}
 
 namespace Microsoft.Xna.Framework
 {
@@ -14,32 +24,59 @@ namespace Microsoft.Xna.Framework
     /// If you use <see cref="Game"/> class, <see cref="Update()"/> is called automatically.
     /// Otherwise you must call it as part of your game loop.
     /// </remarks>
-    public static class FrameworkDispatcher
+    public sealed class FrameworkDispatcher : IFrameworkDispatcher
     {
-        private static bool _initialized = false;
+        private static FrameworkDispatcher _current;
+
+        /// <summary>
+        /// Returns the current FrameworkDispatcher instance.
+        /// </summary> 
+        public static FrameworkDispatcher Current
+        {
+            get
+            {
+                lock (typeof(FrameworkDispatcher))
+                {
+                    if (_current == null)
+                        _current = new FrameworkDispatcher();
+                    return _current;
+                }
+            }
+        }
 
         /// <summary>
         /// Processes framework events.
         /// </summary>
         public static void Update()
         {
-            if (!_initialized)
-                Initialize();
-
-            DoUpdate();
+            ((IFrameworkDispatcher)FrameworkDispatcher.Current).Update();
         }
 
-        private static void DoUpdate()
+
+        private Action _onUpdate;
+
+        private FrameworkDispatcher()
         {
+
+        }
+
+        event Action IFrameworkDispatcher.OnUpdate
+        {
+            add { _onUpdate += value; }
+            remove { _onUpdate -= value; }
+        }
+
+        void IFrameworkDispatcher.Update()
+        {
+            var updateHandler = _onUpdate;
+            if (updateHandler != null)
+                updateHandler();
+
             DynamicSoundEffectInstanceManager.UpdatePlayingInstances();
             SoundEffectInstancePool.Update();
             Microphone.UpdateMicrophones();
         }
-
-        private static void Initialize()
-        {
-            _initialized = true;
-        }
     }
 }
+
 
