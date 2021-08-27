3013cd137d0e2a8ff647ccb9b88152bbb07be7ce
Nikos Kastellanos <nkastellanos@gmail.com>
2021-08-27 22:09:08 +0300

    VectorConverter

--- MonoGame.Framework.Content.Pipeline/Graphics/VertexChannelGeneric.cs
+++ MonoGame.Framework.Content.Pipeline/Graphics/VertexChannelGeneric.cs
@@ -2,12 +2,9 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
-using Microsoft.Xna.Framework.Design;
-using Microsoft.Xna.Framework.Graphics.PackedVector;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 
 namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
@@ -78,19 +75,6 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
             items = new List<T>();
         }
 
-        static VertexChannel()
-        {
-            // Some platforms (such as Windows Store) don't support TypeConverter, which
-            // is normally referenced with an attribute on the target type. To keep them
-            // out of the main assembly, they are registered here before their use.
-
-            //TypeDescriptor.AddAttributes(typeof(Single), new TypeConverterAttribute(typeof(SingleTypeConverter)));
-            TypeDescriptor.AddAttributes(typeof(Vector2), new TypeConverterAttribute(typeof(Vector2TypeConverter)));
-            TypeDescriptor.AddAttributes(typeof(Vector3), new TypeConverterAttribute(typeof(Vector3TypeConverter)));
-            TypeDescriptor.AddAttributes(typeof(Vector4), new TypeConverterAttribute(typeof(Vector4TypeConverter)));
-            //TypeDescriptor.AddAttributes(typeof(IPackedVector), new TypeConverterAttribute(typeof(PackedVectorTypeConverter)));
-        }
-
         /// <summary>
         /// Determines whether the specified element is in the channel.
         /// </summary>
@@ -159,7 +143,7 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
             return Convert<TargetType>(items);
         }
 
-        private static IEnumerable<TargetType> Convert<TargetType>(IEnumerable<T> items)
+        private static IEnumerable<TargetType> Convert<TargetType>(List<T> items)
         {
             // The following formats are supported:
             // - Single
@@ -168,18 +152,14 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
             // - Vector4 Structure
             // - Any implementation of IPackedVector Interface.
 
-            var converter = TypeDescriptor.GetConverter(typeof(T));
-            if (!converter.CanConvertTo(typeof(TargetType)))
-            {
-                // If you got this exception, check out the static constructor above
-                // to make sure your type is registered.
-                throw new NotImplementedException(
-                    string.Format("TypeConverter for {0} -> {1} is not implemented.",
-                    typeof(T).Name, typeof(TargetType).Name));
-            }
+            var vconverter4 = VectorConverter.GetConverter<T, Vector4>();
+            var converter = VectorConverter.GetConverter<Vector4, TargetType>();
 
-            foreach (var item in items)
-                yield return (TargetType)converter.ConvertTo(item, typeof(TargetType));
+            foreach (T item in items)
+            {
+                Vector4 vec4 = vconverter4(item);
+                yield return (TargetType)converter(vec4);
+            }
         }
 
         /// <summary>
