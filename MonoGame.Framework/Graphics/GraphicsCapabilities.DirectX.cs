nkast/MonoGame

Extend Graphics profiles

MonoGame.Framework/Platform/Graphics/GraphicsCapabilities.DirectX.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Xna.Framework.Graphics
     {
         private void PlatformInitialize(GraphicsDevice device)
         {
-            SupportsNonPowerOfTwo = device.GraphicsProfile == GraphicsProfile.HiDef;
+            SupportsNonPowerOfTwo = device.GraphicsProfile >= GraphicsProfile.HiDef;
             SupportsTextureFilterAnisotropic = true;
 
             SupportsDepth24 = true;
@@ -22,12 +22,12 @@ namespace Microsoft.Xna.Framework.Graphics
 
             SupportsSRgb = true;
 
-            SupportsTextureArrays = device.GraphicsProfile == GraphicsProfile.HiDef;
-            SupportsDepthClamp = device.GraphicsProfile == GraphicsProfile.HiDef;
-            SupportsVertexTextures = device.GraphicsProfile == GraphicsProfile.HiDef;
-            SupportsFloatTextures = device.GraphicsProfile == GraphicsProfile.HiDef;
-            SupportsHalfFloatTextures = device.GraphicsProfile == GraphicsProfile.HiDef;
-            SupportsNormalized = device.GraphicsProfile == GraphicsProfile.HiDef;
+            SupportsTextureArrays = device.GraphicsProfile >= GraphicsProfile.HiDef;
+            SupportsDepthClamp = device.GraphicsProfile >= GraphicsProfile.HiDef;
+            SupportsVertexTextures = device.GraphicsProfile >= GraphicsProfile.HiDef;
+            SupportsFloatTextures = device.GraphicsProfile >= GraphicsProfile.HiDef;
+            SupportsHalfFloatTextures = device.GraphicsProfile >= GraphicsProfile.HiDef;
+            SupportsNormalized = device.GraphicsProfile >= GraphicsProfile.HiDef;
 
             SupportsInstancing = true;
             //TNC: TODO: detect suport based on feture level

kniEngine/kni

MonoGame.Framework/Graphics/GraphicsCapabilities.DirectX.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Xna.Framework.Graphics
     {
         private void PlatformInitialize(GraphicsDevice device)
         {
-            SupportsNonPowerOfTwo = device.GraphicsProfile == GraphicsProfile.HiDef;
+            SupportsNonPowerOfTwo = device.GraphicsProfile >= GraphicsProfile.HiDef;
             SupportsTextureFilterAnisotropic = true;
 
             SupportsDepth24 = true;
@@ -22,12 +22,12 @@ namespace Microsoft.Xna.Framework.Graphics
 
             SupportsSRgb = true;
 
-            SupportsTextureArrays = device.GraphicsProfile == GraphicsProfile.HiDef;
-            SupportsDepthClamp = device.GraphicsProfile == GraphicsProfile.HiDef;
-            SupportsVertexTextures = device.GraphicsProfile == GraphicsProfile.HiDef;
-            SupportsFloatTextures = device.GraphicsProfile == GraphicsProfile.HiDef;
-            SupportsHalfFloatTextures = device.GraphicsProfile == GraphicsProfile.HiDef;
-            SupportsNormalized = device.GraphicsProfile == GraphicsProfile.HiDef;
+            SupportsTextureArrays = device.GraphicsProfile >= GraphicsProfile.FL10_0;
+            SupportsDepthClamp = device.GraphicsProfile >= GraphicsProfile.HiDef;
+            SupportsVertexTextures = device.GraphicsProfile >= GraphicsProfile.FL10_0;
+            SupportsFloatTextures = device.GraphicsProfile >= GraphicsProfile.HiDef;
+            SupportsHalfFloatTextures = device.GraphicsProfile >= GraphicsProfile.HiDef;
+            SupportsNormalized = device.GraphicsProfile >= GraphicsProfile.HiDef;
 
             SupportsInstancing = true;
             //TNC: TODO: detect suport based on feture level