d6fdc921cd0c438a86ca6c0c88695f9cd1859356
Nick Kastellanos <nkastellanos@gmail.com>
2022-08-17 11:53:39 +0300

    RefInput

---
+++ MonoGame.Framework/Input/GamePad.Ref.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (C)2022 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Xna.Framework.Input
+{
+    static partial class GamePad
+    {
+
+        static GamePad()
+        {
+        }
+
+        private static int PlatformGetMaxNumberOfGamePads()
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        private static GamePadCapabilities PlatformGetCapabilities(int index)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        private static GamePadState PlatformGetState(int index, GamePadDeadZone leftDeadZoneMode, GamePadDeadZone rightDeadZoneMode)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        private static bool PlatformSetVibration(int index, float leftMotor, float rightMotor, float leftTrigger, float rightTrigger)
+        {
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
---
+++ MonoGame.Framework/Input/Keyboard.Ref.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (C)2022 Nick Kastellanos
+
+using System;
+
+namespace Microsoft.Xna.Framework.Input
+{
+    public static partial class Keyboard
+    {
+        private static KeyboardState PlatformGetState()
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+    }
+}
