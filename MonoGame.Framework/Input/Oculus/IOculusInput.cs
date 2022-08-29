b7f1522d05d4e9ff2c61ed633909fb53f462b9e6
Nick Kastellanos <nkastellanos@gmail.com>
2022-08-29 14:52:59 +0300

    replace TouchControllerStrategy with IOculusInput

---
+++ MonoGame.Framework/Input/Oculus/IOculusInput.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (C)2022 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Oculus;
+
+
+namespace Microsoft.Xna.Platform.Input.Oculus
+{
+    public interface IOculusInput
+    {
+        GamePadCapabilities GetCapabilities(TouchControllerType type);
+        TouchControllerState GetState(TouchControllerType type);
+        bool SetVibration(TouchControllerType type, float amplitude);
+    }
+}
