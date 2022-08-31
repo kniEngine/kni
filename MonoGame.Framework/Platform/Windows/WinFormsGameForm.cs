1c3518775e3a41ba43b9c75646eebe38a5e37061
Nick Kastellanos <nkastellanos@gmail.com>
2022-08-31 09:28:41 +0300

    simplifying (WINDOWS || DESKTOPGL) && DIRECTX

--- MonoGame.Framework/Platform/Windows/WinFormsGameForm.cs
+++ MonoGame.Framework/Platform/Windows/WinFormsGameForm.cs
@@ -134,7 +134,7 @@ namespace Microsoft.Xna.Framework.Windows
                         m.Result = new IntPtr(flags);
                         return;
                     }
-#if ((WINDOWS || DESKTOPGL) && DIRECTX)
+#if WINDOWS
                 case WM_KEYDOWN:
                     HandleKeyMessage(ref m);
                     switch (m.WParam.ToInt32())
