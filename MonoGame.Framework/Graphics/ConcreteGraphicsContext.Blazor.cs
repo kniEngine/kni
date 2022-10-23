4e2d600619855c27bab214ece6201b7279078c89
Nick Kastellanos <nkastellanos@gmail.com>
2022-10-23 08:14:06 +0300

    ConcreteGraphicsContext

---
+++ MonoGame.Framework/Graphics/ConcreteGraphicsContext.Blazor.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (C)2022 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+
+
+namespace Microsoft.Xna.Platform.Graphics
+{
+    internal sealed class ConcreteGraphicsContext : GraphicsContextStrategy
+    {
+
+        internal ConcreteGraphicsContext() : base()
+        {
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ThrowIfDisposed();
+
+                base.Dispose();
+            }
+        }
+    }
+}
