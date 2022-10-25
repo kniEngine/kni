d945b0adc383fa987654f143d73f1bc2d29d4565
Nikos Kastellanos <nkastellanos@gmail.com>
2022-10-25 07:37:01 +0300

    GraphicsContext (#78)
    * GraphicsContext
    
    * ConcreteGraphicsContext

---
+++ MonoGame.Framework/Graphics/GraphicsContextStrategy.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (C)2022 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+namespace Microsoft.Xna.Platform.Graphics
+{
+    public abstract class GraphicsContextStrategy : IDisposable
+    {
+        private bool _isDisposed = false;
+
+        protected GraphicsContextStrategy()
+        {
+            
+        }
+
+        #region IDisposable Members
+
+        ~GraphicsContextStrategy()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ThrowIfDisposed();
+
+                _isDisposed = true;
+            }
+        }
+
+        //[Conditional("DEBUG")]
+        protected void ThrowIfDisposed()
+        {
+            if (!_isDisposed)
+                return;
+
+            throw new ObjectDisposedException("Object is Disposed.");
+        }
+
+        #endregion IDisposable Members
+
+    }
+}
