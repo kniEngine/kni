17f23fa6b7601065a639c64c4f2d141ba3bb1678
Nick Kastellanos <nkastellanos@gmail.com>
2022-12-09 18:10:53 +0200

    Glyph .ctor

--- MonoGame.Framework.Content.Pipeline/Graphics/Font/Glyph.cs
+++ MonoGame.Framework.Content.Pipeline/Graphics/Font/Glyph.cs
@@ -27,11 +27,11 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
     internal class Glyph
     {
         // Constructor.
-        public Glyph(uint glyphIndex, BitmapContent bitmap, Rectangle? subrect = null)
+        public Glyph(uint glyphIndex, BitmapContent bitmap)
         {
             GlyphIndex = glyphIndex;
             Bitmap = bitmap;
-            Subrect = subrect.GetValueOrDefault(new Rectangle(0, 0, bitmap.Width, bitmap.Height));
+            Subrect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
             Width = bitmap.Width;
             Height = bitmap.Height;
         }
