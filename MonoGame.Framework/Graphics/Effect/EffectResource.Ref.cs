0ff07a751de542f20f2d7a58b66b75e9be411672
Nick Kastellanos <nkastellanos@gmail.com>
2022-12-30 04:32:25 +0200

    fxo

--- MonoGame.Framework/Graphics/Effect/EffectResource.Ref.cs
+++ MonoGame.Framework/Graphics/Effect/EffectResource.Ref.cs
@@ -4,11 +4,11 @@ namespace Microsoft.Xna.Framework.Graphics
 {
     internal partial class EffectResource
     {
-        const string AlphaTestEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.AlphaTestEffect.ref.mgfxo";
-        const string BasicEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.BasicEffect.ref.mgfxo";
-        const string DualTextureEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.DualTextureEffect.ref.mgfxo";
-        const string EnvironmentMapEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.EnvironmentMapEffect.ref.mgfxo";
-        const string SkinnedEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.SkinnedEffect.ref.mgfxo";
-        const string SpriteEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.SpriteEffect.ref.mgfxo";
+        const string AlphaTestEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.AlphaTestEffect.ref.fxo";
+        const string BasicEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.BasicEffect.ref.fxo";
+        const string DualTextureEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.DualTextureEffect.ref.fxo";
+        const string EnvironmentMapEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.EnvironmentMapEffect.ref.fxo";
+        const string SkinnedEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.SkinnedEffect.ref.fxo";
+        const string SpriteEffectName = "Microsoft.Xna.Framework.Graphics.Effect.Resources.SpriteEffect.ref.fxo";
     }
 }
