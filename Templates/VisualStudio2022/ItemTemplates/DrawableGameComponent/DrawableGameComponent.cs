6457d20877e629f58d0d50130decff1664f681ba
Nikos Kastellanos <nkastellanos@gmail.com>
2023-01-07 07:03:46 +0200

    DrawableGameComponent Template (#138)

---
+++ Templates/VisualStudio2022/ItemTemplates/DrawableGameComponent/DrawableGameComponent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace $rootnamespace$
+{
+    internal class $safeitemname$ : DrawableGameComponent
+    {
+        public $safeitemname$(Game game) : base(game)
+        {
+        }
+
+        /// <summary>Initializes the component. Used to load non-graphical resources.</summary>
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>Load graphical resources needed by this component.</summary>
+        protected override void LoadContent()
+        {
+        }
+
+        /// <summary>Unload graphical resources needed by this component.</summary>
+        protected override void UnloadContent()
+        {
+        }
+
+        /// <summary>Update the component.</summary>
+        /// <param name="gameTime">GameTime of the Game.</param>
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        /// <summary>Draw this component.</summary>
+        /// <param name="gameTime">The time elapsed since the last call to Draw.</param>
+        public override void Draw(GameTime gameTime)
+        {
+        }
+
+    }
+}
