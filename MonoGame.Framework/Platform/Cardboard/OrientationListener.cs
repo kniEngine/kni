5506eb242723205a30289b879a8944db983123b0
Nick Kastellanos <nkastellanos@gmail.com>
2023-01-29 07:51:56 +0200

    Cardboard project

---
+++ MonoGame.Framework/Platform/Cardboard/OrientationListener.cs
@@ -0,0 +1,80 @@
+// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+using Android.App;
+using Android.Content;
+using Android.Hardware;
+using Android.Views;
+using Android.Provider;
+
+
+namespace Microsoft.Xna.Framework
+{
+    internal class OrientationListener : OrientationEventListener
+    {
+        internal DisplayOrientation targetOrientation = DisplayOrientation.Unknown;
+        TimeSpan elapsed = TimeSpan.Zero;
+
+        /// <summary>
+        /// Constructor. SensorDelay.Ui is passed to the base class as this orientation listener 
+        /// is just used for flipping the screen orientation, therefore high frequency data is not required.
+        /// </summary>
+        public OrientationListener(Context context)
+            : base(context, SensorDelay.Ui)
+        {
+        }
+
+        public override void OnOrientationChanged(int orientation)
+        {
+            if (orientation == OrientationEventListener.OrientationUnknown)
+            {                
+                targetOrientation = DisplayOrientation.Unknown;
+                elapsed = TimeSpan.Zero;
+                return;
+            }
+
+            // Avoid changing orientation whilst the screen is locked
+            if (ScreenReceiver.ScreenLocked)
+                return;
+
+            var disporientation = AndroidCompatibility.Current.GetAbsoluteOrientation(orientation);
+
+            AndroidGameWindow gameWindow = (AndroidGameWindow)Game.Instance.Window;
+            if ((gameWindow.GetEffectiveSupportedOrientations() & disporientation) == 0 ||
+                disporientation == gameWindow.CurrentOrientation ||
+                disporientation == DisplayOrientation.Unknown)
+            {
+                targetOrientation = DisplayOrientation.Unknown;
+                elapsed = TimeSpan.Zero;
+                return;
+            }
+
+            // Delay changing of Orientation. Filter random shocks.
+            if (targetOrientation != disporientation)
+            {
+                targetOrientation = disporientation;
+                elapsed = TimeSpan.Zero;
+            }
+
+            return;
+        }
+        
+        internal void Update(TimeSpan elapsedTime)
+        {            
+            if (targetOrientation != DisplayOrientation.Unknown)
+            {
+                elapsed += elapsedTime;
+                // orientation must be stable for 0.5 seconds before changing.
+                if (elapsed.TotalSeconds > 0.5)
+                {
+                    AndroidGameWindow gameWindow = (AndroidGameWindow)Game.Instance.Window;
+                    gameWindow.SetOrientation(targetOrientation, true);
+                    targetOrientation = DisplayOrientation.Unknown;
+                    elapsed = TimeSpan.Zero;
+                }
+            }
+        }
+    }
+}
