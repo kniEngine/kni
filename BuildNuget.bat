56ebed581a62a151208c402713e78230d8fe102f
Nikos Kastellanos <nkastellanos@gmail.com>
2023-04-09 08:59:42 +0300

    pipeline modules (#290)

--- BuildNuget.bat
+++ BuildNuget.bat
@@ -16,7 +16,6 @@ dotnet pack MonoGame.Framework\Xna.Framework.Ref.csproj --output NuGetPackages\O
 dotnet pack MonoGame.Framework\XNA.Framework.Oculus.OvrDX11.csproj --output NuGetPackages\Output\ /t:Build /p:Configuration=Release 
 
 "C:\Program Files (x86)\NuGet3\nuget.exe" pack NuGetPackages/MonoGame.Framework.Portable.nuspec			-OutputDirectory NuGetPackages\Output\  -BasePath .  -Version 3.8.9102.0  -Properties Configuration=Release
-"C:\Program Files (x86)\NuGet3\nuget.exe" pack NuGetPackages/MonoGame.Framework.Content.Pipeline.Portable.nuspec -OutputDirectory NuGetPackages\Output\  -BasePath .  -Version 3.8.9102.0  -Properties Configuration=Release
 
 "C:\Program Files (x86)\NuGet3\nuget.exe" pack NuGetPackages/MonoGame.Framework.Cardboard.nuspec        -OutputDirectory NuGetPackages\Output\  -BasePath .  -Version 3.8.9102.0  -Properties Configuration=Release
 
