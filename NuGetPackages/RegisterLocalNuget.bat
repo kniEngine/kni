afb1f313212ead3cec87372f1e1a5dce7a0d3ff3
Nick Kastellanos <nkastellanos@gmail.com>
2023-04-22 08:09:38 +0300

    register local nuget Packages

---
+++ NuGetPackages/RegisterLocalNuget.bat
@@ -0,0 +1,38 @@
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework                           3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Design                    3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Ref                       3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Content.Pipeline          3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Content.Pipeline.Audio    3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Content.Pipeline.Graphics 3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Content.Pipeline.Media    3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Blazor                    3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete MonoGame.Framework.WindowsDX.9000             3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete MonoGame.Framework.WindowsUniversal.9000      3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete MonoGame.Framework.DesktopGL.9000             3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete MonoGame.Framework.Android.9000               3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Oculus.OvrDX11            3.8.9102 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+
+
+
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.3.8.9102.nupkg                           -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Design.3.8.9102.nupkg                    -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Ref.3.8.9102.nupkg                       -Source "C:\Users\usename\.nuget\localPackages"
+
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.3.8.9102.nupkg          -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Audio.3.8.9102.nupkg    -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Graphics.3.8.9102.nupkg -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Media.3.8.9102.nupkg    -Source "C:\Users\usename\.nuget\localPackages"
+
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Blazor.3.8.9102.nupkg                    -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\MonoGame.Framework.WindowsDX.9000.3.8.9102.nupkg             -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\MonoGame.Framework.WindowsUniversal.9000.3.8.9102.nupkg      -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\MonoGame.Framework.DesktopGL.9000.3.8.9102.nupkg             -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\MonoGame.Framework.Android.9000.3.8.9102.nupkg               -Source "C:\Users\usename\.nuget\localPackages"
+
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Oculus.OvrDX11.3.8.9102.nupkg            -Source "C:\Users\usename\.nuget\localPackages"
+
+
+@pause
