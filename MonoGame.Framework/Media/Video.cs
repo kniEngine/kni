41ead0bc3064ee1859ead4b36a79a11e3b67b077
Nick Kastellanos <nkastellanos@gmail.com>
2023-05-04 15:31:41 +0300

    VideoStrategy

--- MonoGame.Framework/Media/Video.cs
+++ MonoGame.Framework/Media/Video.cs
@@ -1,10 +1,12 @@
-﻿// MonoGame - Copyright (C) The MonoGame Team
+﻿// Copyright (C)2023 Nick Kastellanos
+
+// MonoGame - Copyright (C) The MonoGame Team
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
-using System.IO;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Platform.Media;
 
 namespace Microsoft.Xna.Framework.Media
 {
@@ -12,42 +14,60 @@ namespace Microsoft.Xna.Framework.Media
     /// Represents a video.
     /// </summary>
     public sealed partial class Video : IDisposable
-	{
+    {
         private bool _disposed;
+        private VideoStrategy _strategy;
 
-        internal GraphicsDevice GraphicsDevice { get; private set; }
+        internal VideoStrategy Strategy { get { return _strategy; } }
+        internal GraphicsDevice GraphicsDevice { get { return Strategy.GraphicsDevice; } }
 
         #region Public API
 
         /// <summary>
         /// I actually think this is a file PATH...
         /// </summary>
-        public string FileName { get; private set; }
+        public string FileName { get { return Strategy.FileName; } }
 
 		/// <summary>
 		/// Gets the duration of the Video.
         /// </summary>
-        public TimeSpan Duration { get; internal set; }
+        public TimeSpan Duration { get { return Strategy.Duration; } }
 
         /// <summary>
         /// Gets the frame rate of this video.
         /// </summary>
-        public float FramesPerSecond { get; internal set; }
+        public float FramesPerSecond
+        {
+            get { return Strategy.FramesPerSecond; }
+            internal set { Strategy.FramesPerSecond = value; }
+        }
 
         /// <summary>
         /// Gets the height of this video, in pixels.
         /// </summary>
-        public int Height { get; internal set; }
+        public int Height
+        {
+            get { return Strategy.Height; }
+            internal set { Strategy.Height = value; }
+        }
 
         /// <summary>
         /// Gets the VideoSoundtrackType for this video.
         /// </summary>
-        public VideoSoundtrackType VideoSoundtrackType { get; internal set; }
+        public VideoSoundtrackType VideoSoundtrackType
+        {
+            get { return Strategy.VideoSoundtrackType; }
+            internal set { Strategy.VideoSoundtrackType = value; }
+        }
 
         /// <summary>
         /// Gets the width of this video, in pixels.
         /// </summary>
-        public int Width { get; internal set; }
+        public int Width
+        {
+            get { return Strategy.Width; }
+            internal set { Strategy.Width = value; }
+}
 
         #endregion
 
@@ -55,9 +75,7 @@ namespace Microsoft.Xna.Framework.Media
 
         internal Video(GraphicsDevice graphicsDevice, string fileName, float durationMS)
         {
-            GraphicsDevice = graphicsDevice;
-            FileName = fileName;
-            Duration = TimeSpan.FromMilliseconds(durationMS);
+            _strategy = new VideoStrategy(graphicsDevice, fileName, TimeSpan.FromMilliseconds(durationMS));
 
             PlatformInitialize();
         }
@@ -82,10 +100,14 @@ namespace Microsoft.Xna.Framework.Media
             if (!_disposed)
             {
                 //PlatformDispose(disposing);
+
+                if (disposing)
+                {
+                    _strategy.Dispose();
+                }
+
                 _disposed = true;
             }
-
-            GraphicsDevice = null;
         }
 
         #endregion
---
+++ MonoGame.Framework/Media/VideoStrategy.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (C)2023 Nick Kastellanos
+
+using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+
+namespace Microsoft.Xna.Platform.Media
+{
+    public class VideoStrategy : IDisposable
+    {
+        public VideoStrategy(GraphicsDevice graphicsDevice, string fileName, TimeSpan duration)
+        {
+            this.GraphicsDevice = graphicsDevice;
+            this.FileName = fileName;
+            this.Duration = duration;
+        }
+
+        public GraphicsDevice GraphicsDevice { get; private set; }
+
+        /// <summary>
+        /// I actually think this is a file PATH...
+        /// </summary>
+        public string FileName { get; private set; }
+
+        /// <summary>
+        /// Gets the duration of the Video.
+        /// </summary>
+        public TimeSpan Duration { get; internal set; }
+
+        /// <summary>
+        /// Gets the frame rate of this video.
+        /// </summary>
+        public float FramesPerSecond { get; internal set; }
+
+        /// <summary>
+        /// Gets the width of this video, in pixels.
+        /// </summary>
+        public int Width { get; internal set; }
+
+        /// <summary>
+        /// Gets the height of this video, in pixels.
+        /// </summary>
+        public int Height { get; internal set; }
+
+        /// <summary>
+        /// Gets the VideoSoundtrackType for this video.
+        /// </summary>
+        public VideoSoundtrackType VideoSoundtrackType { get; internal set; }
+
+
+        #region IDisposable Members
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+            }
+
+            GraphicsDevice = null;
+        }
+
+        #endregion
+    }
+}
