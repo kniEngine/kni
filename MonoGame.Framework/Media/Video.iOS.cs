d56aa9de628ee58025321747cac9b079749147d7
Nick Kastellanos <nkastellanos@gmail.com>
2023-05-05 03:55:56 +0300

    remove dead code

--- MonoGame.Framework/Media/Video.iOS.cs
+++ MonoGame.Framework/Media/Video.iOS.cs
@@ -4,8 +4,6 @@
 
 using System;
 using System.IO;
-using Microsoft.Xna.Framework.Graphics;
-
 using MediaPlayer;
 using Foundation;
 
@@ -19,25 +17,6 @@ namespace Microsoft.Xna.Framework.Media
     {
         internal MPMoviePlayerViewController MovieView { get; private set; }
 
-        /*
-        // NOTE: https://developer.apple.com/library/ios/documentation/MediaPlayer/Reference/MPMoviePlayerController_Class/Reference/Reference.html
-        // It looks like BackgroundColor doesn't even exist anymore
-        // in recent versions of iOS... Why still have this?
-        public Color BackgroundColor
-        {
-            get
-            {
-                var col = MovieView.MoviePlayer.BackgroundColor;
-                return new Color(col.X, col.Y, col.Z, col.W);
-            }
-
-            set
-            {
-                var col = value.ToVector4();
-                return MovieView.MoviePlayer.BackgroundColor = UIKit.UIColor(col.X, col.Y, col.Z, col.W);
-            }
-        }
-        */
 
         private void PlatformInitialize()
         {
