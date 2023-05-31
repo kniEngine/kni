f7ff76ef21ba3921bae349aadc6ae50160f1702d
Nick Kastellanos <nkastellanos@gmail.com>
2023-05-31 16:48:19 +0300

    implement VideoPlayer.GetTexture() for Android

--- MonoGame.Framework/Media/Android/ConcreteVideo.cs
+++ MonoGame.Framework/Media/Android/ConcreteVideo.cs
@@ -12,36 +12,22 @@ namespace Microsoft.Xna.Platform.Media
 {
     internal sealed class ConcreteVideoStrategy : VideoStrategy
     {
-        internal VideoPlatformStream _videoPlatformStream;
 
 
         internal ConcreteVideoStrategy(GraphicsDevice graphicsDevice, string fileName, TimeSpan duration)
             : base(graphicsDevice, fileName, duration)
         {
-            this._videoPlatformStream = new VideoPlatformStream(this.FileName);
 
-        }
-
-        internal VideoPlatformStream GetVideoPlatformStream()
-        {
-            return _videoPlatformStream;
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                {
-                    if (_videoPlatformStream != null)
-                    {
-                        _videoPlatformStream.Dispose();
-                        _videoPlatformStream = null;
-                    }
 
-                }
-
-                base.Dispose(disposing);
             }
+
+            base.Dispose(disposing);
         }
     }
     
