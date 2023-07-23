7eaef574c494ef297fbb8d4f44a53aa0faa82c20
Nick Kastellanos <nkastellanos@gmail.com>
2023-07-23 12:14:56 +0300

    pass a null terminated string to SDL_SetWindowTitle

--- MonoGame.Framework/Platform/SDL/SDL2.cs
+++ MonoGame.Framework/Platform/SDL/SDL2.cs
@@ -439,7 +439,7 @@ internal static class Sdl
 
         public static void SetTitle(IntPtr handle, string title)
         {
-            var bytes = Encoding.UTF8.GetBytes(title);
+            byte[] bytes = Encoding.UTF8.GetBytes(title+'\0');
             SDL_SetWindowTitle(handle, ref bytes[0]);
         }
 
