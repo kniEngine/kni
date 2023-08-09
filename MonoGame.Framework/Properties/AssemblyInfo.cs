8e622d384bd01cc161a325d2c55e6d7c64ea648d
Nikos Kastellanos <nkastellanos@gmail.com>
2023-08-09 12:57:22 +0300

    ver3.9.9001 (#517)

--- MonoGame.Framework/Properties/AssemblyInfo.cs
+++ MonoGame.Framework/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Resources;
 
 // Version information for the assembly which is automatically
 // set by our automated build process.
-[assembly: AssemblyVersion("3.8.9102.0")]
-[assembly: AssemblyFileVersion("3.8.9102.0")]
+[assembly: AssemblyVersion("3.9.9001.0")]
+[assembly: AssemblyFileVersion("3.9.9001.0")]
