1335724b165c9b0a7a9ca45abdc21a8e752d0939
Nick Kastellanos <nkastellanos@gmail.com>
2023-08-25 16:22:26 +0300

    protected TextureCube

--- MonoGame.Framework/Graphics/TextureCube.cs
+++ MonoGame.Framework/Graphics/TextureCube.cs
@@ -25,7 +25,7 @@ namespace Microsoft.Xna.Framework.Graphics
 		{
         }
 
-        internal TextureCube(GraphicsDevice graphicsDevice, int size, bool mipMap, SurfaceFormat format, bool renderTarget)
+        protected TextureCube(GraphicsDevice graphicsDevice, int size, bool mipMap, SurfaceFormat format, bool renderTarget)
         {
             if (graphicsDevice == null)
                 throw new ArgumentNullException("graphicsDevice", FrameworkResources.ResourceCreationWhenDeviceIsNull);
