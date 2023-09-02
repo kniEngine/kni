88e34f692bc6c90a19f806ce59f03b2b5300ad01
Nikos Kastellanos <nkastellanos@gmail.com>
2023-09-02 08:18:01 +0300

    GraphicsResourceStrategy (#601)
    * GraphicsResourceStrategy
    
    * GraphicsResourceStrategy.Disposing

---
+++ MonoGame.Framework/Graphics/GraphicsResourceStrategy.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (C)2023 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Microsoft.Xna.Platform.Graphics
+{
+    public class GraphicsResourceStrategy : IGraphicsResourceStrategy
+    {
+        private GraphicsDeviceStrategy _deviceStrategy;
+
+        public GraphicsDevice GraphicsDevice
+        { 
+            get 
+            {
+                if (_deviceStrategy == null)
+                    return null;
+                return _deviceStrategy.Device;
+            }
+        }
+
+        public event EventHandler<EventArgs> Disposing;
+
+        internal GraphicsResourceStrategy()
+        {
+        }
+
+
+        internal void SetGraphicsDevice(GraphicsDeviceStrategy deviceStrategy)
+        {
+            _deviceStrategy = deviceStrategy;
+        }
+
+
+        #region IDisposable Members
+
+        ~GraphicsResourceStrategy()
+        {
+            OnDisposing(EventArgs.Empty);
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            OnDisposing(EventArgs.Empty);
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        internal void OnDisposing(EventArgs e)
+        {
+            var handler = Disposing;
+            if (handler != null)
+                handler(this, e);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+                _deviceStrategy = null;
+            }
+
+        }
+
+        #endregion IDisposable Members
+
+    }
+}
---
+++ MonoGame.Framework/Graphics/IGraphicsResourceStrategy.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (C)2023 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Microsoft.Xna.Platform.Graphics
+{
+    public interface IGraphicsResourceStrategy : IDisposable
+    {
+        public GraphicsDevice GraphicsDevice { get; }
+
+        public event EventHandler<EventArgs> Disposing;
+    }
+}
