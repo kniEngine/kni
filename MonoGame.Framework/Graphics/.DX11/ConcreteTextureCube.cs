7d2e0073061d7fbd22840acecf6c71fc6627dd8f
Nick Kastellanos <nkastellanos@gmail.com>
2023-09-05 18:45:23 +0300

    move DX texture SetData()
    move copyright

--- MonoGame.Framework/Graphics/.DX11/ConcreteTextureCube.cs
+++ MonoGame.Framework/Graphics/.DX11/ConcreteTextureCube.cs
@@ -1,4 +1,8 @@
-﻿// Copyright (C)2023 Nick Kastellanos
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+// Copyright (C)2023 Nick Kastellanos
 
 using System;
 using System.IO;
