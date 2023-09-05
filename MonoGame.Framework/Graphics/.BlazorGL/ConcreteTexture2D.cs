add4869d5b733877a1ca581f764cf0cc4b91a0f2
Nick Kastellanos <nkastellanos@gmail.com>
2023-09-05 18:42:09 +0300

    move GL texture SetData()

--- MonoGame.Framework/Graphics/.BlazorGL/ConcreteTexture2D.cs
+++ MonoGame.Framework/Graphics/.BlazorGL/ConcreteTexture2D.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Framework.Utilities;
+using nkast.Wasm.Canvas.WebGL;
 
 
 namespace Microsoft.Xna.Platform.Graphics
@@ -36,11 +38,70 @@ namespace Microsoft.Xna.Platform.Graphics
         public void SetData<T>(int level, T[] data, int startIndex, int elementCount)
             where T : struct
         {
+            var GL = GraphicsDevice.Strategy.CurrentContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
+
+            int w, h;
+            Texture.GetSizeForLevel(Width, Height, level, out w, out h);
+
+            var elementSizeInByte = ReflectionHelpers.SizeOf<T>();
+
+            var startBytes = startIndex * elementSizeInByte;
+            if (startIndex != 0 && !_glIsCompressedTexture)
+                throw new NotImplementedException("startIndex");
+
+            System.Diagnostics.Debug.Assert(_glTexture == null);
+            GL.BindTexture(WebGLTextureTarget.TEXTURE_2D, _glTexture);
+            GraphicsExtensions.CheckGLError();
+
+            GL.PixelStore(WebGLPixelParameter.UNPACK_ALIGNMENT, Math.Min(this.Format.GetSize(), 8));
+            GraphicsExtensions.CheckGLError();
+
+            if (_glIsCompressedTexture)
+            {
+                GL.CompressedTexImage2D(
+                        WebGLTextureTarget.TEXTURE_2D, level, _glInternalFormat, w, h, data, startIndex, elementCount);
+            }
+            else
+            {
+                GL.TexImage2D(WebGLTextureTarget.TEXTURE_2D, level, _glInternalFormat, w, h, _glFormat, _glType, data);
+            }
+            GraphicsExtensions.CheckGLError();
+
+            //GL.Finish();
+            //GraphicsExtensions.CheckGLError();
         }
 
-        public void SetData<T>(int level, int arraySlice, Rectangle rect, T[] data, int startIndex, int elementCount)
+        public void SetData<T>(int level, int arraySlice, Rectangle checkedRect, T[] data, int startIndex, int elementCount)
             where T : struct
         {
+            var GL = GraphicsDevice.Strategy.CurrentContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
+
+            var elementSizeInByte = ReflectionHelpers.SizeOf<T>();
+
+            var startBytes = startIndex * elementSizeInByte;
+            if (startIndex != 0)
+                throw new NotImplementedException("startIndex");
+
+            System.Diagnostics.Debug.Assert(_glTexture == null);
+            GL.BindTexture(WebGLTextureTarget.TEXTURE_2D, _glTexture);
+            GraphicsExtensions.CheckGLError();
+
+            GL.PixelStore(WebGLPixelParameter.UNPACK_ALIGNMENT, Math.Min(this.Format.GetSize(), 8));
+
+            if (_glIsCompressedTexture)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                GL.TexSubImage2D(
+                    WebGLTextureTarget.TEXTURE_2D, level, checkedRect.X, checkedRect.Y, checkedRect.Width, checkedRect.Height,
+                    _glFormat, _glType, data);
+            }
+            GraphicsExtensions.CheckGLError();
+
+            //GL.Finish();
+            //GraphicsExtensions.CheckGLError();
         }
 
         public void GetData<T>(int level, int arraySlice, Rectangle rect, T[] data, int startIndex, int elementCount)
