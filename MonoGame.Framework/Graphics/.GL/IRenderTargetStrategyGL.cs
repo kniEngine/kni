5bf77fd265c3a7c8938e1b01c80691329b7fff2c
Nick Kastellanos <nkastellanos@gmail.com>
2023-09-08 19:19:48 +0300

    move IRenderTargetStrategyGL.cs

--- MonoGame.Framework/Graphics/.GL/IRenderTargetStrategyGL.cs
+++ MonoGame.Framework/Graphics/.GL/IRenderTargetStrategyGL.cs
@@ -2,6 +2,8 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
+// Copyright (C)2023 Nick Kastellanos
+
 using MonoGame.OpenGL;
 
 namespace Microsoft.Xna.Platform.Graphics
