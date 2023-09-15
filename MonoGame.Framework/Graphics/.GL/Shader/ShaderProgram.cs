74eb30e21351d1aa531f8ab4817ca5108099c515
Nikos Kastellanos <nkastellanos@gmail.com>
2023-09-15 20:43:46 +0300

    move ShaderProgram (#668)

--- MonoGame.Framework/Graphics/.GL/Shader/ShaderProgram.cs
+++ MonoGame.Framework/Graphics/.GL/Shader/ShaderProgram.cs
@@ -1,8 +1,11 @@
+// Copyright (C)2023 Nick Kastellanos
 
 using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
 
-namespace Microsoft.Xna.Framework.Graphics
+
+namespace Microsoft.Xna.Platform.Graphics
 {
     internal class ShaderProgram
     {
