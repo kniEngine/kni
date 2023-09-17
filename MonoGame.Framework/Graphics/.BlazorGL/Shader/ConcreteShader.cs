3dd820243a4606cfa8850e27b4a0bb4a90e67a99
Nick Kastellanos <nkastellanos@gmail.com>
2023-09-17 06:37:43 +0300

    move ConcreteShader members WebGL

--- MonoGame.Framework/Graphics/.BlazorGL/Shader/ConcreteShader.cs
+++ MonoGame.Framework/Graphics/.BlazorGL/Shader/ConcreteShader.cs
@@ -4,12 +4,18 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using nkast.Wasm.Canvas.WebGL;
 
 
 namespace Microsoft.Xna.Platform.Graphics
 {
     public class ConcreteShader : ShaderStrategy
     {
+        // The shader handle.
+        private WebGLShader _shaderHandle = null;
+
+        // We keep this around for recompiling on context lost and debugging.
+        private string _glslCode;
     
         internal ConcreteShader(GraphicsContextStrategy contextStrategy, ShaderStage stage, byte[] shaderBytecode, SamplerInfo[] samplers, int[] cBuffers, VertexAttribute[] attributes, ShaderProfileType profile)
             : base(contextStrategy, stage, shaderBytecode, samplers, cBuffers, attributes, profile)
@@ -17,6 +23,87 @@ namespace Microsoft.Xna.Platform.Graphics
             if (profile != ShaderProfileType.OpenGL_Mojo)
                 throw new Exception("This effect was built for a different platform.");
 
+            _glslCode = System.Text.Encoding.ASCII.GetString(shaderBytecode);
+
+            _hashKey = MonoGame.Framework.Utilities.Hash.ComputeHash(shaderBytecode);
+        }
+
+        internal WebGLShader GetShaderHandle()
+        {
+            // If the shader has already been created then return it.
+            if (_shaderHandle != null)
+                return _shaderHandle;
+
+            var GL = GraphicsDevice.Strategy.CurrentContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
+
+            //
+            _shaderHandle = GL.CreateShader(Stage == ShaderStage.Vertex ? WebGLShaderType.VERTEX : WebGLShaderType.FRAGMENT);
+            GraphicsExtensions.CheckGLError();
+            GL.ShaderSource(_shaderHandle, _glslCode);
+            GraphicsExtensions.CheckGLError();
+            GL.CompileShader(_shaderHandle);
+            GraphicsExtensions.CheckGLError();
+            bool compiled = false;
+            compiled = GL.GetShaderParameter(_shaderHandle, WebGLShaderStatus.COMPILE);
+            GraphicsExtensions.CheckGLError();
+            if (compiled != true)
+            {
+                var log = GL.GetShaderInfoLog(_shaderHandle);
+                _shaderHandle.Dispose();
+                _shaderHandle = null;
+                throw new InvalidOperationException("Shader Compilation Failed");
+            }
+
+            return _shaderHandle;
+        }
+
+        internal void GetVertexAttributeLocations(WebGLProgram program)
+        {
+            var GL = GraphicsDevice.Strategy.CurrentContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
+
+            for (int i = 0; i < Attributes.Length; i++)
+            {
+                Attributes[i].location = GL.GetAttribLocation(program, Attributes[i].name);
+                GraphicsExtensions.CheckGLError();
+            }
+        }
+
+        internal int GetAttribLocation(VertexElementUsage usage, int index)
+        {
+            for (int i = 0; i < Attributes.Length; i++)
+            {
+                if ((Attributes[i].usage == usage) && (Attributes[i].index == index))
+                    return Attributes[i].location;
+            }
+            return -1;
+        }
+
+        internal void ApplySamplerTextureUnits(WebGLProgram program)
+        {
+            var GL = GraphicsDevice.Strategy.CurrentContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
+
+            // Assign the texture unit index to the sampler uniforms.
+            foreach (var sampler in Samplers)
+            {
+                var loc = GL.GetUniformLocation(program, sampler.name);
+                GraphicsExtensions.CheckGLError();
+                if (loc != null)
+                {
+                    GL.Uniform1i(loc, sampler.textureSlot);
+                    GraphicsExtensions.CheckGLError();
+                }
+            }
+        }
+
+        internal override void PlatformGraphicsDeviceResetting()
+        {
+            if (_shaderHandle != null)
+            {
+                _shaderHandle.Dispose();
+                _shaderHandle = null;
+            }
+
+            base.PlatformGraphicsDeviceResetting();
         }
 
 
@@ -24,6 +111,11 @@ namespace Microsoft.Xna.Platform.Graphics
         {
             if (disposing)
             {
+                if (_shaderHandle != null)
+                {
+                    _shaderHandle.Dispose();
+                    _shaderHandle = null;
+                }
             }
 
             base.Dispose(disposing);
--- MonoGame.Framework/Graphics/Shader/Shader.Blazor.cs
+++ MonoGame.Framework/Graphics/Shader/Shader.Blazor.cs
@@ -7,95 +7,17 @@
 using System;
 using System.Diagnostics;
 using Microsoft.Xna.Platform.Graphics;
-using nkast.Wasm.Canvas.WebGL;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
     internal partial class Shader
     {
-        // The shader handle.
-        private WebGLShader _shaderHandle = null;
-
-        // We keep this around for recompiling on context lost and debugging.
-        private string _glslCode;
-
         private void PlatformConstructShader(ShaderStage stage, byte[] shaderBytecode)
         {
-            _glslCode = System.Text.Encoding.ASCII.GetString(shaderBytecode);
-
-            _strategy._hashKey = MonoGame.Framework.Utilities.Hash.ComputeHash(shaderBytecode);
-        }
-
-        internal WebGLShader GetShaderHandle()
-        {
-            // If the shader has already been created then return it.
-            if (_shaderHandle != null)
-                return _shaderHandle;
-
-            var GL = GraphicsDevice.Strategy.CurrentContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
-
-            //
-            _shaderHandle = GL.CreateShader(Stage == ShaderStage.Vertex ? WebGLShaderType.VERTEX : WebGLShaderType.FRAGMENT);
-            GraphicsExtensions.CheckGLError();
-            GL.ShaderSource(_shaderHandle, _glslCode);
-            GraphicsExtensions.CheckGLError();
-            GL.CompileShader(_shaderHandle);
-            GraphicsExtensions.CheckGLError();
-            bool compiled = false;
-            compiled = GL.GetShaderParameter(_shaderHandle, WebGLShaderStatus.COMPILE);
-            GraphicsExtensions.CheckGLError();
-            if (compiled != true)
-            {
-                var log = GL.GetShaderInfoLog(_shaderHandle);
-                _shaderHandle.Dispose();
-                _shaderHandle = null;
-                throw new InvalidOperationException("Shader Compilation Failed");
-            }
-
-            return _shaderHandle;
-        }
-
-        internal void GetVertexAttributeLocations(WebGLProgram program)
-        {
-            var GL = GraphicsDevice.Strategy.CurrentContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
-
-            for (int i = 0; i < Attributes.Length; i++)
-            {
-                Attributes[i].location = GL.GetAttribLocation(program, Attributes[i].name);
-                GraphicsExtensions.CheckGLError();
-            }
-        }
-
-        internal int GetAttribLocation(VertexElementUsage usage, int index)
-        {
-            for (int i = 0; i < Attributes.Length; i++)
-            {
-                if ((Attributes[i].usage == usage) && (Attributes[i].index == index))
-                    return Attributes[i].location;
-            }
-            return -1;
-        }
-
-        internal void ApplySamplerTextureUnits(WebGLProgram program)
-        {
-            var GL = GraphicsDevice.Strategy.CurrentContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
-
-            // Assign the texture unit index to the sampler uniforms.
-            foreach (var sampler in Samplers)
-            {
-                var loc = GL.GetUniformLocation(program, sampler.name);
-                GraphicsExtensions.CheckGLError();
-                if (loc != null)
-                {
-                    GL.Uniform1i(loc, sampler.textureSlot);
-                    GraphicsExtensions.CheckGLError();
-                }
-            }
         }
 
         private void PlatformGraphicsDeviceResetting()
         {
-            throw new NotImplementedException();
         }
     }
 }
