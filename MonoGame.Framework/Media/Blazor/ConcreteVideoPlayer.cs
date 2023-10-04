d3240f0ec3cb2993fc3d45baa6ffa101357efb5c
Nick Kastellanos <nkastellanos@gmail.com>
2023-10-04 17:58:39 +0300

    implement VideoPlayer

--- MonoGame.Framework/Media/Blazor/ConcreteVideoPlayer.cs
+++ MonoGame.Framework/Media/Blazor/ConcreteVideoPlayer.cs
@@ -8,12 +8,21 @@ using System;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Platform;
+using Microsoft.Xna.Platform.Graphics;
+using nkast.Wasm.Canvas.WebGL;
+using WasmDom = nkast.Wasm.Dom;
 
 
 namespace Microsoft.Xna.Platform.Media
 {
     internal sealed class ConcreteVideoPlayerStrategy : VideoPlayerStrategy
     {
+        private WasmDom.Video _player;
+
+        private bool _isPlaying = false;
+        private bool _frameAvailable;
+        private Texture2D _lastFrame;
+
         public override MediaState State
         {
             get { return base.State; }
@@ -23,7 +32,12 @@ namespace Microsoft.Xna.Platform.Media
         public override bool IsLooped
         {
             get { return base.IsLooped; }
-            set { base.IsLooped = value; }
+            set
+            {
+                base.IsLooped = value;
+                _player.Loop = value;
+
+            }
         }
 
         public override bool IsMuted
@@ -32,7 +46,7 @@ namespace Microsoft.Xna.Platform.Media
             set
             {
                 base.IsMuted = value;
-                throw new NotImplementedException();
+                _player.Muted = value;
             }
         }
 
@@ -47,20 +61,62 @@ namespace Microsoft.Xna.Platform.Media
             set
             {
                 base.Volume = value;
-                if (base.Video != null)
-                    PlatformSetVolume();
+                PlatformSetVolume();
             }
         }
 
         internal ConcreteVideoPlayerStrategy()
         {
-            
+            _player = new WasmDom.Video();
+            _player.OnPlaying += Player_OnPlaying;
+            _player.OnTimeUpdate += Player_OnTimeUpdate;
+            _player.OnEnded += Player_OnEnded;
+
+            return;
         }
+
 
         public override Texture2D PlatformGetTexture()
         {
-            throw new NotImplementedException();
+            if (_lastFrame != null)
+            {
+                if (_lastFrame.Width != base.Video.Width || _lastFrame.Height != base.Video.Height)
+                {
+                    _lastFrame.Dispose();
+                    _lastFrame = null;
+                }
+            }
+            if (_lastFrame == null)
+                _lastFrame = new Texture2D(base.Video.GraphicsDevice, base.Video.Width, base.Video.Height, false, SurfaceFormat.Color);
+
+            if (_isPlaying && _frameAvailable)
+            {
+                var GL = base.Video.GraphicsDevice.Strategy.CurrentContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
+
+                ConcreteTexture2D texture2D = _lastFrame.GetTextureStrategy<ConcreteTexture2D>();
+
+                System.Diagnostics.Debug.Assert(texture2D._glTexture != null);
+                texture2D.GraphicsDevice.CurrentContext.Textures.Strategy.Dirty(0);
+                GL.ActiveTexture(WebGLTextureUnit.TEXTURE0 + 0);
+                GraphicsExtensions.CheckGLError();
+                GL.BindTexture(WebGLTextureTarget.TEXTURE_2D, texture2D._glTexture);
+                GraphicsExtensions.CheckGLError();
+
+                GL.PixelStore(WebGLPixelParameter.UNPACK_ALIGNMENT, Math.Min(texture2D.Format.GetSize(), 8));
+                GraphicsExtensions.CheckGLError();
+
+                GL.TexImage2D(WebGLTextureTarget.TEXTURE_2D, 0,
+                    texture2D._glInternalFormat, texture2D._glFormat, texture2D._glType,
+                    _player);
+                GraphicsExtensions.CheckGLError();
+
+                //GL.Finish();
+                //GraphicsExtensions.CheckGLError();
+            }
+
+            return _lastFrame;
         }
+
 
         protected override void PlatformUpdateState(ref MediaState state)
         {
@@ -68,35 +124,80 @@ namespace Microsoft.Xna.Platform.Media
 
         public override void PlatformPlay(Video video)
         {
+            _player.Pause();
+            _player.Src = "";
+
+            _isPlaying = false;
+            _frameAvailable = false;
+
             base.Video = video;
+
+            ConcreteVideoStrategy videoStrategy = (ConcreteVideoStrategy)video.Strategy;
+
+            _player.Src = videoStrategy.FileName;
+            _player.Load();
+            _player.Play();
 
             State = MediaState.Playing;
         }
 
         public override void PlatformPause()
         {
+            _player.Pause();
+            _isPlaying = false;
+            _frameAvailable = false;
+
             State = MediaState.Paused;
         }
 
         public override void PlatformResume()
         {
+            _player.Play();
+
             State = MediaState.Playing;
         }
         
         public override void PlatformStop()
         {
+            _player.Pause();
+            _player.Src = "";
+
+            _isPlaying = false;
+            _frameAvailable = false;
+
             State = MediaState.Stopped;
         }
 
         private void PlatformSetVolume()
         {
-            throw new NotImplementedException();
+            _player.Volume = Volume;
+        }
+
+        private void Player_OnPlaying(object sender, EventArgs e)
+        {
+            _isPlaying = true;
+        }
+
+        private void Player_OnTimeUpdate(object sender, EventArgs e)
+        {
+            _frameAvailable = true;
+        }
+
+        private void Player_OnEnded(object sender, EventArgs e)
+        {
+            _isPlaying = false;
+            _frameAvailable = false;
+
+            State = MediaState.Stopped;
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
+                _player.OnPlaying -= Player_OnPlaying;
+                _player.OnTimeUpdate -= Player_OnTimeUpdate;
+                _player.OnEnded -= Player_OnEnded;
             }
 
             base.Dispose(disposing);
