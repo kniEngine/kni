46cfc98f13a3269571ff06b255a0d598208a22d0
Nick Kastellanos <nkastellanos@gmail.com>
2023-10-25 15:39:59 +0300

    Android _glSharedContext

--- MonoGame.Framework/Graphics/.GL.Android/ConcreteGraphicsContext.Android.cs
+++ MonoGame.Framework/Graphics/.GL.Android/ConcreteGraphicsContext.Android.cs
@@ -3,7 +3,11 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using Javax.Microedition.Khronos.Egl;
+using Android.Runtime;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Platform;
 
 
 namespace Microsoft.Xna.Platform.Graphics
@@ -11,11 +15,20 @@ namespace Microsoft.Xna.Platform.Graphics
     internal sealed class ConcreteGraphicsContext : ConcreteGraphicsContextGL
     {
         private int _glContextCurrentThreadId = -1;
+        EGLContext _glSharedContext;
 
         internal ConcreteGraphicsContext(GraphicsContext context)
             : base(context)
         {
             _glContextCurrentThreadId = Thread.CurrentThread.ManagedThreadId;
+
+            AndroidGameWindow gameWindow = AndroidGameWindow.FromHandle(context.DeviceStrategy.PresentationParameters.DeviceWindowHandle);
+            ISurfaceView view = gameWindow.GameView;
+
+            int[] attribs = view.GLesVersion.GetAttributes();
+            _glSharedContext = view.Egl.EglCreateContext(EGL10.EglNoDisplay, view.EglConfig, view.EglContext, attribs);
+            if (_glSharedContext != null && _glSharedContext != EGL10.EglNoContext)
+                    throw new Exception("Could not create _glSharedContext");
         }
 
         public override void BindDisposeContext()
@@ -23,11 +36,23 @@ namespace Microsoft.Xna.Platform.Graphics
             if (Thread.CurrentThread.ManagedThreadId == _glContextCurrentThreadId)
                 return;
 
-            throw new InvalidOperationException("Operation not called on UI thread.");
+            AndroidGameWindow gameWindow = AndroidGameWindow.FromHandle(Context.DeviceStrategy.PresentationParameters.DeviceWindowHandle);
+            ISurfaceView view = gameWindow.GameView;
+
+            if (!view.Egl.EglMakeCurrent(view.EglDisplay, EGL10.EglNoSurface, EGL10.EglNoSurface, _glSharedContext))
+                throw new Exception("Could not Bind DisposeContext");
         }
 
         public override void UnbindDisposeContext()
         {
+            if (Thread.CurrentThread.ManagedThreadId == _glContextCurrentThreadId)
+                return;
+
+            AndroidGameWindow gameWindow = AndroidGameWindow.FromHandle(Context.DeviceStrategy.PresentationParameters.DeviceWindowHandle);
+            ISurfaceView view = gameWindow.GameView;
+
+            if (!view.Egl.EglMakeCurrent(view.EglDisplay, EGL10.EglNoSurface, EGL10.EglNoSurface, EGL10.EglNoContext))
+                throw new Exception("Could not Unbind DisposeContext");
         }
 
 
@@ -38,6 +63,12 @@ namespace Microsoft.Xna.Platform.Graphics
                 ThrowIfDisposed();
 
             }
+
+            if (_glSharedContext != null)
+            {
+                //_egl.EglDestroyContext(_eglDisplay, _glSharedContext);
+            }
+            _glSharedContext = null;
 
             base.Dispose(disposing);
         }
