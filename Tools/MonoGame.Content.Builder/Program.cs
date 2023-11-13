57e6d79f1fe5f91129b22148ead0bd02a403d2da
Nick Kastellanos <nkastellanos@gmail.com>
2023-11-13 17:45:18 +0200

    content builder Time formatting

--- Tools/MonoGame.Content.Builder/Program.cs
+++ Tools/MonoGame.Content.Builder/Program.cs
@@ -46,11 +46,11 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Builder
             if (!content.Quiet)
             {
                 Console.WriteLine("\nBuild {0} succeeded, {1} failed.\n", content.SuccessCount, content.ErrorCount);
-                Console.WriteLine("Time elapsed {0:hh\\:mm\\:ss\\.ff}.", DateTime.Now - buildStarted);
+                Console.WriteLine("Time elapsed {0:hh\\:mm\\:ss\\.fff}.", DateTime.Now - buildStarted);
             }
             else
             {
-                Console.WriteLine("KNI content pipeline builder: {0} succeeded, {1} failed, took {2:hh\\:mm\\:ss\\.ff}.", content.SuccessCount, content.ErrorCount, DateTime.Now - buildStarted);
+                Console.WriteLine("KNI content pipeline builder: {0} succeeded, {1} failed, took {2:hh\\:mm\\:ss\\.fff}.", content.SuccessCount, content.ErrorCount, DateTime.Now - buildStarted);
             }
 
             // Return the error count.
