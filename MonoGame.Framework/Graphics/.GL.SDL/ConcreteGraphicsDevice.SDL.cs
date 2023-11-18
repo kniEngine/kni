69878f69efc8d91ab0a797ac5b4ad175cacd9f85
Nick Kastellanos <nkastellanos@gmail.com>
2023-11-18 15:17:20 +0200

    split PlatformSetup

--- MonoGame.Framework/Graphics/.GL.SDL/ConcreteGraphicsDevice.SDL.cs
+++ MonoGame.Framework/Graphics/.GL.SDL/ConcreteGraphicsDevice.SDL.cs
@@ -68,6 +68,54 @@ namespace Microsoft.Xna.Platform.Graphics
         }
 
 
+        internal void PlatformSetup()
+        {
+            _mainContext = new GraphicsContext(this);
+            var GL = _mainContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
+
+            // try getting the context version
+            // GL_MAJOR_VERSION and GL_MINOR_VERSION are GL 3.0+ only, so we need to rely on GL_VERSION string
+            try
+            {
+                string version = GL.GetString(StringName.Version);
+                if (string.IsNullOrEmpty(version))
+                    throw new NoSuitableGraphicsDeviceException("Unable to retrieve OpenGL version");
+
+                // for OpenGL, the GL_VERSION string always starts with the version number in the "major.minor" format,
+                // optionally followed by multiple vendor specific characters
+                // for GLES, the GL_VERSION string is formatted as:
+                //     OpenGL<space>ES<space><version number><space><vendor-specific information>
+#if GLES
+                if (version.StartsWith("OpenGL ES "))
+                    version =  version.Split(' ')[2];
+                else // if it fails, we assume to be on a 1.1 context
+                    version = "1.1";
+#endif
+                _glMajorVersion = Convert.ToInt32(version.Substring(0, 1));
+                _glMinorVersion = Convert.ToInt32(version.Substring(2, 1));
+            }
+            catch (FormatException)
+            {
+                // if it fails, we assume to be on a 1.1 context
+                _glMajorVersion = 1;
+                _glMinorVersion = 1;
+            }
+
+            _capabilities = new ConcreteGraphicsCapabilities();
+            ((ConcreteGraphicsCapabilities)_capabilities).PlatformInitialize(this, _glMajorVersion, _glMinorVersion);
+
+
+#if DESKTOPGL
+            // Initialize draw buffer attachment array
+            _mainContext.Strategy.ToConcrete<ConcreteGraphicsContext>()._drawBuffers = new DrawBuffersEnum[((ConcreteGraphicsCapabilities)this.Capabilities).MaxDrawBuffers];
+            for (int i = 0; i < _mainContext.Strategy.ToConcrete<ConcreteGraphicsContext>()._drawBuffers.Length; i++)
+                _mainContext.Strategy.ToConcrete<ConcreteGraphicsContext>()._drawBuffers[i] = (DrawBuffersEnum)(DrawBuffersEnum.ColorAttachment0 + i);
+#endif
+
+            _mainContext.Strategy.ToConcrete<ConcreteGraphicsContext>()._newEnabledVertexAttributes = new bool[this.Capabilities.MaxVertexBufferSlots];
+        }
+
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
