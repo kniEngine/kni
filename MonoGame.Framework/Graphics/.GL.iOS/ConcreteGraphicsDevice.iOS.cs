837feb3a0f64b4e5c142b79ee7e7cd0d0262bef2
Nikos Kastellanos <nkastellanos@gmail.com>
2023-11-18 15:35:51 +0200

    refactor PlatformSetup (#897)
    * split PlatformSetup
    
    * remove contitions
    
    * normalize whitespaces

--- MonoGame.Framework/Graphics/.GL.iOS/ConcreteGraphicsDevice.iOS.cs
+++ MonoGame.Framework/Graphics/.GL.iOS/ConcreteGraphicsDevice.iOS.cs
@@ -28,6 +28,43 @@ namespace Microsoft.Xna.Platform.Graphics
         }
 
 
+        internal void PlatformSetup()
+        {
+            _mainContext = new GraphicsContext(this);
+            var GL = _mainContext.Strategy.ToConcrete<ConcreteGraphicsContext>().GL;
+
+            // try getting the context version
+            // GL_MAJOR_VERSION and GL_MINOR_VERSION are GL 3.0+ only, so we need to rely on GL_VERSION string
+            try
+            {
+                string version = GL.GetString(StringName.Version);
+                if (string.IsNullOrEmpty(version))
+                    throw new NoSuitableGraphicsDeviceException("Unable to retrieve OpenGL version");
+
+                // for GLES, the GL_VERSION string is formatted as:
+                //     OpenGL<space>ES<space><version number><space><vendor-specific information>
+                if (version.StartsWith("OpenGL ES "))
+                    version =  version.Split(' ')[2];
+                else // if it fails, we assume to be on a 1.1 context
+                    version = "1.1";
+
+                _glMajorVersion = Convert.ToInt32(version.Substring(0, 1));
+                _glMinorVersion = Convert.ToInt32(version.Substring(2, 1));
+            }
+            catch (FormatException)
+            {
+                // if it fails, we assume to be on a 1.1 context
+                _glMajorVersion = 1;
+                _glMinorVersion = 1;
+            }
+
+            _capabilities = new ConcreteGraphicsCapabilities();
+            ((ConcreteGraphicsCapabilities)_capabilities).PlatformInitialize(this, _glMajorVersion, _glMinorVersion);
+
+            _mainContext.Strategy.ToConcrete<ConcreteGraphicsContext>()._newEnabledVertexAttributes = new bool[this.Capabilities.MaxVertexBufferSlots];
+        }
+
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
