58bd2562632730e7f48bb46c987f2a7f66a8de52
Nikos Kastellanos <nkastellanos@gmail.com>
2023-11-21 17:39:16 +0200

    update Templates (#907)
    * fix UAP TemplateID
    
    * WindowsDX net4
    
    * uap xaml TemplateIcon
    
    * move UAP Properties
    
    * tabs
    
    * move UAP Assets
    
    * rename UAP folders

--- Templates/VisualStudio2022/ProjectTemplates/Multiplatform.NetFramework/UWPCore/Properties/AssemblyInfo.cs
+++ Templates/VisualStudio2022/ProjectTemplates/Multiplatform.NetFramework/UWPCore/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("$projectname$")]
-[assembly: AssemblyProduct("$ext_safeprojectname$")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("$registeredorganization$")]
+[assembly: AssemblyProduct("$ext_safeprojectname$")]
 [assembly: AssemblyCopyright("Copyright © $registeredorganization$ $year$")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
