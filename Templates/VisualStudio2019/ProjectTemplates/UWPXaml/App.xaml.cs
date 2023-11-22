32dfb3a820ebe9beddba4495a6ffad84d7e56df2
Nick Kastellanos <nkastellanos@gmail.com>
2023-11-22 14:05:46 +0200

    check PrelaunchActivated

--- Templates/VisualStudio2019/ProjectTemplates/UWPXaml/App.xaml.cs
+++ Templates/VisualStudio2019/ProjectTemplates/UWPXaml/App.xaml.cs
@@ -93,16 +93,19 @@ namespace $safeprojectname$
                 // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
-
-            if (rootFrame.Content == null)
+            
+            if (e.PrelaunchActivated == false)
             {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Ensure the current window is active
+                Window.Current.Activate();
             }
-            // Ensure the current window is active
-            Window.Current.Activate();
         }
 
         /// <summary>
--- Templates/VisualStudio2022/ProjectTemplates/UWPXaml.UAP/App.xaml.cs
+++ Templates/VisualStudio2022/ProjectTemplates/UWPXaml.UAP/App.xaml.cs
@@ -94,15 +94,18 @@ namespace $safeprojectname$
                 Window.Current.Content = rootFrame;
             }
 
-            if (rootFrame.Content == null)
+            if (e.PrelaunchActivated == false)
             {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Ensure the current window is active
+                Window.Current.Activate();
             }
-            // Ensure the current window is active
-            Window.Current.Activate();
         }
 
         /// <summary>
