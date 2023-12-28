6609b5b8fd7517cd91fa1ef91d56e66482ee6720
Nick Kastellanos <nkastellanos@gmail.com>
2023-12-28 06:47:48 +0200

    GraphicsFactory

---
+++ MonoGame.Framework/Graphics/.BlazorGL/ConcreteGraphicsFactory.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (C)2023 Nick Kastellanos
+
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Microsoft.Xna.Platform.Graphics
+{
+    public sealed class ConcreteGraphicsFactory : GraphicsFactory
+    {
+        internal override GraphicsAdaptersProviderStrategy CreateGraphicsAdaptersProviderStrategy()
+        {
+            return new ConcreteGraphicsAdaptersProvider();
+        }
+
+        internal override GraphicsDeviceStrategy CreateGraphicsDeviceStrategy(GraphicsDevice device, GraphicsAdapter adapter, GraphicsProfile graphicsProfile, bool preferHalfPixelOffset, PresentationParameters presentationParameters)
+        {
+            return new ConcreteGraphicsDevice(device, adapter, graphicsProfile, preferHalfPixelOffset, presentationParameters);
+        }
+    }
+}
---
+++ MonoGame.Framework/Graphics/GraphicsFactory.cs
@@ -0,0 +1,55 @@
+// Copyright (C)2023 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Microsoft.Xna.Platform.Graphics
+{
+    public abstract class GraphicsFactory
+    {
+        private volatile static GraphicsFactory _current;
+
+        internal static GraphicsFactory Current
+        {
+            get
+            {
+                GraphicsFactory current = _current;
+                if (current != null)
+                    return current;
+
+                GraphicsFactory graphicsFactory = CreateGraphicsFactory();
+                GraphicsFactory.RegisterGraphicsFactory(graphicsFactory);
+
+                return _current;
+            }
+        }
+
+        private static GraphicsFactory CreateGraphicsFactory()
+        {
+            return new ConcreteGraphicsFactory();
+        }
+
+        public static void RegisterGraphicsFactory(GraphicsFactory graphicsFactory)
+        {
+            lock (typeof(GraphicsFactory))
+            {
+                if (_current == null)
+                {
+                    if (graphicsFactory == null)
+                        throw new NullReferenceException("graphicsFactory");
+                    _current = graphicsFactory;
+                }
+                else
+                {
+                    throw new InvalidOperationException("GraphicsFactory allready registered.");
+                }
+            }
+        }
+
+        internal abstract GraphicsAdaptersProviderStrategy CreateGraphicsAdaptersProviderStrategy();
+        internal abstract GraphicsDeviceStrategy CreateGraphicsDeviceStrategy(GraphicsDevice device, GraphicsAdapter adapter, GraphicsProfile graphicsProfile, bool preferHalfPixelOffset, PresentationParameters presentationParameters);
+    }
+
+}
+
