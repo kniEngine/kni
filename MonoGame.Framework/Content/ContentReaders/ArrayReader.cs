371b25e8b7c4a8d6c0c62121fdd3aa6c4c6bcf14
Nick Kastellanos <nkastellanos@gmail.com>
2024-01-22 13:22:38 +0200

    Content.Utilities.ReflectionHelpers and FileHelpers

--- MonoGame.Framework/Content/ContentReaders/ArrayReader.cs
+++ MonoGame.Framework/Content/ContentReaders/ArrayReader.cs
@@ -3,7 +3,7 @@
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
-using MonoGame.Framework.Utilities;
+using Microsoft.Xna.Platform.Content.Utilities;
 
 namespace Microsoft.Xna.Framework.Content
 {
