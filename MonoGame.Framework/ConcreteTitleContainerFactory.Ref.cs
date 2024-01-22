1175e0e10bc23e95ada255ca910b4b328acb1e03
Nikos Kastellanos <nkastellanos@gmail.com>
2024-01-22 19:57:32 +0200

    TitleContainerFactory (#1098)

---
+++ MonoGame.Framework/ConcreteTitleContainerFactory.Ref.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (C)2024 Nick Kastellanos
+
+using Microsoft.Xna.Framework;
+
+namespace Microsoft.Xna.Platform
+{
+    public sealed class ConcreteTitleContainerFactory : TitleContainerFactory
+    {
+        public override TitleContainerStrategy CreateTitleContainerStrategy()
+        {
+            return new ConcreteTitleContainer();
+        }
+    }
+}
