578a3596a6ff440d44a88300a6de80d86d3ac911
Nikos Kastellanos <nkastellanos@gmail.com>
2024-01-31 17:45:20 +0200

    Builder 3.10.9001-rc (#1140)

--- NuGetPackages/RegisterLocalNuget.bat
+++ NuGetPackages/RegisterLocalNuget.bat
@@ -7,7 +7,7 @@
 "C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Content.Pipeline.Graphics 3.10.9001 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
 "C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Content.Pipeline.Media    3.10.9001 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
 
-"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Content.Pipeline.Builder 3.10.9001 -Source "C:\Users\usename\.nuget\localPackages"   -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Content.Pipeline.Builder 3.10.9001-rc -Source "C:\Users\usename\.nuget\localPackages"   -NonInteractive
 
 "C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Blazor                    3.10.9001 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
 "C:\Program Files (x86)\nuget\nuget.exe" delete MonoGame.Framework.WindowsDX.9000             3.10.9001 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
@@ -27,7 +27,7 @@
 "C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Graphics.3.10.9001.nupkg -Source "C:\Users\usename\.nuget\localPackages"
 "C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Media.3.10.9001.nupkg    -Source "C:\Users\usename\.nuget\localPackages"
 
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Builder.3.10.9001.nupkg -Source "C:\Users\usename\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Builder.3.10.9001-rc.nupkg -Source "C:\Users\usename\.nuget\localPackages"
 
 "C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Blazor.3.10.9001.nupkg                    -Source "C:\Users\usename\.nuget\localPackages"
 "C:\Program Files (x86)\nuget\nuget.exe" add output\MonoGame.Framework.WindowsDX.9000.3.10.9001.nupkg             -Source "C:\Users\usename\.nuget\localPackages"
