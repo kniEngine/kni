45c7a287d8bfb4753cb62d3b78278e010a3f608c
Nikos Kastellanos <nkastellanos@gmail.com>
2024-01-31 05:46:36 +0200

    Model whitespace (#1135)

--- MonoGame.Framework/Graphics/ModelBone.cs
+++ MonoGame.Framework/Graphics/ModelBone.cs
@@ -1,85 +1,85 @@
-﻿using Microsoft.Xna.Framework;
-using System;
+﻿using System;
+using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
-	// Summary:
-	//     Represents bone data for a model. Reference page contains links to related
-	//     conceptual articles.
-	public sealed class ModelBone
-	{
-		private List<ModelBone> children = new List<ModelBone>();
-		
-		private List<ModelMesh> meshes = new List<ModelMesh>();
+    // Summary:
+    //     Represents bone data for a model. Reference page contains links to related
+    //     conceptual articles.
+    public sealed class ModelBone
+    {
+        private List<ModelBone> _children = new List<ModelBone>();
+        
+        private List<ModelMesh> _meshes = new List<ModelMesh>();
 
-		// Summary:
-		//     Gets a collection of bones that are children of this bone.
-		public ModelBoneCollection Children { get; private set; }
-		//
-		// Summary:
-		//     Gets the index of this bone in the Bones collection.
-		public int Index { get; internal set; }
-		//
-		// Summary:
-		//     Gets the name of this bone.
-		public string Name { get; internal set; }
-		//
-		// Summary:
-		//     Gets the parent of this bone.
-		public ModelBone Parent { get; internal set; }
-		//
-		// Summary:
-		//     Gets or sets the matrix used to transform this bone relative to its parent
-		//     bone.
-		internal Matrix transform;
-		public Matrix Transform 
-		{ 
-			get { return this.transform; } 
-			set { this.transform = value; }
-		}
-		
-		internal ModelBone()	
-		{
-			Children = new ModelBoneCollection(new List<ModelBone>());
-		}
-		
-		internal void AddMesh(ModelMesh mesh)
-		{
-			meshes.Add(mesh);
-		}
+        // Summary:
+        //     Gets a collection of bones that are children of this bone.
+        public ModelBoneCollection Children { get; private set; }
+        //
+        // Summary:
+        //     Gets the index of this bone in the Bones collection.
+        public int Index { get; internal set; }
+        //
+        // Summary:
+        //     Gets the name of this bone.
+        public string Name { get; internal set; }
+        //
+        // Summary:
+        //     Gets the parent of this bone.
+        public ModelBone Parent { get; internal set; }
+        //
+        // Summary:
+        //     Gets or sets the matrix used to transform this bone relative to its parent
+        //     bone.
+        internal Matrix transform;
+        public Matrix Transform 
+        { 
+            get { return this.transform; } 
+            set { this.transform = value; }
+        }
+        
+        internal ModelBone()	
+        {
+            Children = new ModelBoneCollection(new List<ModelBone>());
+        }
+        
+        internal void AddMesh(ModelMesh mesh)
+        {
+            _meshes.Add(mesh);
+        }
 
-		internal void AddChild(ModelBone modelBone)
-		{
-			children.Add(modelBone);
-			Children = new ModelBoneCollection(children);
-		}
-	}
+        internal void AddChild(ModelBone modelBone)
+        {
+            _children.Add(modelBone);
+            Children = new ModelBoneCollection(_children);
+        }
+    }
 
-	//// Summary:
-	////     Represents bone data for a model. Reference page contains links to related
-	////     conceptual articles.
-	//public sealed class ModelBone
-	//{
-	//    // Summary:
-	//    //     Gets a collection of bones that are children of this bone.
-	//    public ModelBoneCollection Children { get { throw new NotImplementedException(); } }
-	//    //
-	//    // Summary:
-	//    //     Gets the index of this bone in the Bones collection.
-	//    public int Index { get { throw new NotImplementedException(); } }
-	//    //
-	//    // Summary:
-	//    //     Gets the name of this bone.
-	//    public string Name { get { throw new NotImplementedException(); } }
-	//    //
-	//    // Summary:
-	//    //     Gets the parent of this bone.
-	//    public ModelBone Parent { get { throw new NotImplementedException(); } }
-	//    //
-	//    // Summary:
-	//    //     Gets or sets the matrix used to transform this bone relative to its parent
-	//    //     bone.
-	//    public Matrix Transform { get { throw new NotImplementedException(); } set { throw new NotImplementedException(); } }
-	//}
+    //// Summary:
+    ////     Represents bone data for a model. Reference page contains links to related
+    ////     conceptual articles.
+    //public sealed class ModelBone
+    //{
+    //    // Summary:
+    //    //     Gets a collection of bones that are children of this bone.
+    //    public ModelBoneCollection Children { get { throw new NotImplementedException(); } }
+    //    //
+    //    // Summary:
+    //    //     Gets the index of this bone in the Bones collection.
+    //    public int Index { get { throw new NotImplementedException(); } }
+    //    //
+    //    // Summary:
+    //    //     Gets the name of this bone.
+    //    public string Name { get { throw new NotImplementedException(); } }
+    //    //
+    //    // Summary:
+    //    //     Gets the parent of this bone.
+    //    public ModelBone Parent { get { throw new NotImplementedException(); } }
+    //    //
+    //    // Summary:
+    //    //     Gets or sets the matrix used to transform this bone relative to its parent
+    //    //     bone.
+    //    public Matrix Transform { get { throw new NotImplementedException(); } set { throw new NotImplementedException(); } }
+    //}
 }
--- MonoGame.Framework/Graphics/ModelEffectCollection.cs
+++ MonoGame.Framework/Graphics/ModelEffectCollection.cs
@@ -5,84 +5,84 @@ using System.Collections.ObjectModel;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
-	// Summary:
-	//     Represents a collection of effects associated with a model.
-	public sealed class ModelEffectCollection : ReadOnlyCollection<Effect>
-	{
-		internal ModelEffectCollection(IList<Effect> list)
-			: base(list)
-		{
+    // Summary:
+    //     Represents a collection of effects associated with a model.
+    public sealed class ModelEffectCollection : ReadOnlyCollection<Effect>
+    {
+        internal ModelEffectCollection(IList<Effect> list)
+            : base(list)
+        {
 
-		}
+        }
 
-	    internal ModelEffectCollection() : base(new List<Effect>())
-	    {
-	    }
-		
-		//ModelMeshPart needs to be able to add to ModelMesh's effects list
-		internal void Add(Effect item)
-		{
-			Items.Add(item);
-		}
-		internal void Remove(Effect item)
-		{
-			Items.Remove(item);
-		}
+        internal ModelEffectCollection() : base(new List<Effect>())
+        {
+        }
+        
+        //ModelMeshPart needs to be able to add to ModelMesh's effects list
+        internal void Add(Effect item)
+        {
+            Items.Add(item);
+        }
+        internal void Remove(Effect item)
+        {
+            Items.Remove(item);
+        }
 
-	    // Summary:
-	    //     Returns a ModelEffectCollection.Enumerator that can iterate through a ModelEffectCollection.
-	    public new ModelEffectCollection.Enumerator GetEnumerator()
-		{
-			return new ModelEffectCollection.Enumerator((List<Effect>)Items);
-		}
+        // Summary:
+        //     Returns a ModelEffectCollection.Enumerator that can iterate through a ModelEffectCollection.
+        public new ModelEffectCollection.Enumerator GetEnumerator()
+        {
+            return new ModelEffectCollection.Enumerator((List<Effect>)Items);
+        }
 
-	    // Summary:
-	    //     Provides the ability to iterate through the bones in an ModelEffectCollection.
-	    public struct Enumerator : IEnumerator<Effect>, IDisposable, IEnumerator
-	    {
-			List<Effect>.Enumerator enumerator;
-            bool disposed;
+        // Summary:
+        //     Provides the ability to iterate through the bones in an ModelEffectCollection.
+        public struct Enumerator : IEnumerator<Effect>, IDisposable, IEnumerator
+        {
+            List<Effect>.Enumerator _enumerator;
+            bool _isDisposed;
 
-			internal Enumerator(List<Effect> list)
-			{
-				enumerator = list.GetEnumerator();
-                disposed = false;
-			}
-
-	        // Summary:
-	        //     Gets the current element in the ModelEffectCollection.
-	        public Effect Current { get { return enumerator.Current; } }
-
-	        // Summary:
-	        //     Immediately releases the unmanaged resources used by this object.
-	        public void Dispose()
+            internal Enumerator(List<Effect> list)
             {
-                if (!disposed)
+                _enumerator = list.GetEnumerator();
+                _isDisposed = false;
+            }
+
+            // Summary:
+            //     Gets the current element in the ModelEffectCollection.
+            public Effect Current { get { return _enumerator.Current; } }
+
+            // Summary:
+            //     Immediately releases the unmanaged resources used by this object.
+            public void Dispose()
+            {
+                if (!_isDisposed)
                 {
-                    enumerator.Dispose();
-                    disposed = true;
+                    _enumerator.Dispose();
+                    _isDisposed = true;
                 }
             }
-	        //
-	        // Summary:
-	        //     Advances the enumerator to the next element of the ModelEffectCollection.
-	        public bool MoveNext() { return enumerator.MoveNext(); }
+            //
+            // Summary:
+            //     Advances the enumerator to the next element of the ModelEffectCollection.
+            public bool MoveNext() { return _enumerator.MoveNext(); }
 
-	        #region IEnumerator Members
+            #region IEnumerator Members
 
-	        object IEnumerator.Current
-	        {
-	            get { return Current; }
-	        }
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
 
-	        void IEnumerator.Reset()
-	        {
-				IEnumerator resetEnumerator = enumerator;
-				resetEnumerator.Reset();
-				enumerator = (List<Effect>.Enumerator)resetEnumerator;
-	        }
+            void IEnumerator.Reset()
+            {
+                IEnumerator resetEnumerator = _enumerator;
+                resetEnumerator.Reset();
+                _enumerator = (List<Effect>.Enumerator)resetEnumerator;
+            }
 
-	        #endregion
-	    }
-	}
+            #endregion
+        }
+    }
 }
