ac6e53ea44e3b5770179e9de7357b2911f8f27ea
Nikos Kastellanos <nkastellanos@gmail.com>
2024-02-10 23:18:43 +0200

    Audio Module (#1176)
    * Xna.Framework.Audio
    
    * Audio templates reference

---
+++ src/Xna.Framework.Audio/Audio/InstancePlayLimitException.cs
@@ -0,0 +1,21 @@
+// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+using System.Runtime.Serialization;
+
+namespace Microsoft.Xna.Framework.Audio
+{
+    /// <summary>
+    /// The exception thrown when the system attempts to play more SoundEffectInstances than allotted.
+    /// </summary>
+    /// <remarks>
+    /// Most platforms have a hard limit on how many sounds can be played simultaneously. This exception is thrown when that limit is exceeded.
+    /// </remarks>
+    [DataContract]
+    public sealed class InstancePlayLimitException : Exception
+    {
+    }
+}
+
