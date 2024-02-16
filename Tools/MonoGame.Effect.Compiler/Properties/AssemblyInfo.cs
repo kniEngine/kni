148fe5cd74881153444debbdade34a04983d7b77
Nikos Kastellanos <nkastellanos@gmail.com>
2024-02-16 03:44:53 +0200

    upgrade Tools projects (#1211)

--- Tools/MonoGame.Effect.Compiler/Properties/AssemblyInfo.cs
+++ Tools/MonoGame.Effect.Compiler/Properties/AssemblyInfo.cs
@@ -17,3 +17,24 @@ using System.Runtime.InteropServices;
 
 // Mark the assembly as CLS compliant so it can be safely used in other .NET languages
 [assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("91c6a368-4017-4be4-babb-39cca4870312")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.10.9001.0")]
+[assembly: AssemblyFileVersion("3.10.9001.0")]
