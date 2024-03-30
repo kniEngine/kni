5639e9cc0fee5a68f60929a96cd7620f7b30bbad
Nikos Kastellanos <nkastellanos@gmail.com>
2024-03-30 09:21:15 +0200

    IMessageBox (#1346)

--- MonoGame.Framework/Input/MessageBox.Default.cs
+++ MonoGame.Framework/Input/MessageBox.Default.cs
@@ -4,14 +4,14 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Xna.Framework.Input
 {
-    public static partial class MessageBox
+    public sealed partial class MessageBox
     {
-        private static Task<int?> PlatformShow(string title, string description, List<string> buttons)
+        private Task<int?> PlatformShow(string title, string description, List<string> buttons)
         {
             throw new NotImplementedException("MessageBox is not implemented on this platform.");
         }
 
-        private static void PlatformCancel(int? result)
+        private void PlatformCancel(int? result)
         {
             throw new NotImplementedException("MessageBox is not implemented on this platform.");
         }
--- MonoGame.Framework/Input/MessageBox.cs
+++ MonoGame.Framework/Input/MessageBox.cs
@@ -2,12 +2,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Platform.Input;
+
+namespace Microsoft.Xna.Platform.Input
+{
+    public interface IMessageBox
+    {
+        bool IsVisible { get; }
+
+        Task<int?> Show(string title, string description, IEnumerable<string> buttons);
+        void Cancel(int? result);
+    }
+}
 
 namespace Microsoft.Xna.Framework.Input
 {
-    public static partial class MessageBox
+
+    public sealed partial class MessageBox : IMessageBox
     {
-        public static bool IsVisible { get; private set; }
+        private static MessageBox _current;
+
+        /// <summary>
+        /// Returns the current Mouse instance.
+        /// </summary> 
+        public static MessageBox Current
+        {
+            get
+            {
+                if (_current != null)
+                    return _current;
+
+                lock (typeof(MessageBox))
+                {
+                    if (_current == null)
+                        _current = new MessageBox();
+
+                    return _current;
+                }
+            }
+        }
+
+
+        public static bool IsVisible
+        {
+            get { return ((IMessageBox)MessageBox.Current).IsVisible; }
+        }
 
         /// <summary>
         /// Displays the message box interface asynchronously.
@@ -24,20 +64,7 @@ namespace Microsoft.Xna.Framework.Input
         /// </example>
         public static async Task<int?> Show(string title, string description, IEnumerable<string> buttons)
         {
-            if (IsVisible)
-                throw new Exception("The function cannot be completed at this time: the MessageBox UI is already active. Wait until MessageBox.IsVisible is false before issuing this call.");
-
-            IsVisible = true;
-
-            var buttonsList = buttons.ToList();
-            if (buttonsList.Count > 3 || buttonsList.Count == 0)
-                throw new ArgumentException("Invalid number of buttons: one to three required", "buttons");
-
-            var result = await PlatformShow(title, description, buttonsList);
-
-            IsVisible = false;
-
-            return result;
+            return await ((IMessageBox)MessageBox.Current).Show(title, description, buttons);
         }
 
         /// <summary>
@@ -54,10 +81,49 @@ namespace Microsoft.Xna.Framework.Input
         /// </example>
         public static void Cancel(int? result)
         {
+            ((IMessageBox)MessageBox.Current).Cancel(result);
+        }
+
+
+        //private MessageBox()
+        //{ 
+        //}
+
+        #region IMessageBox
+
+        private static bool _isVisible;
+
+        bool IMessageBox.IsVisible
+        {
+            get { return _isVisible; }
+        }
+
+        async Task<int?> IMessageBox.Show(string title, string description, IEnumerable<string> buttons)
+        {
+            if (IsVisible)
+                throw new Exception("The function cannot be completed at this time: the MessageBox UI is already active. Wait until MessageBox.IsVisible is false before issuing this call.");
+
+            _isVisible = true;
+
+            List<string> buttonsList = buttons.ToList();
+            if (buttonsList.Count > 3 || buttonsList.Count == 0)
+                throw new ArgumentException("Invalid number of buttons: one to three required", "buttons");
+
+            int? result = await PlatformShow(title, description, buttonsList);
+
+            _isVisible = false;
+
+            return result;
+        }
+
+        void IMessageBox.Cancel(int? result)
+        {
             if (!IsVisible)
                 throw new Exception("The function cannot be completed at this time: the MessageBox UI is not active.");
 
             PlatformCancel(result);
         }
+
+        #endregion IMessageBox
     }
 }
