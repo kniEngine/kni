ec1851bc642992da7d3abffc5bb1d70e0ef79824
Nikos Kastellanos <nkastellanos@gmail.com>
2024-03-31 16:09:50 +0300

    move concrete input strategies (#1360)
    * move Concrete input strategies
    
    * move default Concrete input strategies
    
    move Joystick strategy

---
+++ MonoGame.Framework/Input/.Ref/ConcreteKeyboardInput.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (C)2024 Nick Kastellanos
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace Microsoft.Xna.Platform.Input
+{
+    public sealed class ConcreteKeyboardInput : KeyboardInputStrategy
+    {
+        public override Task<string> PlatformShow(string title, string description, string defaultText, bool usePasswordMode)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override void PlatformCancel(string result)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+    }
+}
---
+++ MonoGame.Framework/Input/.SDL2/ConcreteMessageBox.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (C)2024 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.Xna.Platform.Input
+{
+    public sealed class ConcreteMessageBox : MessageBoxStrategy
+    {
+        public override Task<int?> PlatformShow(string title, string description, List<string> buttons)
+        {
+            throw new NotImplementedException("MessageBox is not implemented on this platform.");
+        }
+
+        public override void PlatformCancel(int? result)
+        {
+            throw new NotImplementedException("MessageBox is not implemented on this platform.");
+        }
+    }
+}
