0a305c6d4b1c401d710d443d07f1b31be4fc1546
Nikos Kastellanos <nkastellanos@gmail.com>
2024-04-01 07:54:01 +0300

    move ConcreteMouseCursor

---
+++ MonoGame.Framework/Input/.UAP/ConcreteMouseCursor.cs
@@ -0,0 +1,37 @@
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+// Copyright (C)2021-2024 Nick Kastellanos
+
+using System;
+using Microsoft.Xna.Framework.Input;
+
+namespace Microsoft.Xna.Platform.Input
+{
+    public sealed class ConcreteMouseCursor : MouseCursorStrategy
+    {
+
+        public ConcreteMouseCursor(MouseCursorStrategy.MouseCursorType cursorType)
+        {
+            this._cursorType = cursorType;
+            this._handle = IntPtr.Zero;
+        }
+
+
+        public ConcreteMouseCursor(byte[] data, int w, int h, int originx, int originy)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        protected override void Dispose(bool dispose)
+        {
+            if (dispose)
+            {
+            }
+
+            base.Dispose(dispose);
+        }
+
+    }
+}
