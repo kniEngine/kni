1accbbf8277e0c8665cb3ba7df3e9deb4d4437c5
Nikos Kastellanos <nkastellanos@gmail.com>
2024-04-06 15:08:34 +0300

    conditional reset WindowHandle

--- MonoGame.Framework/Input/.Blazor/ConcreteMouse.cs
+++ MonoGame.Framework/Input/.Blazor/ConcreteMouse.cs
@@ -19,6 +19,8 @@ namespace Microsoft.Xna.Platform.Input
 
         public override void PlatformSetWindowHandle(IntPtr windowHandle)
         {
+            _wndHandle = windowHandle;
+
             // Unregister old window
             if (_domWindow != null)
             {
