bd82e8115935cc8322077c0a7f9076cbdb2b146f
Nikos Kastellanos <nkastellanos@gmail.com>
2024-04-09 17:34:41 +0300

    call the standard first Update at 0:00

--- MonoGame.Framework/.Android/ConcreteGame.cs
+++ MonoGame.Framework/.Android/ConcreteGame.cs
@@ -252,6 +252,14 @@ namespace Microsoft.Xna.Platform
 
             Game.CallBeginRun();
             base.Timer.Restart();
+
+            // XNA runs one Update even before showing the window
+            // DoUpdate
+            {
+                this.Game.AssertNotDisposed();
+                ((IFrameworkDispatcher)FrameworkDispatcher.Current).Update();
+                this.Game.CallUpdate(new GameTime());
+            }
         }
 
         bool _isReadyToRun = false;
