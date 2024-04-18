481f0cbeb8759e76dc598648b61adc26fe44c990
Nikos Kastellanos <nkastellanos@gmail.com>
2024-04-18 12:00:28 +0300

    seperate NamespaceDoc

---
+++ MonoGame.Framework/NamespaceDoc.cs
@@ -0,0 +1,14 @@
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+namespace Microsoft.Xna.Framework.Input
+{
+    /// <summary>
+    /// Provides functionality to handle input from keyboards, mice, gamepads, etc.
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    class NamespaceDoc
+    {
+    }
+}
