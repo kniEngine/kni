926fe50d73757b1588ea8522b7fef111d5bead9e
Nikos Kastellanos <nkastellanos@gmail.com>
2024-04-26 09:48:27 +0300

    pack Cardboard (#1511)

--- BuildNuget.bat
+++ BuildNuget.bat
@@ -23,6 +23,6 @@ dotnet pack XNA.Framework.Blazor.sln --output NuGetPackages\Output\ /t:Build /p:
 dotnet pack MonoGame.Framework\Xna.Framework.Ref.csproj --output NuGetPackages\Output\ /t:Build /p:Configuration=Release 
 dotnet pack MonoGame.Framework\XNA.Framework.Oculus.OvrDX11.csproj --output NuGetPackages\Output\ /t:Build /p:Configuration=Release 
 
-"C:\Program Files (x86)\NuGet3\nuget.exe" pack NuGetPackages/MonoGame.Framework.Cardboard.nuspec        -OutputDirectory NuGetPackages\Output\  -BasePath .  -Version 3.11.9002.0  -Properties Configuration=Release
+dotnet pack MonoGame.Framework\Xna.Framework.Cardboard.csproj --output NuGetPackages\Output\ /t:Build /p:Configuration=Release
 
 @pause
