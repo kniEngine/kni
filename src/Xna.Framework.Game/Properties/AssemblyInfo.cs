c6c390c64e8c2c647c944d5329610199eff31947
Nikos Kastellanos <nkastellanos@gmail.com>
2024-05-02 08:58:36 +0300

    remove InternalsVisibleTo Tests

--- src/Xna.Framework.Game/Properties/AssemblyInfo.cs
+++ src/Xna.Framework.Game/Properties/AssemblyInfo.cs
@@ -1,6 +1,3 @@
 using System;
-using System.Runtime.CompilerServices;
 
 [assembly:CLSCompliant(true)]
-[assembly:InternalsVisibleTo("Kni.Tests")]
-
--- src/Xna.Framework/Properties/AssemblyInfo.cs
+++ src/Xna.Framework/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Runtime.CompilerServices;
 
 [assembly:CLSCompliant(true)]
-[assembly:InternalsVisibleTo("Kni.Tests")]
 
