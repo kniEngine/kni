a909a2a7bebe3e3b0924b1f83ae970b5eb845ca0
Alex West <alxwest@gmail.com>
2024-05-04 14:14:22 +0100

    OnMouseDown and OnMouseUp changed local variables rather than the class variables as intended. (#1535)

--- MonoGame.Framework/Input/.Blazor/ConcreteMouse.cs
+++ MonoGame.Framework/Input/.Blazor/ConcreteMouse.cs
@@ -93,18 +93,18 @@ namespace Microsoft.Xna.Platform.Input
         {
             _pos.X = x;
             _pos.Y = y;
-            ButtonState _leftButton   = ((buttons & 1) != 0) ? ButtonState.Pressed : ButtonState.Released;
-            ButtonState _rightButton  = ((buttons & 2) != 0) ? ButtonState.Pressed : ButtonState.Released;
-            ButtonState _middleButton = ((buttons & 4) != 0) ? ButtonState.Pressed : ButtonState.Released;
+            _leftButton   = ((buttons & 1) != 0) ? ButtonState.Pressed : ButtonState.Released;
+            _rightButton  = ((buttons & 2) != 0) ? ButtonState.Pressed : ButtonState.Released;
+            _middleButton = ((buttons & 4) != 0) ? ButtonState.Pressed : ButtonState.Released;
         }
 
         private void OnMouseUp(object sender, int x, int y, int buttons)
         {
             _pos.X = x;
             _pos.Y = y;
-            ButtonState _leftButton   = ((buttons & 1) != 0) ? ButtonState.Pressed : ButtonState.Released;
-            ButtonState _rightButton  = ((buttons & 2) != 0) ? ButtonState.Pressed : ButtonState.Released;
-            ButtonState _middleButton = ((buttons & 4) != 0) ? ButtonState.Pressed : ButtonState.Released;
+            _leftButton   = ((buttons & 1) != 0) ? ButtonState.Pressed : ButtonState.Released;
+            _rightButton  = ((buttons & 2) != 0) ? ButtonState.Pressed : ButtonState.Released;
+            _middleButton = ((buttons & 4) != 0) ? ButtonState.Pressed : ButtonState.Released;
         }
 
         public void OnMouseWheel(object sender, int deltaX, int deltaY, int deltaZ, int deltaMode)
