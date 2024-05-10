6b80927623bc91ac47999490624280e266513f29
Nikos Kastellanos <nkastellanos@gmail.com>
2024-05-10 15:57:33 +0300

    copyright

--- src/Xna.Framework.Content.Pipeline.Graphics.MojoProcessor/EffectCompiler/Preprocessor.cs
+++ src/Xna.Framework.Content.Pipeline.Graphics.MojoProcessor/EffectCompiler/Preprocessor.cs
@@ -2,7 +2,7 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
-// Copyright (C)2022 Nick Kastellanos
+// Copyright (C)2022-2024 Nick Kastellanos
 
 using System;
 using System.Collections.Generic;
