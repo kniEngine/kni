ede386354e19da7ebed43190d6652ac072cf1345
Nikos Kastellanos <nkastellanos@gmail.com>
2024-06-10 16:27:13 +0300

    remove unused usings

--- MonoGame.Framework/Input/.UAP/ConcreteGamePadDevice.cs
+++ MonoGame.Framework/Input/.UAP/ConcreteGamePadDevice.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-using Windows.ApplicationModel;
 using WGI = Windows.Gaming.Input;
 
 namespace Microsoft.Xna.Platform.Input
