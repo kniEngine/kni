62ed32fae047058ade9cdc99f882c37ec2c39a4c
Nikos Kastellanos <nkastellanos@gmail.com>
2024-07-15 15:43:57 +0300

    implement Web GamePad

--- MonoGame.Framework/Input/.Blazor/ConcreteGamePad.cs
+++ MonoGame.Framework/Input/.Blazor/ConcreteGamePad.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using nkast.Wasm.Dom;
+using nkast.Wasm.Input;
 
 namespace Microsoft.Xna.Platform.Input
 {
@@ -20,30 +22,220 @@ namespace Microsoft.Xna.Platform.Input
 
     public sealed class ConcreteGamePad : GamePadStrategy
     {
+        const int MaxNumberOfGamePads = 4;
+
+        private readonly BlazorGamePadDevice[] _gamepads = new BlazorGamePadDevice[MaxNumberOfGamePads];
 
         // Default & SDL Xbox Controller dead zones
         // Based on the XInput constants
         public override float LeftThumbDeadZone { get { return 0.24f; } }
         public override float RightThumbDeadZone { get { return 0.265f; } }
 
+        public ConcreteGamePad()
+        {
+            try
+            {
+                Gamepad[] gamepads = Window.Current.Navigator.GetGamepads();
+                for (int i = 0; i < _gamepads.Length && i < gamepads.Length; i++)
+                {
+                    if (_gamepads[i] != null)
+                    {
+                        _gamepads[i] = new BlazorGamePadDevice(gamepads[i].Index);
+                        _gamepads[i].Capabilities = CreateCapabilities(gamepads[i]);
+                    }
+                }
+
+                Window.Current.OnGamepadConnected += Window_OnGamepadConnected;
+                Window.Current.OnGamepadDisconnected += Window_OnGamepadDisconnected;
+            }
+            catch
+            {
+            }
+        }
+
+        internal void Window_OnGamepadConnected(object sender, int index)
+        {
+            _gamepads[index] = new BlazorGamePadDevice(index);
+            Gamepad[] gamepads = Window.Current.Navigator.GetGamepads();
+            _gamepads[index].Capabilities = CreateCapabilities(gamepads[index]);
+        }
+
+        internal void Window_OnGamepadDisconnected(object sender, int index)
+        {
+            BlazorGamePadDevice gamepad = _gamepads[index];
+
+            _gamepads[index] = null;
+        }
+
+
         public override int PlatformGetMaxNumberOfGamePads()
         {
-            throw new NotImplementedException();
+            return MaxNumberOfGamePads;
         }
 
         public override GamePadCapabilities PlatformGetCapabilities(int index)
         {
-            throw new NotImplementedException();
+            BlazorGamePadDevice gamepadDevice = _gamepads[index];
+            if (gamepadDevice == null)
+                return GetDefaultCapabilities();
+
+            return gamepadDevice.Capabilities;
         }
+
 
         public override GamePadState PlatformGetState(int index, GamePadDeadZone leftDeadZoneMode, GamePadDeadZone rightDeadZoneMode)
         {
-            throw new NotImplementedException();
+            BlazorGamePadDevice gamePad = _gamepads[index];
+            GamePadState state = new GamePadState();
+
+            if (gamePad != null)
+            {
+                Gamepad[] gamepads = Window.Current.Navigator.GetGamepads();
+                Gamepad gamepad = gamepads[gamePad._index];
+
+                if (gamepad.Mapping == "standard")
+                {
+                    state = CreateGamePadState(gamepad, leftDeadZoneMode, rightDeadZoneMode);
+                }
+            }
+            else
+            {
+                state = base.CreateGamePadState(new GamePadThumbSticks(), new GamePadTriggers(), new GamePadButtons(), new GamePadDPad(),
+                        isConnected: false);
+            }
+
+            return state;
         }
 
         public override bool PlatformSetVibration(int index, float leftMotor, float rightMotor, float leftTrigger, float rightTrigger)
         {
             throw new NotImplementedException();
+        }
+
+
+        private GamePadState CreateGamePadState(Gamepad gamepad, GamePadDeadZone leftDeadZoneMode, GamePadDeadZone rightDeadZoneMode)
+        {
+            GamePadState state;
+            GamepadButton[] gpbuttons = gamepad.Buttons;
+            float[] axes = gamepad.Axes;
+
+            Vector2 leftStick  = new Vector2(axes[0], -axes[1]);
+            Vector2 rightStick = new Vector2(axes[2], -axes[3]);
+
+            GamePadThumbSticks thumbSticks = base.CreateGamePadThumbSticks(leftStick, rightStick, leftDeadZoneMode, rightDeadZoneMode);
+
+            GamePadTriggers triggers = new GamePadTriggers(
+                gpbuttons[6].Value, // LeftTrigger
+                gpbuttons[7].Value  // RightTrigger
+            );
+
+            Buttons buttons = (Buttons)0;
+            if (gpbuttons[0].Pressed)
+                buttons |= Buttons.A;
+            if (gpbuttons[1].Pressed)
+                buttons |= Buttons.B;
+            if (gpbuttons[2].Pressed)
+                buttons |= Buttons.X;
+            if (gpbuttons[3].Pressed)
+                buttons |= Buttons.Y;
+            if (gpbuttons[4].Pressed)
+                buttons |= Buttons.LeftShoulder;
+            if (gpbuttons[5].Pressed)
+                buttons |= Buttons.RightShoulder;
+            //if (gpbuttons[6].Pressed)
+            //    buttons |= Buttons.LeftTrigger;
+            //if (gpbuttons[7].Pressed)
+            //    buttons |= Buttons.RightTrigger;
+            if (gpbuttons[8].Pressed)
+                buttons |= Buttons.Back;
+            if (gpbuttons[9].Pressed)
+                buttons |= Buttons.Start;
+            if (gpbuttons[10].Pressed)
+                buttons |= Buttons.LeftStick;
+            if (gpbuttons[11].Pressed)
+                buttons |= Buttons.RightStick;
+            if (gpbuttons[16].Pressed)
+                buttons |= Buttons.BigButton;
+
+            GamePadDPad dPad = new GamePadDPad(
+                (gpbuttons[12].Pressed) ? ButtonState.Pressed : ButtonState.Released, // DPadUp
+                (gpbuttons[13].Pressed) ? ButtonState.Pressed : ButtonState.Released, // DpadDown
+                (gpbuttons[14].Pressed) ? ButtonState.Pressed : ButtonState.Released, // DpadLeft
+                (gpbuttons[15].Pressed) ? ButtonState.Pressed : ButtonState.Released  // DpadRight
+            );
+
+            int packetNumber = gamepad.Timestamp;
+
+
+            state = base.CreateGamePadState(thumbSticks, triggers, new GamePadButtons(buttons), dPad,
+                                                       packetNumber: packetNumber);
+            return state;
+        }
+
+        private GamePadCapabilities CreateCapabilities(Gamepad gamepad)
+        {
+            //--
+            GamePadType gamePadType = GamePadType.Unknown;
+            string displayName = String.Empty;
+            string identifier = String.Empty;
+            bool isConnected = false;
+            Buttons buttons = (Buttons)0;
+            bool hasLeftVibrationMotor = false;
+            bool hasRightVibrationMotor = false;
+            bool hasVoiceSupport = false;
+            //--
+
+            {
+                displayName = gamepad.Id;
+
+                if (gamepad.Mapping == "standard")
+                {
+                    isConnected = true;
+                    gamePadType = GamePadType.GamePad;
+                    buttons |= Buttons.A;
+                    buttons |= Buttons.B;
+                    buttons |= Buttons.X;
+                    buttons |= Buttons.Y;
+                    buttons |= Buttons.LeftShoulder;
+                    buttons |= Buttons.RightShoulder;
+                    buttons |= Buttons.LeftTrigger;
+                    buttons |= Buttons.RightTrigger;
+                    buttons |= Buttons.Back;
+                    buttons |= Buttons.Start;
+                    buttons |= Buttons.LeftStick;
+                    buttons |= Buttons.RightStick;
+                    buttons |= Buttons.DPadUp;
+                    buttons |= Buttons.DPadDown;
+                    buttons |= Buttons.DPadLeft;
+                    buttons |= Buttons.DPadRight;
+                    buttons |= Buttons.BigButton;
+                }
+            }
+
+            return base.CreateGamePadCapabilities(
+                    gamePadType: gamePadType,
+                    displayName: displayName,
+                    identifier: identifier,
+                    isConnected: isConnected,
+                    buttons: buttons,
+                    hasLeftVibrationMotor: hasLeftVibrationMotor,
+                    hasRightVibrationMotor: hasRightVibrationMotor,
+                    hasVoiceSupport: hasVoiceSupport
+                );
+        }
+
+        private GamePadCapabilities GetDefaultCapabilities()
+        {
+            return base.CreateGamePadCapabilities(
+                    gamePadType: GamePadType.Unknown,
+                    displayName: null,
+                    identifier: null,
+                    isConnected: false,
+                    buttons: (Buttons)0,
+                    hasLeftVibrationMotor: false,
+                    hasRightVibrationMotor: false,
+                    hasVoiceSupport: false
+                );
         }
     }
 }
