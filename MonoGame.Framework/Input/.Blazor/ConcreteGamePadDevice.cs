4ec39392b21147ce96b1f7dd454713185bc36dfb
Nikos Kastellanos <nkastellanos@gmail.com>
2024-07-21 20:08:24 +0300

    implement Web GamePad (#1730)

--- MonoGame.Framework/Input/.Blazor/ConcreteGamePadDevice.cs
+++ MonoGame.Framework/Input/.Blazor/ConcreteGamePadDevice.cs
@@ -4,16 +4,18 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using nkast.Wasm.Input;
 
 namespace Microsoft.Xna.Platform.Input
 {
     internal class BlazorGamePadDevice : GamePadDevice
     {
+        public int _index;
 
-        public BlazorGamePadDevice()
+        public BlazorGamePadDevice(int index)
             : base()
         {
-
+            _index = index;
         }
     }
 
