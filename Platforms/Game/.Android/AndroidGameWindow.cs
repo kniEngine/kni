90f12829b4c74560794e7e9f5680751af6550cc5
Nikos Kastellanos <nkastellanos@gmail.com>
2024-09-29 06:56:10 +0300

    refactor RunStep() (#1907)
    * inline ProcessStateExited()
    
    inline ProcessStateExited()
    
    * inline ProcessStateResumed()
    
    * refactor Screen Lock check

--- Platforms/Game/.Android/AndroidGameWindow.cs
+++ Platforms/Game/.Android/AndroidGameWindow.cs
@@ -510,14 +510,30 @@ namespace Microsoft.Xna.Framework
             {
                 case AndroidGameWindow.AppState.Resumed:
                     if (GameView._isAndroidSurfaceAvailable) // do not run game if surface is not available
-                        ProcessStateResumed();
+                    {
+                        _orientationListener.Update();
+
+                        try
+                        {
+                            if (!_screenReceiver.IsScreenLocked) // do not run game if Screen is Locked
+                            {
+                                if (_game != null)
+                                {
+                                    ((IPlatformGame)_game).GetStrategy<ConcreteGame>().OnFrameTick();
+                                }
+                            }
+                        }
+                        catch (OpenGLException ex)
+                        {
+                            Log.Error("AndroidGameWindow", "OpenGL Exception occurred during OnFrameTick {0}", ex.Message);
+                        }
+                    }
                     break;
 
                 case AndroidGameWindow.AppState.Paused:
                     break;
 
                 case AndroidGameWindow.AppState.Exited:
-                    ProcessStateExited();
                     break;
 
                 default:
@@ -525,27 +541,6 @@ namespace Microsoft.Xna.Framework
             }
 
             return;
-        }
-
-        void ProcessStateResumed()
-        {
-            _orientationListener.Update();
-
-            try
-            {
-                if (_game != null && !_screenReceiver.IsScreenLocked)
-                {
-                    ((IPlatformGame)_game).GetStrategy<ConcreteGame>().OnFrameTick();
-                }
-            }
-            catch (OpenGLException ex)
-            {
-                Log.Error("AndroidGameView", "OpenGL Exception occurred during RunIteration {0}", ex.Message);
-            }
-        }
-
-        private void ProcessStateExited()
-        {
         }
 
         private EGLConfig _eglConfig;
