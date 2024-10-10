500fa609fd259a496dc9102482896d07787ef1dd
Nikos Kastellanos <nkastellanos@gmail.com>
2024-10-10 21:29:12 +0300

    SensorStrategy

---
+++ Platforms/Sensors/AccelerometerStrategy.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (C)2024 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Microsoft.Devices.Sensors
+{
+    public class AccelerometerStrategy : SensorStrategy<AccelerometerReading>
+        , IDisposable
+    {
+
+        public AccelerometerStrategy()
+        {
+        }
+
+    }
+}
---
+++ Platforms/Sensors/CompassStrategy.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (C)2024 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Microsoft.Devices.Sensors
+{
+    public class CompassStrategy : SensorStrategy<CompassReading>
+        , IDisposable
+    {
+
+        public CompassStrategy()
+        {
+        }
+
+    }
+}
