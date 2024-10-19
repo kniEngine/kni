31567912fee4c4d2481d2410d0fdb5978b10dd83
Nikos Kastellanos <nkastellanos@gmail.com>
2024-10-20 01:03:08 +0300

    implement UAP Accelerometer

--- Platforms/Devices/.UAP/Sensors/ConcreteAccelerometer.cs
+++ Platforms/Devices/.UAP/Sensors/ConcreteAccelerometer.cs
@@ -4,11 +4,13 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Devices.Sensors;
+using WinSensors = Windows.Devices.Sensors;
 
 namespace Microsoft.Xna.Platform.Devices.Sensors
 {
     internal sealed class ConcreteAccelerometer : AccelerometerStrategy
     {
+        internal WinSensors.Accelerometer _waccelerometer;
 
         public override SensorState State
         {
@@ -25,36 +27,99 @@ namespace Microsoft.Xna.Platform.Devices.Sensors
         public override TimeSpan TimeBetweenUpdates
         {
             get { return base.TimeBetweenUpdates; }
-            set { base.TimeBetweenUpdates = value; }
+            set
+            {
+                base.TimeBetweenUpdates = value;
+                _waccelerometer.ReportInterval = (uint)value.TotalMilliseconds;
+            }
         }
 
         public override AccelerometerReading CurrentValue
         {
-            get { return base.CurrentValue; }
+            get
+            {
+                WinSensors.AccelerometerReading wreading = _waccelerometer.GetCurrentReading();
+                if (wreading == null)
+                {
+                    base.IsDataValid = false;
+                    return new AccelerometerReading();
+                }
+
+                AccelerometerReading reading = base.CreateAccelerometerReading(
+                    acceleration: new Vector3((float)wreading.AccelerationX, (float)wreading.AccelerationY, (float)wreading.AccelerationZ),
+                    // timestamp: war.Timestamp //cause Memory garbage
+                    timestamp: DateTimeOffset.MinValue
+                );
+                base.IsDataValid = true;
+                return reading;
+            }
             set { base.CurrentValue = value; }
         }
 
-
         public ConcreteAccelerometer()
         {
-            base.State = SensorState.NotSupported;
+            _waccelerometer = WinSensors.Accelerometer.GetDefault();
+            if (_waccelerometer == null)
+            {
+                base.State = SensorState.NotSupported;
+                return;
+            }
 
+            base.State = SensorState.Disabled;
         }
 
         public override void Start()
         {
+            if (this.State != SensorState.Ready)
+            {
+                _waccelerometer.ReadingChanged += _waccelerometer_ReadingChanged;
+
+                base.State = SensorState.Ready;
+            }
         }
 
         public override void Stop()
         {
+            if (this.State == SensorState.Ready)
+            {
+                _waccelerometer.ReadingChanged -= _waccelerometer_ReadingChanged;
+
+
+                base.State = SensorState.Disabled;
+            }
         }
 
+        private void _waccelerometer_ReadingChanged(WinSensors.Accelerometer sender, WinSensors.AccelerometerReadingChangedEventArgs args)
+        {
+            WinSensors.AccelerometerReading wreading = args.Reading;
+
+            if (wreading == null)
+            {
+                base.IsDataValid = false;
+                return;
+            }
+
+            AccelerometerReading reading = base.CreateAccelerometerReading(
+                   acceleration: new Vector3((float)wreading.AccelerationX, (float)wreading.AccelerationY, (float)wreading.AccelerationZ),
+                   // timestamp: war.Timestamp //cause Memory garbage
+                   timestamp: DateTimeOffset.MinValue
+               );
+            base.IsDataValid = true;
+
+            base.CurrentValue = reading;
+
+            var eventArgs = new SensorReadingEventArgs<AccelerometerReading>(reading);
+            base.OnCurrentValueChanged(eventArgs);
+        }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
             }
+
+            if (this.State == SensorState.Ready)
+                Stop();
 
             base.Dispose(disposing);
         }
