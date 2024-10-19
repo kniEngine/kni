e26b44113ee64244c6a40d91e32af3e20cb4c949
Nikos Kastellanos <nkastellanos@gmail.com>
2024-10-20 01:14:11 +0300

    implement UAP Accelerometer (#1948)

--- Platforms/Devices/.UAP/Sensors/ConcreteSensorService.cs
+++ Platforms/Devices/.UAP/Sensors/ConcreteSensorService.cs
@@ -3,19 +3,23 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework.Devices.Sensors;
+using WinSensors = Windows.Devices.Sensors;
 
 namespace Microsoft.Xna.Platform.Devices.Sensors
 {
     internal sealed class ConcreteSensorService : SensorServiceStrategy
     {
+        bool _isAccelerometerSupported;
 
         internal ConcreteSensorService()
         {
+            WinSensors.Accelerometer waccelerometer = WinSensors.Accelerometer.GetDefault();
+            _isAccelerometerSupported = (waccelerometer != null);
         }
 
         public override bool PlatformIsAccelerometerSupported()
         {
-            return false;
+            return _isAccelerometerSupported;
         }
 
         public override bool PlatformIsCompassSupported()
