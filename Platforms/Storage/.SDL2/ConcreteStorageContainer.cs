d6dbef622ff1138c48ce0b7df0658f12006c5298
Nikos Kastellanos <nkastellanos@gmail.com>
2024-10-25 01:29:26 +0300

    move concrete files

---
+++ Platforms/Storage/.SDL2/ConcreteStorageContainer.cs
@@ -0,0 +1,290 @@
+﻿// MIT License - Copyright (C) The Mono.Xna Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+// Copyright (C)2024 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Storage;
+
+#if DESKTOPGL
+using MonoGame.Framework.Utilities;
+#endif
+
+#if (UAP || WINUI)
+using System.Linq;
+using Windows.Storage;
+using Windows.Storage.Search;
+#endif
+
+namespace Microsoft.Xna.Platform.Storage
+{
+    //	Implementation on Windows
+    //	
+    //	User storage is in the My Documents folder of the user who is currently logged in, in the SavedGames folder. 
+    //	A subfolder is created for each game according to the titleName passed to the BeginOpenContainer method. 
+    //	When no PlayerIndex is specified, content is saved in the AllPlayers folder. When a PlayerIndex is specified, 
+    //	the content is saved in the Player1, Player2, Player3, or Player4 folder, depending on which PlayerIndex 
+    //	was passed to BeginShowSelector.
+
+    internal class ConcreteStorageContainer : StorageContainerStrategy
+    {
+        public override string DisplayName
+        {
+            get { return base.DisplayName; }
+        }
+
+        public ConcreteStorageContainer(string name, PlayerIndex? playerIndex) : base(name)
+        {
+            string saved = "";
+
+#if DESKTOPGL
+            switch (CurrentPlatform.OS)
+            {
+                case OS.Windows:
+                    {
+                        saved = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                        saved = Path.Combine(saved, "SavedGames");
+                    }
+                    break;
+                case OS.Linux:
+                    {
+                        saved = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
+                        if (String.IsNullOrEmpty(saved))
+                        {
+                            saved = Environment.GetEnvironmentVariable("HOME");
+                            if (!String.IsNullOrEmpty(saved))
+                                saved += "/.local/share";
+                            else
+                                saved = ".";
+                        }
+                    }
+                    break;
+                case OS.MacOSX:
+                    {
+                        saved = Environment.GetEnvironmentVariable("HOME");
+                        if (!String.IsNullOrEmpty(saved))
+                            saved += "/Library/Application Support";
+                        else
+                            saved = ".";
+                    }
+                    break;
+
+                default:
+                    throw new Exception("Unexpected platform.");
+            }               
+#else
+            string root = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saved = Path.Combine(root,"SavedGames");
+#endif
+            _storagePath = Path.Combine(saved, name);
+
+            string playerSave = string.Empty;
+            if (playerIndex.HasValue)
+                playerSave = Path.Combine(_storagePath, "Player" + (int)playerIndex.Value);
+            
+            if (!string.IsNullOrEmpty(playerSave))
+                _storagePath = Path.Combine(_storagePath, "Player" + (int)playerIndex);
+
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            var task = folder.CreateFolderAsync(_storagePath, CreationCollisionOption.OpenIfExists);
+            task.AsTask().Wait();
+#else
+            if (!Directory.Exists(_storagePath))
+                Directory.CreateDirectory(_storagePath);
+#endif
+        }
+
+
+        public override void CreateDirectory(string directory)
+        {
+            // relative so combine with our path
+            string dirPath = Path.Combine(_storagePath, directory);
+
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            var task = folder.CreateFolderAsync(dirPath, CreationCollisionOption.OpenIfExists);
+            task.AsTask().Wait();
+#else
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+#endif
+        }
+
+        public override Stream CreateFile(string file)
+        {
+            // relative so combine with our path
+            string filePath = Path.Combine(_storagePath, file);
+
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            var awaiter = folder.OpenStreamForWriteAsync(filePath, CreationCollisionOption.ReplaceExisting).GetAwaiter();
+            return awaiter.GetResult();
+#else
+            // return A new file with read/write access.
+            return File.Create(filePath);
+#endif
+        }
+
+        public override void DeleteDirectory(string directory)
+        {
+            // relative so combine with our path
+            string dirPath = Path.Combine(_storagePath, directory);
+
+            // Now let's try to delete itd
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            var deleteFolder = folder.GetFolderAsync(dirPath).AsTask().GetAwaiter().GetResult();
+            deleteFolder.DeleteAsync().AsTask().Wait();
+#else
+            Directory.Delete(dirPath);
+#endif
+        }
+
+        public override void DeleteFile(string file)
+        {
+            // relative so combine with our path
+            string filePath = Path.Combine(_storagePath, file);
+
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            StorageFile deleteFile = folder.GetFileAsync(filePath).AsTask().GetAwaiter().GetResult();
+            deleteFile.DeleteAsync().AsTask().Wait();
+#else
+            // Now let's try to delete it
+            File.Delete(filePath);
+#endif
+        }
+
+        public override bool DirectoryExists(string directory)
+        {
+            // relative so combine with our path
+            string dirPath = Path.Combine(_storagePath, directory);
+
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+            try
+            {
+                StorageFolder result = folder.GetFolderAsync(dirPath).GetResults();
+                return result != null;
+            }
+            catch
+            {
+                return false;
+            }
+#else
+            return Directory.Exists(dirPath);
+#endif
+        }
+
+        public override bool FileExists(string file)
+        {
+            // relative so combine with our path
+            string filePath = Path.Combine(_storagePath, file);
+
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            // GetFile returns an exception if the file doesn't exist, so we catch it here and return the boolean.
+            try
+            {
+                StorageFile existsFile = folder.GetFileAsync(filePath).GetAwaiter().GetResult();
+                return existsFile != null;
+            }
+            catch
+            {
+                return false;
+            }
+#else
+            // return A new file with read/write access.
+            return File.Exists(filePath);
+#endif
+        }
+
+        public override string[] GetDirectoryNames()
+        {
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            IReadOnlyList<StorageFolder> results = folder.GetFoldersAsync().AsTask().GetAwaiter().GetResult();
+            return results.Select<StorageFolder, string>(e => e.Name).ToArray();
+#else
+            return Directory.GetDirectories(_storagePath);
+#endif
+        }
+
+        public override string[] GetDirectoryNames(string searchPattern)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetFileNames()
+        {
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            var results = folder.GetFilesAsync().AsTask().GetAwaiter().GetResult();
+            return results.Select<StorageFile, string>(e => e.Name).ToArray();
+#else
+            return Directory.GetFiles(_storagePath);
+#endif
+        }
+
+        public override string[] GetFileNames(string searchPattern)
+        {
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            QueryOptions options = new QueryOptions( CommonFileQuery.DefaultQuery, new [] { searchPattern } );
+            StorageFileQueryResult query = folder.CreateFileQueryWithOptions(options);
+            IReadOnlyList<StorageFile> files = query.GetFilesAsync().AsTask().GetAwaiter().GetResult();
+            return files.Select<StorageFile, string>(e => e.Name).ToArray();
+#else
+            return Directory.GetFiles(_storagePath, searchPattern);
+#endif
+        }
+
+        public override Stream OpenFile(string file, FileMode fileMode, FileAccess fileAccess, FileShare fileShare)
+        {
+            // relative so combine with our path
+            string filePath = Path.Combine(_storagePath, file);
+
+#if (UAP || WINUI)
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            if (fileMode == FileMode.Create || fileMode == FileMode.CreateNew)
+            {
+                return folder.OpenStreamForWriteAsync(filePath, CreationCollisionOption.ReplaceExisting).GetAwaiter().GetResult();
+            }
+            else if (fileMode == FileMode.OpenOrCreate)
+            {
+                if (fileAccess == FileAccess.Read && FileExists(file))
+                    return folder.OpenStreamForReadAsync(filePath).GetAwaiter().GetResult();
+                else
+                {
+                    // Not using OpenStreamForReadAsync because the stream position is placed at the end of the file, instead of the beginning
+                    StorageFile f = folder.CreateFileAsync(filePath, CreationCollisionOption.OpenIfExists).AsTask().GetAwaiter().GetResult();
+                    return f.OpenAsync(FileAccessMode.ReadWrite).AsTask().GetAwaiter().GetResult().AsStream();
+                }
+            }
+            else if (fileMode == FileMode.Truncate)
+            {
+                return folder.OpenStreamForWriteAsync(filePath, CreationCollisionOption.ReplaceExisting).GetAwaiter().GetResult();
+            }
+            else
+            {
+                //if (fileMode == FileMode.Append)
+                // Not using OpenStreamForReadAsync because the stream position is placed at the end of the file, instead of the beginning
+                folder.CreateFileAsync(filePath, CreationCollisionOption.OpenIfExists).AsTask().GetAwaiter().GetResult().OpenAsync(FileAccessMode.ReadWrite).AsTask().GetAwaiter().GetResult().AsStream();
+                StorageFile f = folder.CreateFileAsync(filePath, CreationCollisionOption.OpenIfExists).AsTask().GetAwaiter().GetResult();
+                return f.OpenAsync(FileAccessMode.ReadWrite).AsTask().GetAwaiter().GetResult().AsStream();
+            }
+#else
+            return File.Open(filePath, fileMode, fileAccess, fileShare);
+#endif
+        }
+
+    }
+}
---
+++ Platforms/Storage/.WindowsDX/ConcreteStorageDevice.cs
@@ -0,0 +1,226 @@
+﻿// Copyright (C)2024 Nick Kastellanos
+
+using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Storage;
+
+#if DESKTOPGL
+using MonoGame.Framework.Utilities;
+#endif
+
+#if (UAP || WINUI)
+using Windows.Storage;
+#endif
+
+#if ANDROID || IOS || TVOS || NETFX_CORE
+using System.Threading.Tasks;
+#endif
+
+
+namespace Microsoft.Xna.Platform.Storage
+{
+    internal class ConcreteStorageDevice : StorageDeviceStrategy
+    {
+
+        public override long FreeSpace
+        {
+            get
+            {
+#if (UAP || WINUI)
+                return long.MaxValue;
+#else
+                return new DriveInfo(GetDevicePath).AvailableFreeSpace;
+#endif
+            }
+        }
+
+        public override bool IsConnected
+        {
+            get
+            {
+#if (UAP || WINUI)
+                return true;
+#else
+                return new DriveInfo(GetDevicePath).IsReady;
+#endif
+            }
+        }
+
+        public override long TotalSpace
+        {
+            get
+            {
+#if (UAP || WINUI)
+                return long.MaxValue;
+#else
+                // Not sure if this should be TotalSize or TotalFreeSize
+                return new DriveInfo(GetDevicePath).TotalSize;
+#endif
+            }
+        }
+
+        public override string GetDevicePath
+        {
+            get
+            {
+                // We may not need to store the StorageContainer in the future
+                // when we get DeviceChanged events working.
+                if (_storageContainer == null)
+                {
+    #if (UAP || WINUI)
+                    return ApplicationData.Current.LocalFolder.Path;
+    #elif DESKTOPGL
+                    switch (CurrentPlatform.OS)
+                    {
+                        case OS.Windows:
+                            {
+                                return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                            }
+                        case OS.Linux:
+                            {
+                                string osConfigDir = Environment.GetEnvironmentVariable("XDG_DATA_HOME");
+                                if (String.IsNullOrEmpty(osConfigDir))
+                                {
+                                    osConfigDir = Environment.GetEnvironmentVariable("HOME");
+                                    if (!String.IsNullOrEmpty(osConfigDir))
+                                        osConfigDir += "/.local/share";
+                                    else
+                                        osConfigDir = ".";
+                                }
+                                return osConfigDir;
+                            }
+                        case OS.MacOSX:
+                            {
+                                string osConfigDir = Environment.GetEnvironmentVariable("HOME");
+                                if (!String.IsNullOrEmpty(osConfigDir))
+                                    osConfigDir += "/Library/Application Support";
+                                else
+                                    osConfigDir = ".";
+
+                                return osConfigDir;
+                            }
+                        default:
+                            throw new Exception("Unexpected platform.");
+                    }
+    #else
+                    return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    #endif
+                }
+                else
+                {
+                    return _storageContainer.Strategy._storagePath;
+                }
+            }
+        }
+
+
+        public ConcreteStorageDevice(PlayerIndex? player, int directoryCount) : base(player, directoryCount)
+        {
+        }
+
+
+        public override IAsyncResult BeginOpenContainer(StorageDevice storageDevice, string displayName, AsyncCallback callback, object state)
+        {
+#if ANDROID || IOS || TVOS || NETFX_CORE
+            TaskCompletionSource<StorageContainer> tcs = new TaskCompletionSource<StorageContainer>(state);
+            Task<StorageContainer> task = Task.Run<StorageContainer>(() => Open(storageDevice, displayName));
+            task.ContinueWith((t) =>
+            {
+                // Copy the task result into the returned task.
+                if (t.IsFaulted)
+                    tcs.TrySetException(t.Exception.InnerExceptions);
+                else if (t.IsCanceled)
+                    tcs.TrySetCanceled();
+                else
+                    tcs.TrySetResult(t.Result);
+
+                // Invoke the user callback if necessary.
+                if (callback != null)
+                    callback(tcs.Task);
+            });
+            return tcs.Task;
+#else
+            try
+            {
+                OpenContainerAsynchronous AsynchronousOpen = new OpenContainerAsynchronous(Open);
+#if (UAP || WINUI)
+                _containerDelegate = AsynchronousOpen;
+#endif
+                return AsynchronousOpen.BeginInvoke(storageDevice, displayName, callback, state);
+            }
+            finally
+            {
+            }
+#endif
+        }
+
+        public override StorageContainer EndOpenContainer(IAsyncResult result)
+        {
+#if ANDROID || IOS || TVOS || NETFX_CORE
+            try
+            {
+                return ((Task<StorageContainer>)result).Result;
+            }
+            catch (AggregateException ex)
+            {
+                throw;
+            }
+#else
+            StorageContainer returnValue = null;
+            try
+            {
+#if (UAP || WINUI)
+                // AsyncResult does not exist in WinRT
+                var asyncResult = _containerDelegate as OpenContainerAsynchronous;
+                if (asyncResult != null)
+                {
+                    // Wait for the WaitHandle to become signaled.
+                    result.AsyncWaitHandle.WaitOne();
+
+                    // Call EndInvoke to retrieve the results.
+                    returnValue = asyncResult.EndInvoke(result);
+                }
+                _containerDelegate = null;
+#elif NET4_0_OR_GREATER
+                // Retrieve the delegate.
+                AsyncResult asyncResult = result as AsyncResult;
+                if (asyncResult != null)
+                {
+                    OpenContainerAsynchronous asyncDelegate = asyncResult.AsyncDelegate as OpenContainerAsynchronous;
+
+                    // Wait for the WaitHandle to become signaled.
+                    result.AsyncWaitHandle.WaitOne();
+
+                    // Call EndInvoke to retrieve the results.
+                    if (asyncDelegate != null)
+                        returnValue = asyncDelegate.EndInvoke(result);
+                }
+#else // NET6_0_OR_GREATER
+                throw new NotImplementedException();
+#endif
+            }
+            finally
+            {
+                // Close the wait handle.
+                result.AsyncWaitHandle.Dispose();
+            }
+            
+            return returnValue;
+#endif
+        }
+
+        public override void DeleteContainer(string titleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override StorageContainer Open(StorageDevice storageDevice, string displayName)
+        {
+            StorageContainer storageContainer = new StorageContainer(storageDevice, displayName, _player);
+            _storageContainer = storageContainer;
+
+            return storageContainer;
+        }
+    }
+}
