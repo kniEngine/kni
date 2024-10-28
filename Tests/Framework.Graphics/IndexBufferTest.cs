d07b5fe884b3bca7abd05e25e0a5cb0bc5b051f7
Nikos Kastellanos <nkastellanos@gmail.com>
2024-10-28 18:12:41 +0200

    IndexBuffer struct Test (#1968)

--- Tests/Framework.Graphics/IndexBufferTest.cs
+++ Tests/Framework.Graphics/IndexBufferTest.cs
@@ -73,7 +73,103 @@ namespace Kni.Tests.Graphics
 
             indexBuffer.Dispose();
         }
-        
+
+
+        struct TriangleIndices16
+        {
+            public short A, B, C;
+
+            public TriangleIndices16(short a, short b, short c)
+            {
+                this.A = a;
+                this.B = b;
+                this.C = c;
+            }
+
+            public int VertexStride { get { return (sizeof(short)) * 3; } }
+
+            public override string ToString()
+            {
+                return String.Format("A:{0,4} B:{1,4} C:{2,4}", A, B, C);
+            }
+        }
+
+        [Test]
+        public void ShouldSetAndGetStructData()
+        {
+            short[] savedData = new short[] { 1, 2, 3, 4, 5, 6 };
+            IndexBuffer indexBuffer = new IndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length, BufferUsage.None);
+            indexBuffer.SetData(savedData);
+
+            TriangleIndices16[] readData = new TriangleIndices16[2];
+            indexBuffer.GetData(readData, 0, 2);
+            Assert.AreEqual(savedData[0], readData[0].A);
+            Assert.AreEqual(savedData[1], readData[0].B);
+            Assert.AreEqual(savedData[2], readData[0].C);
+            Assert.AreEqual(savedData[3], readData[1].A);
+            Assert.AreEqual(savedData[4], readData[1].B);
+            Assert.AreEqual(savedData[5], readData[1].C);
+
+            indexBuffer.Dispose();
+        }
+
+        [Test]
+        public void ShouldSetStructAndGetData()
+        {
+            TriangleIndices16[] savedData = new TriangleIndices16[] { new TriangleIndices16(1,2,3), new TriangleIndices16(4, 5, 6) };
+            IndexBuffer indexBuffer = new IndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length*3, BufferUsage.None);
+            indexBuffer.SetData(savedData);
+
+            short[] readData = new short[6];
+            indexBuffer.GetData(readData, 0, 6);
+            Assert.AreEqual(savedData[0].A, readData[0]);
+            Assert.AreEqual(savedData[0].B, readData[1]);
+            Assert.AreEqual(savedData[0].C, readData[2]);
+            Assert.AreEqual(savedData[1].A, readData[3]);
+            Assert.AreEqual(savedData[1].B, readData[4]);
+            Assert.AreEqual(savedData[1].C, readData[5]);
+
+            indexBuffer.Dispose();
+        }
+
+        [Test]
+        public void ShouldDynamicSetAndGetStructData()
+        {
+            short[] savedData = new short[] { 1, 2, 3, 4, 5, 6 };
+            IndexBuffer indexBuffer = new DynamicIndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length, BufferUsage.None);
+            indexBuffer.SetData(savedData);
+
+            TriangleIndices16[] readData = new TriangleIndices16[2];
+            indexBuffer.GetData(readData, 0, 2);
+            Assert.AreEqual(savedData[0], readData[0].A);
+            Assert.AreEqual(savedData[1], readData[0].B);
+            Assert.AreEqual(savedData[2], readData[0].C);
+            Assert.AreEqual(savedData[3], readData[1].A);
+            Assert.AreEqual(savedData[4], readData[1].B);
+            Assert.AreEqual(savedData[5], readData[1].C);
+
+            indexBuffer.Dispose();
+        }
+
+        [Test]
+        public void ShouldDynamicSetStructAndGetData()
+        {
+            TriangleIndices16[] savedData = new TriangleIndices16[] { new TriangleIndices16(1, 2, 3), new TriangleIndices16(4, 5, 6) };
+            IndexBuffer indexBuffer = new DynamicIndexBuffer(gd, IndexElementSize.SixteenBits, savedData.Length*3, BufferUsage.None);
+            indexBuffer.SetData(savedData);
+
+            short[] readData = new short[6];
+            indexBuffer.GetData(readData, 0, 6);
+            Assert.AreEqual(savedData[0].A, readData[0]);
+            Assert.AreEqual(savedData[0].B, readData[1]);
+            Assert.AreEqual(savedData[0].C, readData[2]);
+            Assert.AreEqual(savedData[1].A, readData[3]);
+            Assert.AreEqual(savedData[1].B, readData[4]);
+            Assert.AreEqual(savedData[1].C, readData[5]);
+
+            indexBuffer.Dispose();
+        }
+
         [Test]
         public void NullDeviceShouldThrowArgumentNullException()
         {
