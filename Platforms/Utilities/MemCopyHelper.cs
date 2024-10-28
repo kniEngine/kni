7f313e990b787fbd683b2a5fe04983666bb9fee5
Nikos Kastellanos <nkastellanos@gmail.com>
2024-10-28 19:40:38 +0200

    cross platform MemCopyHelper

--- Platforms/Utilities/MemCopyHelper.cs
+++ Platforms/Utilities/MemCopyHelper.cs
@@ -1,21 +1,75 @@
 ﻿// Copyright (C)2024 Nick Kastellanos
 
 using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace Microsoft.Xna.Platform.Utilities
 {
     internal static class MemCopyHelper
     {
-        unsafe public static void MemoryCopy<T>(T[] data, IntPtr dstPtr, int startIndex, int count) where T : struct
+
+#if WINDOWSDX
+        [DllImport("kernel32.dll", EntryPoint = "RtlMoveMemory", SetLastError = false)]
+        static extern void RtlMoveMemory(IntPtr destination, IntPtr source, uint count);
+#endif
+
+        public static void MemoryCopy<T>(T[] data, IntPtr dstPtr, int startIndex, int count) where T : struct
         {
-            SharpDX.Utilities.Write(dstPtr, data, startIndex, count);
+            int elementSizeInBytes = Microsoft.Xna.Platform.Graphics.Utilities.ReflectionHelpers.SizeOf<T>();
+
+            GCHandle dataHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+            try
+            {
+                IntPtr srcPtr = dataHandle.AddrOfPinnedObject();
+                srcPtr = srcPtr + startIndex * elementSizeInBytes;
+
+                int sizeInBytes = count * elementSizeInBytes;
+
+                MemoryCopy(srcPtr, dstPtr, sizeInBytes);
+            }
+            finally
+            {
+                dataHandle.Free();
+            }
         }
 
-        unsafe public static void MemoryCopy(IntPtr srcPtr, IntPtr dstPtr, int sizeInBytes)
+        unsafe public static void MemoryCopy(IntPtr srcPtr, IntPtr dstPtr, int count)
         {
-            SharpDX.Utilities.CopyMemory(dstPtr, srcPtr, sizeInBytes);
+#if NET6_0_OR_GREATER || NETSTANDARD2_0
+               Buffer.MemoryCopy((void*)srcPtr, (void*)dstPtr, count, count);
+#else
+
+#if WINDOWSDX
+            if (count >= 64)
+            {
+                RtlMoveMemory(dstPtr, srcPtr, (uint)count);
+                return;
+            }
+#endif
+
+            byte* dst = (byte*)dstPtr;
+            byte* src = (byte*)srcPtr;
+            int len = count;
+
+            while (len >= 8)
+            {
+                *((long*)dst) = *((long*)src);
+                dst += 8; src += 8;
+                len -= 8;
+            }
+            while (len >= 4)
+            {
+                *((int*)dst) = *((int*)src);
+                dst += 4; src += 4;
+                len -= 4;
+            }
+            while (len > 0)
+            {
+                *((byte*)dst) = *((byte*)src);
+                dst += 1; src += 1;
+                len -= 1;
+            }
+#endif
         }
     }
 }
