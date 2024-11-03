599027ace4054c5acb744ff37d32acc9cdc77b9a
Nikos Kastellanos <nkastellanos@gmail.com>
2024-11-03 21:23:04 +0200

    update comment

--- src/Xna.Framework.Storage/Storage/StorageDevice.cs
+++ src/Xna.Framework.Storage/Storage/StorageDevice.cs
@@ -76,154 +76,103 @@ namespace Microsoft.Xna.Framework.Storage
         /// </summary>
         public static event EventHandler<EventArgs> DeviceChanged;
 
-        // Summary:
-        //     Begins the process for opening a StorageContainer containing any files for
-        //     the specified title.
-        //
-        // Parameters:
-        //   displayName:
-        //     A constant human-readable string that names the file.
-        //
-        //   callback:
-        //     An AsyncCallback that represents the method called when the operation is
-        //     complete.
-        //
-        //   state:
-        //     A user-created object used to uniquely identify the request, or null.
+        /// <summary>
+        /// Begins the process for opening a StorageContainer containing any files for
+        /// the specified title.
+        /// </summary>
+        /// <param name="displayName">A constant human-readable string that names the file.</param>
+        /// <param name="callback">An AsyncCallback that represents the method called when the operation is complete.</param>
+        /// <param name="state"A user-created object used to uniquely identify the request, or null.</param>
         public IAsyncResult BeginOpenContainer(string displayName, AsyncCallback callback, object state)
         {
             return _strategy.BeginOpenContainer(this, displayName, callback, state);
         }
     
-        // Summary:
-        //     Ends the process for opening a StorageContainer.
-        //
-        // Parameters:
-        //   result:
-        //     The IAsyncResult returned from BeginOpenContainer.
+        /// <summary>
+        /// Ends the process for opening a StorageContainer.
+        /// </summary>
+        /// <param name="result">The IAsyncResult returned from BeginOpenContainer.</param>
         public StorageContainer EndOpenContainer(IAsyncResult result)
         {
             return _strategy.EndOpenContainer(result);
         }			
 
-        // Parameters:
-        //   titleName:
-        //     The name of the storage container to delete.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="titleName">The name of the storage container to delete.</param>
         public void DeleteContainer(string titleName)
         {
             _strategy.DeleteContainer(titleName);
         }
 
-        // Private method to handle the creation of the StorageDevice
         private StorageContainer Open(string displayName)
         {
             return _strategy.Open(this, displayName);
         }
 
-
-        //
-        // Summary:
-        //     Begins the process for displaying the storage device selector user interface,
-        //     and for specifying a callback implemented when the player chooses a device.
-        //     Reference page contains links to related code samples.
-        //
-        // Parameters:
-        //   callback:
-        //     An AsyncCallback that represents the method called when the player chooses
-        //     a device.
-        //
-        //   state:
-        //     A user-created object used to uniquely identify the request, or null.
+        /// <summary>
+        /// Begins the process for displaying the storage device selector user interface,
+        /// and for specifying a callback implemented when the player chooses a device.
+        /// Reference page contains links to related code samples.
+        /// </summary>
+        /// <param name="callback">An AsyncCallback that represents the method called when the player chooses a device.</param>
+        /// <param name="state">A user-created object used to uniquely identify the request, or null.</param>
         public static IAsyncResult BeginShowSelector(AsyncCallback callback, object state)
         {
             return BeginShowSelector(0, 0, callback, state);
         }
 
-        //
-        // Summary:
-        //     Begins the process for displaying the storage device selector user interface;
-        //     specifies the callback implemented when the player chooses a device. Reference
-        //     page contains links to related code samples.
-        //
-        // Parameters:
-        //   player:
-        //     The PlayerIndex that represents the player who requested the save operation.
-        //     On Windows, the only valid option is PlayerIndex.One.
-        //
-        //   callback:
-        //     An AsyncCallback that represents the method called when the player chooses
-        //     a device.
-        //
-        //   state:
-        //     A user-created object used to uniquely identify the request, or null.
+        /// <summary>
+        ///     Begins the process for displaying the storage device selector user interface;
+        ///     specifies the callback implemented when the player chooses a device. Reference
+        ///     page contains links to related code samples.
+        /// </summary>
+        /// <param name="player">The PlayerIndex that represents the player who requested the save operation. On Windows, the only valid option is PlayerIndex.One.</param>
+        /// <param name="callback">An AsyncCallback that represents the method called when the player chooses a device.</param>
+        /// <param name="state">A user-created object used to uniquely identify the request, or null.</param>
         public static IAsyncResult BeginShowSelector(PlayerIndex player, AsyncCallback callback, object state)
         {
             return BeginShowSelector(player, 0, 0, callback, state);
         }
 
-        //
-        // Summary:
-        //     Begins the process for displaying the storage device selector user interface,
-        //     and for specifying the size of the data to be written to the storage device
-        //     and the callback implemented when the player chooses a device. Reference
-        //     page contains links to related code samples.
-        //
-        // Parameters:
-        //   sizeInBytes:
-        //     The size, in bytes, of data to write to the storage device.
-        //
-        //   directoryCount:
-        //     The number of directories to write to the storage device.
-        //
-        //   callback:
-        //     An AsyncCallback that represents the method called when the player chooses
-        //     a device.
-        //
-        //   state:
-        //     A user-created object used to uniquely identify the request, or null.
+        /// <summary>
+        ///     Begins the process for displaying the storage device selector user interface,
+        ///     and for specifying the size of the data to be written to the storage device
+        ///     and the callback implemented when the player chooses a device. Reference
+        ///     page contains links to related code samples.
+        /// </summary>
+        /// <param name="sizeInBytes">The size, in bytes, of data to write to the storage device.</param>
+        /// <param name="directoryCount">The number of directories to write to the storage device.</param>
+        /// <param name="callback">An AsyncCallback that represents the method called when the player chooses a device.</param>
+        /// <param name="state">A user-created object used to uniquely identify the request, or null.</param>
         public static IAsyncResult BeginShowSelector(int sizeInBytes, int directoryCount, AsyncCallback callback, object state)
         {
             return StorageService.Current.BeginShowSelector(sizeInBytes, directoryCount, callback, state);
         }
-        
-        //
-        // Summary:
-        //     Begins the process for displaying the storage device selector user interface,
-        //     for specifying the player who requested the save operation, for setting the
-        //     size of data to be written to the storage device, and for naming the callback
-        //     implemented when the player chooses a device. Reference page contains links
-        //     to related code samples.
-        //
-        // Parameters:
-        //   player:
-        //     The PlayerIndex that represents the player who requested the save operation.
-        //     On Windows, the only valid option is PlayerIndex.One.
-        //
-        //   sizeInBytes:
-        //     The size, in bytes, of the data to write to the storage device.
-        //
-        //   directoryCount:
-        //     The number of directories to write to the storage device.
-        //
-        //   callback:
-        //     An AsyncCallback that represents the method called when the player chooses
-        //     a device.
-        //
-        //   state:
-        //     A user-created object used to uniquely identify the request, or null.
+
+        /// <summary>
+        ///     Begins the process for displaying the storage device selector user interface,
+        ///     for specifying the player who requested the save operation, for setting the
+        ///     size of data to be written to the storage device, and for naming the callback
+        ///     implemented when the player chooses a device. Reference page contains links
+        ///     to related code samples.
+        /// </summary>
+        /// <param name="player">The PlayerIndex that represents the player who requested the save operation. On Windows, the only valid option is PlayerIndex.One.</param>
+        /// <param name="sizeInBytes">The size, in bytes, of the data to write to the storage device.</param>
+        /// <param name="directoryCount">The number of directories to write to the storage device.</param>
+        /// <param name="callback"> An AsyncCallback that represents the method called when the player chooses a device.</param>
+        /// <param name="state">A user-created object used to uniquely identify the request, or null.</param>
         public static IAsyncResult BeginShowSelector(PlayerIndex player, int sizeInBytes, int directoryCount, AsyncCallback callback, object state)
         {
             return StorageService.Current.BeginShowSelector(player, sizeInBytes, directoryCount, callback, state);
         }
-    
-        //
-        // Summary:
-        //     Ends the display of the storage selector user interface. Reference page contains
-        //     links to related code samples.
-        //
-        // Parameters:
-        //   result:
-        //     The IAsyncResult returned from BeginShowSelector.
+
+        /// <summary>
+        ///     Ends the display of the storage selector user interface. Reference page contains
+        ///     links to related code samples.
+        /// </summary>
+        /// <param name="result">The IAsyncResult returned from BeginShowSelector.</param>
         public static StorageDevice EndShowSelector(IAsyncResult result) 
         {
             return StorageService.Current.EndShowSelector(result);
