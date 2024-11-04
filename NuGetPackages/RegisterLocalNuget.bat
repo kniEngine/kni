8cce0dd995690bf94a82080e777ee0a0bd6de341
Nikos Kastellanos <nkastellanos@gmail.com>
2024-11-04 09:26:03 +0200

    register Devices Package (#1991)

--- NuGetPackages/RegisterLocalNuget.bat
+++ NuGetPackages/RegisterLocalNuget.bat
@@ -8,6 +8,8 @@ set username=username
 "C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Media                     3.14.9001 -Source "C:\Users\%username%\.nuget\localPackages" -NonInteractive
 "C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Input                     3.14.9001 -Source "C:\Users\%username%\.nuget\localPackages" -NonInteractive
 "C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Game                      3.14.9001 -Source "C:\Users\%username%\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Devices                   3.14.9001 -Source "C:\Users\%username%\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Storage                   3.14.9001 -Source "C:\Users\%username%\.nuget\localPackages" -NonInteractive
 
 "C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Content.Pipeline          3.14.9001 -Source "C:\Users\%username%\.nuget\localPackages" -NonInteractive
 "C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Xna.Framework.Content.Pipeline.Audio    3.14.9001 -Source "C:\Users\%username%\.nuget\localPackages" -NonInteractive
@@ -34,6 +36,8 @@ set username=username
 "C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Media.3.14.9001.nupkg                     -Source "C:\Users\%username%\.nuget\localPackages"
 "C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Input.3.14.9001.nupkg                     -Source "C:\Users\%username%\.nuget\localPackages"
 "C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Game.3.14.9001.nupkg                      -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Devices.3.14.9001.nupkg                   -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Storage.3.14.9001.nupkg                   -Source "C:\Users\%username%\.nuget\localPackages"
 
 "C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.3.14.9001.nupkg          -Source "C:\Users\%username%\.nuget\localPackages"
 "C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Audio.3.14.9001.nupkg    -Source "C:\Users\%username%\.nuget\localPackages"
