8d1dfbc92e0affc6430659279655279bd16840fc
Nikos Kastellanos <nkastellanos@gmail.com>
2024-11-11 17:37:36 +0200

    rename _glDisposeContext

--- Platforms/Graphics/.GL.SDL/ConcreteGraphicsContext.SDL.cs
+++ Platforms/Graphics/.GL.SDL/ConcreteGraphicsContext.SDL.cs
@@ -31,6 +31,7 @@ namespace Microsoft.Xna.Platform.Graphics
         {
             _glContext = SDL.OpenGL.CreateGLContext(((IPlatformGraphicsContext)context).DeviceStrategy.PresentationParameters.DeviceWindowHandle);
 
+            // create _glDisposeContext for Disposing GL objects from GC Finalizer thread.
             Sdl.Current.OpenGL.SetAttribute(Sdl.GL.Attribute.ContextReleaseBehaviour, 0);
             Sdl.Current.OpenGL.SetAttribute(Sdl.GL.Attribute.ShareWithCurrentContext, 1);
             _glDisposeContextWindowHandle = SDL.WINDOW.Create("", 0, 0, 0, 0, Sdl.Window.State.Hidden | Sdl.Window.State.OpenGL);
