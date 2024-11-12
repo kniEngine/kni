cf50e5babd7f576d7c22c0ddb0e80cbde9c63201
Nikos Kastellanos <nkastellanos@gmail.com>
2024-11-12 12:07:57 +0200

    IntersectsHelper class

---
+++ src/Xna.Framework/IntersectsHelper.cs
@@ -0,0 +1,15 @@
+﻿// MIT License - Copyright (C) The Mono.Xna Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+// Copyright (C)2024 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Xna.Framework
+{
+    internal class IntersectsHelper
+    {
+    }
+}
