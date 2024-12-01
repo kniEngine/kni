985c460041cdfa59c501a2ab081fc6b8aa4f7dd2
Nikos Kastellanos <nkastellanos@gmail.com>
2024-12-01 14:05:24 +0200

    nuget.org search

--- Tools/Content.Pipeline.Editor.WinForms/Forms/PackageDialog.cs
+++ Tools/Content.Pipeline.Editor.WinForms/Forms/PackageDialog.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,20 @@ namespace Content.Pipeline.Editor.Forms
         public PackageDialog()
         {
             InitializeComponent();
+        }
+
+        private void llSearchNuget_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string nugetUrl = "https://www.nuget.org/packages?q=";
+
+            string packageName = this.tbName.Text;
+            string tags = "Tags:\"kni\",\"pipeline\"";
+            string query = string.Format("{0} {1}", packageName, tags);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = nugetUrl + Uri.EscapeDataString(query);
+            startInfo.UseShellExecute = true;
+            Process.Start(startInfo);
         }
     }
 }
