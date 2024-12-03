2d9011190fe0b76e11d32dd3f3a670f0cebacf48
Nikos Kastellanos <nkastellanos@gmail.com>
2024-12-03 17:12:49 +0200

    XRDeviceStrategy

---
+++ Platforms/XR/XRDeviceState.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (C)2024 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Microsoft.Xna.Framework.XR
+{
+    public enum XRDeviceState
+    {
+        Requesting,
+        NotSupported,
+        Disabled,
+        Initializing,
+        Ready,
+        NoPermissions,
+    }
+}
