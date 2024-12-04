eb8d7f371fe45b15532c3f13d2fff571d3dbc1c8
Nikos Kastellanos <nkastellanos@gmail.com>
2024-12-04 17:21:26 +0200

    Concrete carboard XRDevice strategy

---
+++ Platforms/XR/.Cardboard/ConcreteXRDevice.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (C)2024 Nick Kastellanos
+
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Platform.XR;
+
+namespace Microsoft.Xna.Framework.XR
+{
+    internal class ConcreteXRDevice : XRDeviceStrategy
+    {
+        Game _game;
+        IGraphicsDeviceService _graphics;
+        XRMode _xrMode;
+        XRDeviceState _deviceState;
+
+        public override XRMode Mode
+        {
+            get { return _xrMode; }
+        }
+
+        public override XRDeviceState State
+        {
+            get { return _deviceState; }
+        }
+
+        public override bool IsConnected
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override bool TrackFloorLevelOrigin
+        {
+            get { throw new System.NotImplementedException(); }
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public ConcreteXRDevice(Game game, IGraphicsDeviceService graphics, XRMode mode)
+        {
+            this._game = game;
+            this._graphics = graphics;
+            this._xrMode = mode;
+        }
+
+        public override int CreateDevice()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int BeginFrame()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override HeadsetState GetHeadsetState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IEnumerable<XREye> GetEyes()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override RenderTarget2D GetEyeRenderTarget(XREye eye)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Matrix CreateProjection(XREye eye, float znear, float zfar)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void CommitRenderTarget(XREye eye, RenderTarget2D rt)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int EndFrame()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override HandsState GetHandsState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+        }
+    }
+}
--- Platforms/XR/XRDevice.Cardboard.cs
+++ Platforms/XR/XRDevice.Cardboard.cs
@@ -2,12 +2,52 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Platform.XR;
 
 namespace Microsoft.Xna.Framework.XR
 {
-    public class XRDevice: IDisposable
+    public class XRDevice : IPlatformXRDevice, IDisposable
     {
+        private XRDeviceStrategy _strategy;
+
+        T IPlatformXRDevice.GetStrategy<T>()
+        {
+            return (T)_strategy;
+        }
+
         internal static GameWindow GameWindow;
+
+
+        public XRDeviceState State
+        {
+            get { return _strategy.State; }
+        }
+
+        public XRMode Mode
+        {
+            get { return _strategy.Mode; }
+        }
+
+        public bool IsConnected
+        {
+            get { return _strategy.IsConnected; }
+        }
+
+        public bool TrackFloorLevelOrigin
+        {
+            get { return _strategy.TrackFloorLevelOrigin; }
+            set { _strategy.TrackFloorLevelOrigin = value; }
+        }
+
+        public XRDevice(Game game, IGraphicsDeviceService graphics,
+            XRMode mode = XRMode.VR)
+        {
+            _strategy = new ConcreteXRDevice(game, graphics, mode);
+
+        }
 
         public static HeadsetState GetState()
         {
@@ -17,6 +57,54 @@ namespace Microsoft.Xna.Framework.XR
             window.UpdateHeadsetState(out state);
 
             return state;
+        }
+
+
+        // the following functions should be called in order
+        public int CreateDevice()
+        {
+            return _strategy.CreateDevice();
+        }
+
+        public int BeginFrame()
+        {
+            return _strategy.BeginFrame();
+        }
+
+        public HeadsetState GetHeadsetState()
+        {
+            return _strategy.GetHeadsetState();
+        }
+
+        public IEnumerable<XREye> GetEyes()
+        {
+            return _strategy.GetEyes();
+        }
+
+        public RenderTarget2D GetEyeRenderTarget(XREye eye)
+        {
+            return _strategy.GetEyeRenderTarget(eye);
+        }
+
+        public Matrix CreateProjection(XREye eye, float znear, float zfar)
+        {
+            return _strategy.CreateProjection(eye, znear, zfar);
+        }
+
+        public int CommitRenderTarget(XREye eye, RenderTarget2D rt)
+        {
+            _strategy.CommitRenderTarget(eye, rt);
+            return 0;
+        }
+
+        public int EndFrame()
+        {
+            return _strategy.EndFrame();
+        }
+
+        public HandsState GetHandsState()
+        {
+            return _strategy.GetHandsState();
         }
 
 
@@ -36,7 +124,8 @@ namespace Microsoft.Xna.Framework.XR
         {
             if (disposing)
             {
-
+                _strategy.Dispose();
+                _strategy = null;
             }
 
         }
