1d9b913efe87dc891efc1eaf12dd2cad58b664e2
Nikos Kastellanos <nkastellanos@gmail.com>
2025-02-18 12:59:16 +0200

    remove TouchLocationData.Invalid

--- src/Xna.Framework.Input/Input/Touch/TouchLocationData.cs
+++ src/Xna.Framework.Input/Input/Touch/TouchLocationData.cs
@@ -26,11 +26,6 @@ namespace Microsoft.Xna.Framework.Input.Touch
         /// </summary>
         internal bool SameFrameReleased;
 
-        /// <summary>
-        /// Helper for assigning an invalid touch location.
-        /// </summary>
-        internal static readonly TouchLocationData Invalid = new TouchLocationData();
-
         internal int Id { get { return _id; } }
         internal TouchLocationState State { get { return _state; } }
         internal Vector2 Position { get { return _position; } }
