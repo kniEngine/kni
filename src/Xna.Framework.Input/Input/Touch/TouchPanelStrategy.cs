b10fed74fcb6cad1fad25edea51b52bc8f7d7191
Nikos Kastellanos <nkastellanos@gmail.com>
2025-02-18 15:39:57 +0200

    indent code blocks

--- src/Xna.Framework.Input/Input/Touch/TouchPanelStrategy.cs
+++ src/Xna.Framework.Input/Input/Touch/TouchPanelStrategy.cs
@@ -95,29 +95,25 @@ namespace Microsoft.Xna.Platform.Input.Touch
         /// </remarks>
         public void BlazorCancelAllTouches()
         {
-            {
-                // local copy of touchStates
-                int nativeTouchIdsCount = _touchIdsMap.Count;
-                int[] nativeTouchIds = new int[nativeTouchIdsCount];
-                _touchIdsMap.Keys.CopyTo(nativeTouchIds, 0);
+            // local copy of touchStates
+            int nativeTouchIdsCount = _touchIdsMap.Count;
+            int[] nativeTouchIds = new int[nativeTouchIdsCount];
+            _touchIdsMap.Keys.CopyTo(nativeTouchIds, 0);
 
-                // submit a fake Canceled event for each touch Id
-                for (int i = 0; i < nativeTouchIdsCount; i++)
-                    AddCanceledEvent(nativeTouchIds[i], Vector2.Zero);
-            }
+            // submit a fake Canceled event for each touch Id
+            for (int i = 0; i < nativeTouchIdsCount; i++)
+                AddCanceledEvent(nativeTouchIds[i], Vector2.Zero);
         }
 
         public void TestReleaseAllTouches()
         {
-            {
-                // local copy of touchStates
-                int nativeTouchIdsCount = _touchIdsMap.Count;
-                int[] nativeTouchIds = new int[nativeTouchIdsCount];
-                _touchIdsMap.Keys.CopyTo(nativeTouchIds, 0);
+            // local copy of touchStates
+            int nativeTouchIdsCount = _touchIdsMap.Count;
+            int[] nativeTouchIds = new int[nativeTouchIdsCount];
+            _touchIdsMap.Keys.CopyTo(nativeTouchIds, 0);
 
-                for (int i = 0; i < nativeTouchIdsCount; i++)
-                    AddReleasedEvent(nativeTouchIds[i], Vector2.Zero);
-            }
+            for (int i = 0; i < nativeTouchIdsCount; i++)
+                AddReleasedEvent(nativeTouchIds[i], Vector2.Zero);
         }
 
         protected TouchPanelCapabilities CreateTouchPanelCapabilities(int maximumTouchCount, bool isConnected, bool hasPressure)
