9ef7aa0fad56f139310a2bdc6d2b3e5df79e273a
Nikos Kastellanos <nkastellanos@gmail.com>
2025-02-22 12:38:39 +0200

    inline ApplyChanges()

--- Platforms/Game/.iOS/ConcreteGraphicsDeviceManager.cs
+++ Platforms/Game/.iOS/ConcreteGraphicsDeviceManager.cs
@@ -110,9 +110,21 @@ namespace Microsoft.Xna.Platform
 
             this.GraphicsDevice = new GraphicsDevice(GraphicsAdapter.DefaultAdapter, GraphicsProfile, this.PreferHalfPixelOffset, pp);
 
-            this.ApplyChanges();
             // ApplyChanges
             {
+                base.GraphicsDevice.PresentationParameters.DisplayOrientation = base.Game.Window.CurrentOrientation;
+
+                bool isLandscape = ((base.Game.Window.CurrentOrientation & (DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight)) != 0);
+                int w = PreferredBackBufferWidth;
+                int h = PreferredBackBufferHeight;
+
+                base.GraphicsDevice.PresentationParameters.BackBufferWidth = isLandscape ? Math.Max(w, h) : Math.Min(w, h);
+                base.GraphicsDevice.PresentationParameters.BackBufferHeight = isLandscape ? Math.Min(w, h) : Math.Max(w, h);
+
+                // TODO: In XNA this seems to be done as part of the GraphicsDevice.DeviceReset event...
+                //       we need to get those working.
+                TouchPanel.DisplayWidth = base.GraphicsDevice.PresentationParameters.BackBufferWidth;
+                TouchPanel.DisplayHeight = base.GraphicsDevice.PresentationParameters.BackBufferHeight;
             }
 
             // TODO: In XNA this seems to be done as part of the GraphicsDevice.DeviceReset event...
