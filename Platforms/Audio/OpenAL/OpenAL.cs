bc5f27474c595633ae0a5992a2f8a9d42c54d176
Nikos Kastellanos <nkastellanos@gmail.com>
2025-03-08 18:00:28 +0200

    update AlcError

--- Platforms/Audio/OpenAL/OpenAL.cs
+++ Platforms/Audio/OpenAL/OpenAL.cs
@@ -112,7 +112,12 @@ namespace Microsoft.Xna.Platform.Audio.OpenAL
 
     internal enum AlcError
     {
-        NoError = 0,
+        NoError        = 0,
+        InvalidDevice  = 0xA001,
+        InvalidContext = 0xA002,
+        InvalidEnum    = 0xA003,
+        InvalidValue   = 0xA004,
+        OutOfMemory    = 0xA005,
     }
 
     internal enum AlcGetString
