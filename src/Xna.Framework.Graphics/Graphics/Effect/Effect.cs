6f2f455a779133c2e116c5b23a2aedc866d8b7c6
Nikos Kastellanos <nkastellanos@gmail.com>
2025-04-17 18:01:06 +0300

    move MGFXHeader

--- src/Xna.Framework.Graphics/Graphics/Effect/Effect.cs
+++ src/Xna.Framework.Graphics/Graphics/Effect/Effect.cs
@@ -14,40 +14,6 @@ namespace Microsoft.Xna.Framework.Graphics
 {
     public partial class Effect : GraphicsResource
     {
-        struct MGFXHeader 
-        {
-            /// <summary>
-            /// The MonoGame Effect file format header identifier ("MGFX"). 
-            /// </summary>
-            public static readonly int MGFXSignature = (BitConverter.IsLittleEndian) ? 0x5846474D: 0x4D474658;
-
-            /// <summary>
-            /// The current MonoGame Effect file format versions
-            /// used to detect old packaged content.
-            /// </summary>
-            /// <remarks>
-            /// We should avoid supporting old versions for very long if at all 
-            /// as users should be rebuilding content when packaging their game.
-            /// </remarks>
-            public const int MGFXVersion = 10;
-
-            public readonly int Signature;
-            public readonly int Version;
-            public readonly ShaderProfileType Profile;
-            public readonly int EffectKey;
-            public readonly int HeaderSize;
-
-            public MGFXHeader(byte[] effectCode, int index)
-            {
-                int offset = 0;
-                Signature = BitConverter.ToInt32(effectCode, index + offset); offset += 4;
-                Version = (int)effectCode[index + offset]; offset += 1;
-                Profile = (ShaderProfileType)effectCode[index + offset]; offset += 1;
-                EffectKey = BitConverter.ToInt32(effectCode, index + offset); offset += 4;
-                HeaderSize = offset;
-            }
-        }
-
         public EffectParameterCollection Parameters { get; private set; }
 
         public EffectTechniqueCollection Techniques { get; private set; }
