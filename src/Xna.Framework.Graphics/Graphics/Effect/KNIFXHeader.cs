d16452cbd022403ba2318fab6c03c94acb3c6df6
Nikos Kastellanos <nkastellanos@gmail.com>
2025-04-17 19:16:41 +0300

    new FX format

---
+++ src/Xna.Framework.Graphics/Graphics/Effect/KNIFXHeader.cs
@@ -0,0 +1,37 @@
+// Copyright (C)2025 Nick Kastellanos
+
+using System;
+using System.Diagnostics;
+using Microsoft.Xna.Platform.Graphics;
+
+namespace Microsoft.Xna.Framework.Graphics
+{
+        internal struct KNIFXHeader
+        {
+            /// <summary>
+            /// The KNI Effect file format header identifier ("KNIF").
+            /// </summary>
+            public static readonly int KNIFXSignature = (BitConverter.IsLittleEndian) ? 0x46494E4B : 0x4B4E4946;
+
+            /// <summary>
+            /// The current KNI Effect file format versions
+            /// </summary>
+            public const int CurrentKNIFXVersion = 11;
+
+            public readonly int Signature;
+            public readonly int Version;
+            public readonly ShaderProfileType Profile;
+            public readonly int EffectKey;
+            public readonly int HeaderSize;
+
+            public KNIFXHeader(byte[] effectCode, int index)
+            {
+                int offset = 0;
+                Signature = BitConverter.ToInt32(effectCode, index + offset); offset += 4;
+                Version = (int)effectCode[index + offset]; offset += 1;
+                Profile = (ShaderProfileType)effectCode[index + offset]; offset += 1;
+                EffectKey = BitConverter.ToInt32(effectCode, index + offset); offset += 4;
+                HeaderSize = offset;
+            }
+        }
+}
