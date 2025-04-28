b26991103ff67c1b2a7ea7fcf830e9a511cb8d3f
Nikos Kastellanos <nkastellanos@gmail.com>
2025-04-28 20:06:32 +0300

    mkv Decoder

--- Platforms/Media/DesktopGL/ConcreteVideoPlayer.cs
+++ Platforms/Media/DesktopGL/ConcreteVideoPlayer.cs
@@ -1,7 +1,14 @@
 // Copyright (C)2022 Nick Kastellanos
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
 
@@ -9,6 +16,10 @@ namespace Microsoft.Xna.Platform.Media
 {
     internal sealed class ConcreteVideoPlayerStrategy : VideoPlayerStrategy
     {
+        private VideoDecoderProcess _decoderProcess;
+        private Texture2D _lastFrame;
+
+        private DynamicSoundEffectInstance _soundPlayer;
 
         public override MediaState State
         {
@@ -50,22 +61,69 @@ namespace Microsoft.Xna.Platform.Media
 
         internal ConcreteVideoPlayerStrategy()
         {
-            throw new NotImplementedException();
+            
         }
 
         public override Texture2D PlatformGetTexture()
         {
-            throw new NotImplementedException();
+            if (_lastFrame != null)
+            {
+                if (_lastFrame.Width != base.Video.Width || _lastFrame.Height != base.Video.Height)
+                {
+                    _lastFrame.Dispose();
+                    _lastFrame = null;
+                }
+            }
+            if (_lastFrame == null)
+                _lastFrame = new Texture2D(((IPlatformVideo)base.Video).Strategy.GraphicsDevice, base.Video.Width, base.Video.Height, false, SurfaceFormat.Color);
+
+            // test. read next frame.
+            if (false)
+            {
+                while (!_decoderProcess._isVideoFrameDataDirty)
+                    _decoderProcess.ParseSegmentCluster2(_decoderProcess._binaryReader, (long)0);
+            }
+
+            if (_decoderProcess._isVideoFrameDataDirty)
+            {
+                _lastFrame.SetData(_decoderProcess._videoFrameData);
+                _decoderProcess._isVideoFrameDataDirty = false;
+            }
+
+            return _lastFrame;
         }
 
         protected override void PlatformUpdateState(ref MediaState state)
         {
-            throw new NotImplementedException();
+            
         }
 
         public override void PlatformPlay(Video video)
         {
-            throw new NotImplementedException();
+            base.Video = video;
+
+            // Cleanup the last video first.
+            if (State != MediaState.Stopped)
+            {
+                _decoderProcess.Dispose();
+                _decoderProcess = null;
+            }
+
+            string ffmpegPath = "x64\\ffmpeg";
+            string inputFile = "" + ((IPlatformVideo)base.Video).Strategy.FileName;
+
+            _decoderProcess = new VideoDecoderProcess(base.Video, ffmpegPath, inputFile);
+
+            _decoderProcess.ParseMKV();
+
+            VideoDecoderProcess.MKVTrack track = _decoderProcess._tracks.Values.First( (t) => (t.Type == VideoDecoderProcess.MKVTrackType.Audio) );
+            VideoDecoderProcess.MKVAudioTrack audioTrack = (VideoDecoderProcess.MKVAudioTrack)track;
+            VideoDecoderProcess.MKVAudioSettings audioSettings = audioTrack.AudioSettings;
+            int SampleRate = (int)audioSettings.SamplingFrequency;
+            AudioChannels channels = (AudioChannels)audioSettings.Channels;
+            _soundPlayer = new DynamicSoundEffectInstance(SampleRate, channels);
+            int samples = (SampleRate * (int)channels) / 2;
+
         }
 
         public override void PlatformPause()
@@ -92,7 +150,14 @@ namespace Microsoft.Xna.Platform.Media
         {
             if (disposing)
             {
+                if (_decoderProcess != null)
+                {
+                    _decoderProcess.Dispose();
+                    _decoderProcess = null;
+                }
+
             }
+
 
             base.Dispose(disposing);
         }
