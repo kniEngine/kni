f952ad80cd5736fe58ce8247076856e968bac435
Nikos Kastellanos <nkastellanos@gmail.com>
2025-06-30 23:43:47 +0300

    InitMicrophoneDeviceAsync()

--- Platforms/Audio/Blazor/ConcreteMicrophone.cs
+++ Platforms/Audio/Blazor/ConcreteMicrophone.cs
@@ -2,8 +2,12 @@
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Audio;
 using nkast.Wasm.Audio;
+using nkast.Wasm.Dom;
+using nkast.Wasm.Media;
 
 namespace Microsoft.Xna.Platform.Audio
 {
@@ -13,6 +17,10 @@ namespace Microsoft.Xna.Platform.Audio
     public sealed class ConcreteMicrophone : MicrophoneStrategy
     {
         AudioContext _ac;
+        private CancellationTokenSource _micInitCts;
+        MediaStream _micStream;
+        MediaStreamSourceNode _micNode;
+        AudioWorkletNode _micWorkletNode;
 
         public override TimeSpan BufferDuration
         {
@@ -36,12 +44,12 @@ namespace Microsoft.Xna.Platform.Audio
 
         public override void PlatformStart(string deviceName)
         {
-            throw new NotImplementedException();
+            InitMicrophoneDeviceAsync();
         }
 
         public override void PlatformStop()
         {
-            throw new NotImplementedException();
+            ReleaseMicrophoneDevice();
         }
 
         public override bool PlatformIsHeadset()
@@ -57,6 +65,59 @@ namespace Microsoft.Xna.Platform.Audio
         public override int PlatformGetData(byte[] buffer, int offset, int count)
         {
             throw new NotImplementedException();
+        }
+
+        private async Task InitMicrophoneDeviceAsync()
+        {
+            _micInitCts = new CancellationTokenSource();
+            CancellationToken token = _micInitCts.Token;
+
+            try
+            {
+                // init micProcessor AudioWorklet
+                await _ac.AudioWorklet.AddModuleAsync("js/micProcessor.js");
+                if (token.IsCancellationRequested) return;
+
+                _micWorkletNode = _ac.CreateWorklet("mic-processor");
+
+                // init and start Microphone
+                MediaDevices md = MediaDevices.FromNavigator(Window.Current.Navigator);
+                _micStream = await md.GetUserMediaAsync(new UserMediaConstraints() { Audio = true });
+                if (token.IsCancellationRequested) return;
+                _micNode = _ac.CreateMediaStreamSource(_micStream);
+                _micNode.Connect(_micWorkletNode);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void ReleaseMicrophoneDevice()
+        {
+            if (_micInitCts != null)
+            {
+                _micInitCts.Cancel();
+                _micInitCts = null;
+            }
+
+            if (_micNode != null)
+            {
+                _micNode.Disconnect(_micWorkletNode);
+                _micNode.Dispose();
+                _micNode = null;
+            }
+
+            if (_micStream != null)
+            {
+                _micStream.Dispose();
+                _micStream = null;
+            }
+
+            if (_micWorkletNode != null)
+            {
+                _micWorkletNode.Dispose();
+                _micWorkletNode = null;
+            }
         }
     }
 }
