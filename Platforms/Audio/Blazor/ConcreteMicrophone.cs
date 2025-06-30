ac6eb0f4e9b92d1444167fe4fa419f3400d0b96f
Nikos Kastellanos <nkastellanos@gmail.com>
2025-06-30 23:27:54 +0300

    update Copyright

--- Platforms/Audio/Blazor/ConcreteMicrophone.cs
+++ Platforms/Audio/Blazor/ConcreteMicrophone.cs
@@ -1,6 +1,4 @@
-﻿// MonoGame - Copyright (C) The MonoGame Team
-// This file is subject to the terms and conditions defined in
-// file 'LICENSE.txt', which is part of this source code package.
+﻿// Copyright (C)2025 Nick Kastellanos
 
 using System;
 using System.Collections.Generic;
