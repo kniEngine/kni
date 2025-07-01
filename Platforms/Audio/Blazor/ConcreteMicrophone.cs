24e948b11bba2bb922849167f43bead83691e3cd
Nikos Kastellanos <nkastellanos@gmail.com>
2025-07-01 14:40:11 +0300

    _dataBuffer

--- Platforms/Audio/Blazor/ConcreteMicrophone.cs
+++ Platforms/Audio/Blazor/ConcreteMicrophone.cs
@@ -23,6 +23,11 @@ namespace Microsoft.Xna.Platform.Audio
         MediaStreamSourceNode _micNode;
         AudioWorkletNode _micWorkletNode;
 
+        byte[] _dataBuffer;
+        private int _dataBufferBegin = 0;
+        private int _dataBufferEnd = 0;
+
+
         public override TimeSpan BufferDuration
         {
             get { return base.BufferDuration; }
@@ -41,6 +46,9 @@ namespace Microsoft.Xna.Platform.Audio
         {
             _ac = new AudioContext();
             base.SampleRate = _ac.SampleRate;
+
+            int bufferSize = base.SampleRate * 1 * sizeof(short) * 2;
+            _dataBuffer = new byte[bufferSize];
         }
 
         public override void PlatformStart(string deviceName)
@@ -60,12 +68,29 @@ namespace Microsoft.Xna.Platform.Audio
 
         public override bool PlatformUpdate()
         {
-            throw new NotImplementedException();
+            if (_dataBufferEnd != _dataBufferBegin)
+                return true;
+
+            return false;
         }
 
         public override int PlatformGetData(byte[] buffer, int offset, int count)
         {
-            throw new NotImplementedException();
+            int availableDataSize = (_dataBufferEnd - _dataBufferBegin);
+            if (availableDataSize < 0)
+                availableDataSize = availableDataSize + _dataBuffer.Length;
+
+            count = Math.Min(count, availableDataSize);
+            if (count > 0)
+            {
+                int firstBlockSize = Math.Min(count, _dataBuffer.Length - _dataBufferBegin);
+                Buffer.BlockCopy(_dataBuffer, _dataBufferBegin, buffer, offset, firstBlockSize);
+                if (count > firstBlockSize)
+                    Buffer.BlockCopy(_dataBuffer, 0, buffer, offset + firstBlockSize, (count - firstBlockSize));
+                _dataBufferBegin = (_dataBufferBegin + count) % _dataBuffer.Length;
+            }
+
+            return count;
         }
 
         private void OnMicMessage(object sender, MessageEventArgs e)
@@ -74,6 +99,24 @@ namespace Microsoft.Xna.Platform.Audio
             {
                 JSUInt8Array data = e.DataByteArray;
 
+                int count = data.Count;
+
+                if (count > _dataBuffer.Length / 2)
+                    throw new InvalidOperationException("_dataBuffer must be at least twice as large as data length.");
+
+                if (count <= (_dataBuffer.Length - _dataBufferEnd))
+                {
+                    data.CopyTo(0, _dataBuffer, _dataBufferEnd, count);
+                }
+                else
+                {
+                    int firstBlockSize = (_dataBuffer.Length - _dataBufferEnd);
+                    data.CopyTo(0, _dataBuffer, _dataBufferEnd, firstBlockSize);
+                    data.CopyTo(firstBlockSize, _dataBuffer, 0, count - firstBlockSize);
+                }
+                _dataBufferEnd = (_dataBufferEnd + count) % _dataBuffer.Length;
+
+                data.Dispose();
             }
             else
             {
