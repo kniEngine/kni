c603b3c7b847e55cd3bca77bcacdb7c3f7b08145
Nikos Kastellanos <nkastellanos@gmail.com>
2025-07-19 16:38:42 +0300

    VideoImporter (#2385)

---
+++ src/Xna.Framework.Content.Pipeline.Media/VideoImporter.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (C)2025 Nick Kastellanos
+
+using System;
+
+namespace Microsoft.Xna.Framework.Content.Pipeline
+{
+    [ContentImporter(".wmv",
+                     ".asf",
+                     ".mp4",
+                     ".m4v",
+                     ".avi",
+                     ".divx",
+                     ".xvid",
+                     ".mov",
+                     ".mpg",
+                     ".mpeg",
+                     ".webm",
+                     ".mkv",
+                     ".ogv",
+                     ".mjpeg",
+                     ".vob",
+                     ".ts",
+                     ".y4m",
+                     ".flv",
+                     ".3gp",
+                     DisplayName = "Video Importer - KNI", DefaultProcessor = "VideoProcessor")]
+    public class VideoImporter : ContentImporter<VideoContent>
+    {
+        public VideoImporter()
+        {
+        }
+
+        public override VideoContent Import(string filename, ContentImporterContext context)
+        {
+            var content = new VideoContent(filename);
+            return content;
+        }
+    }
+}
