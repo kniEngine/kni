d7d31fead0e46d28f3e7ba7708066f641054667e
Nikos Kastellanos <nkastellanos@gmail.com>
2025-08-16 11:51:09 +0300

    BoundingFrustumIntersectsBoundingSphere(...)

--- src/Xna.Framework/IntersectsHelper.cs
+++ src/Xna.Framework/IntersectsHelper.cs
@@ -324,29 +324,47 @@ namespace Microsoft.Xna.Framework
                 return;
 
             float dotCoord0 = frustum._planes[0].DotCoordinate(sphere.Center);
-            Vector3 pt0 = sphere.Center - dotCoord0 * frustum._planes[0].Normal;
-            if (PointInPolygon(ref pt0, new FixedPolygon4(frustum._corners, 1, 0, 3, 2)))
-                return;
+            if (dotCoord0 >= 0 && dotCoord0 <= sphere.Radius)
+            {
+                Vector3 pt0 = sphere.Center - dotCoord0 * frustum._planes[0].Normal;
+                if (PointInPolygon(ref pt0, new FixedPolygon4(frustum._corners, 1, 0, 3, 2)))
+                    return;
+            }
             float dotCoord1 = frustum._planes[1].DotCoordinate(sphere.Center);
-            Vector3 pt1 = sphere.Center - dotCoord1 * frustum._planes[1].Normal;
-            if (PointInPolygon(ref pt1, new FixedPolygon4(frustum._corners, 4, 5, 6, 7)))
-                return;
+            if (dotCoord1 >= 0 && dotCoord1 <= sphere.Radius)
+            {
+                Vector3 pt1 = sphere.Center - dotCoord1 * frustum._planes[1].Normal;
+                if (PointInPolygon(ref pt1, new FixedPolygon4(frustum._corners, 4, 5, 6, 7)))
+                    return;
+            }
             float dotCoord2 = frustum._planes[2].DotCoordinate(sphere.Center);
-            Vector3 pt2 = sphere.Center - dotCoord2 * frustum._planes[2].Normal;
-            if (PointInPolygon(ref pt2, new FixedPolygon4(frustum._corners, 3, 0, 7, 4)))
-                return;
+            if (dotCoord2 >= 0 && dotCoord2 <= sphere.Radius)
+            {
+                Vector3 pt2 = sphere.Center - dotCoord2 * frustum._planes[2].Normal;
+                if (PointInPolygon(ref pt2, new FixedPolygon4(frustum._corners, 3, 0, 7, 4)))
+                    return;
+            }
             float dotCoord3 = frustum._planes[3].DotCoordinate(sphere.Center);
-            Vector3 pt3 = sphere.Center - dotCoord3 * frustum._planes[3].Normal;
-            if (PointInPolygon(ref pt3, new FixedPolygon4(frustum._corners, 1, 2, 5, 6)))
-                return;
+            if (dotCoord3 >= 0 && dotCoord3 <= sphere.Radius)
+            {
+                Vector3 pt3 = sphere.Center - dotCoord3 * frustum._planes[3].Normal;
+                if (PointInPolygon(ref pt3, new FixedPolygon4(frustum._corners, 1, 2, 5, 6)))
+                    return;
+            }
             float dotCoord4 = frustum._planes[4].DotCoordinate(sphere.Center);
-            Vector3 pt4 = sphere.Center - dotCoord4 * frustum._planes[4].Normal;
-            if (PointInPolygon(ref pt4, new FixedPolygon4(frustum._corners, 0, 1, 4, 5)))
-                return;
+            if (dotCoord4 >= 0 && dotCoord4 <= sphere.Radius)
+            {
+                Vector3 pt4 = sphere.Center - dotCoord4 * frustum._planes[4].Normal;
+                if (PointInPolygon(ref pt4, new FixedPolygon4(frustum._corners, 0, 1, 4, 5)))
+                    return;
+            }
             float dotCoord5 = frustum._planes[5].DotCoordinate(sphere.Center);
-            Vector3 pt5 = sphere.Center - dotCoord5 * frustum._planes[5].Normal;
-            if (PointInPolygon(ref pt5, new FixedPolygon4(frustum._corners, 2, 3, 6, 7)))
-                return;
+            if (dotCoord5 >= 0 && dotCoord5 <= sphere.Radius)
+            {
+                Vector3 pt5 = sphere.Center - dotCoord5 * frustum._planes[5].Normal;
+                if (PointInPolygon(ref pt5, new FixedPolygon4(frustum._corners, 2, 3, 6, 7)))
+                    return;
+            }
 
             result = false;
         }
