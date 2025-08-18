281b9afa0213f8ff12663149a967e78beec4f8b2
Nikos Kastellanos <nkastellanos@gmail.com>
2025-08-18 10:31:38 +0300

    Missing Platform error message

--- Tools/EffectCompiler/Program.cs
+++ Tools/EffectCompiler/Program.cs
@@ -43,7 +43,7 @@ namespace EffectCompiler
                 processor.Defines = options.Defines;
 
                 if (options.Platform == (TargetPlatform)(-1))
-                    throw new InvalidOperationException("Platform");
+                    throw new InvalidOperationException("Missing argument 'Platform'");
 
                 ContentProcessorContext processorContext = new ProcessorContext(logger, options.Platform, options.Profile, options.OutputFile, options.Config);
                 CompiledEffectContent output = processor.Process(content, processorContext);
