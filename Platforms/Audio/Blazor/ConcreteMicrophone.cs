4ed9e1c02cf9e61a1e80415efe7ca5b1d1b24629
Nikos Kastellanos <nkastellanos@gmail.com>
2025-09-03 21:22:17 +0300

    implements MicrophoneStrategy for BlazorGL (#2469)
    * create Blazor DefaultMicrophone
    
    * update Copyright
    
    * set system SampleRate
    
    * InitMicrophoneDeviceAsync()
    
    * OnMicMessage event
    
    * _dataBuffer

--- Platforms/Audio/Blazor/ConcreteMicrophone.cs
+++ Platforms/Audio/Blazor/ConcreteMicrophone.cs
@@ -1,10 +1,14 @@
-﻿// MonoGame - Copyright (C) The MonoGame Team
-// This file is subject to the terms and conditions defined in
-// file 'LICENSE.txt', which is part of this source code package.
+﻿// Copyright (C)2025 Nick Kastellanos
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Audio;
+using nkast.Wasm.Audio;
+using nkast.Wasm.ChannelMessaging;
+using nkast.Wasm.Dom;
+using nkast.Wasm.Media;
 
 namespace Microsoft.Xna.Platform.Audio
 {
@@ -13,6 +17,16 @@ namespace Microsoft.Xna.Platform.Audio
     /// </summary>
     public sealed class ConcreteMicrophone : MicrophoneStrategy
     {
+        AudioContext _ac;
+        private CancellationTokenSource _micInitCts;
+        MediaStream _micStream;
+        MediaStreamSourceNode _micNode;
+        AudioWorkletNode _micWorkletNode;
+
+        byte[] _dataBuffer;
+        private int _dataBufferBegin = 0;
+        private int _dataBufferEnd = 0;
+
 
         public override TimeSpan BufferDuration
         {
@@ -30,16 +44,21 @@ namespace Microsoft.Xna.Platform.Audio
         internal ConcreteMicrophone()
             : base()
         {
+            _ac = new AudioContext();
+            base.SampleRate = _ac.SampleRate;
+
+            int bufferSize = base.SampleRate * 1 * sizeof(short) * 2;
+            _dataBuffer = new byte[bufferSize];
         }
 
         public override void PlatformStart(string deviceName)
         {
-            throw new NotImplementedException();
+            InitMicrophoneDeviceAsync();
         }
 
         public override void PlatformStop()
         {
-            throw new NotImplementedException();
+            ReleaseMicrophoneDevice();
         }
 
         public override bool PlatformIsHeadset()
@@ -49,12 +68,116 @@ namespace Microsoft.Xna.Platform.Audio
 
         public override bool PlatformUpdate()
         {
-            throw new NotImplementedException();
+            if (_dataBufferEnd != _dataBufferBegin)
+                return true;
+
+            return false;
         }
 
         public override int PlatformGetData(byte[] buffer, int offset, int count)
         {
-            throw new NotImplementedException();
+            int availableDataSize = (_dataBufferEnd - _dataBufferBegin);
+            if (availableDataSize < 0)
+                availableDataSize = availableDataSize + _dataBuffer.Length;
+
+            count = Math.Min(count, availableDataSize);
+            if (count > 0)
+            {
+                int firstBlockSize = Math.Min(count, _dataBuffer.Length - _dataBufferBegin);
+                Buffer.BlockCopy(_dataBuffer, _dataBufferBegin, buffer, offset, firstBlockSize);
+                if (count > firstBlockSize)
+                    Buffer.BlockCopy(_dataBuffer, 0, buffer, offset + firstBlockSize, (count - firstBlockSize));
+                _dataBufferBegin = (_dataBufferBegin + count) % _dataBuffer.Length;
+            }
+
+            return count;
+        }
+
+        private void OnMicMessage(object sender, MessageEventArgs e)
+        {
+            if (e.DataByteArray != null)
+            {
+                JSUInt8Array data = e.DataByteArray;
+
+                int count = data.Count;
+
+                if (count > _dataBuffer.Length / 2)
+                    throw new InvalidOperationException("_dataBuffer must be at least twice as large as data length.");
+
+                if (count <= (_dataBuffer.Length - _dataBufferEnd))
+                {
+                    data.CopyTo(0, _dataBuffer, _dataBufferEnd, count);
+                }
+                else
+                {
+                    int firstBlockSize = (_dataBuffer.Length - _dataBufferEnd);
+                    data.CopyTo(0, _dataBuffer, _dataBufferEnd, firstBlockSize);
+                    data.CopyTo(firstBlockSize, _dataBuffer, 0, count - firstBlockSize);
+                }
+                _dataBufferEnd = (_dataBufferEnd + count) % _dataBuffer.Length;
+
+                data.Dispose();
+            }
+            else
+            {
+                double msg = e.DataFloat64;
+
+            }
+        }
+
+        private async Task InitMicrophoneDeviceAsync()
+        {
+            _micInitCts = new CancellationTokenSource();
+            CancellationToken token = _micInitCts.Token;
+
+            try
+            {
+                // init micProcessor AudioWorklet
+                await _ac.AudioWorklet.AddModuleAsync("js/micProcessor.js");
+                if (token.IsCancellationRequested) return;
+
+                _micWorkletNode = _ac.CreateWorklet("mic-processor");
+                _micWorkletNode.Port.Message += OnMicMessage;
+
+                // init and start Microphone
+                MediaDevices md = MediaDevices.FromNavigator(Window.Current.Navigator);
+                _micStream = await md.GetUserMediaAsync(new UserMediaConstraints() { Audio = true });
+                if (token.IsCancellationRequested) return;
+                _micNode = _ac.CreateMediaStreamSource(_micStream);
+                _micNode.Connect(_micWorkletNode);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void ReleaseMicrophoneDevice()
+        {
+            if (_micInitCts != null)
+            {
+                _micInitCts.Cancel();
+                _micInitCts = null;
+            }
+
+            if (_micNode != null)
+            {
+                _micNode.Disconnect(_micWorkletNode);
+                _micNode.Dispose();
+                _micNode = null;
+            }
+
+            if (_micStream != null)
+            {
+                _micStream.Dispose();
+                _micStream = null;
+            }
+
+            if (_micWorkletNode != null)
+            {
+                _micWorkletNode.Port.Message-= OnMicMessage;
+                _micWorkletNode.Dispose();
+                _micWorkletNode = null;
+            }
         }
     }
 }
