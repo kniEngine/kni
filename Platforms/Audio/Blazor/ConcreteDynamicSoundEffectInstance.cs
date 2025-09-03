b54a508e157347f1a9177d1521f26793a1325dc7
Nikos Kastellanos <nkastellanos@gmail.com>
2025-09-03 21:14:08 +0300

    implements DynamicSoundEffectInstance (#2468)

--- Platforms/Audio/Blazor/ConcreteDynamicSoundEffectInstance.cs
+++ Platforms/Audio/Blazor/ConcreteDynamicSoundEffectInstance.cs
@@ -1,12 +1,11 @@
-﻿// MonoGame - Copyright (C) The MonoGame Team
-// This file is subject to the terms and conditions defined in
-// file 'LICENSE.txt', which is part of this source code package.
-
-// Copyright (C)2021 Nick Kastellanos
+﻿// Copyright (C)2025 Nick Kastellanos
 
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Audio;
+using nkast.Wasm.Audio;
+using nkast.Wasm.ChannelMessaging;
 
 namespace Microsoft.Xna.Platform.Audio
 {
@@ -16,11 +15,28 @@ namespace Microsoft.Xna.Platform.Audio
         private int _sampleRate;
         private int _channels;
 
+        AudioWorkletNode _streamSource;
+        private bool _isStreamSourceInitialized;
+        private int _pendingBuffers;
+
+
         private readonly WeakReference _dynamicSoundEffectInstanceRef = new WeakReference(null);
         DynamicSoundEffectInstance IDynamicSoundEffectInstanceStrategy.DynamicSoundEffectInstance
         {
             get { return _dynamicSoundEffectInstanceRef.Target as DynamicSoundEffectInstance; }
             set { _dynamicSoundEffectInstanceRef.Target = value; }
+        }
+
+        public override float Pitch
+        {
+            get { return base.Pitch; }
+            set
+            {
+                if (value != 0)
+                    throw new NotSupportedException("DynamicSoundEffectInstance does not support Pitch.");
+
+                base.Pitch = value;
+            }
         }
 
         internal ConcreteDynamicSoundEffectInstance(AudioServiceStrategy audioServiceStrategy, int sampleRate, int channels)
@@ -29,13 +45,21 @@ namespace Microsoft.Xna.Platform.Audio
             _sampleRate = sampleRate;
             _channels = channels;
 
+            AudioContext context = ConcreteAudioService.Context;
+
+            // TODO: implement resampling.
+            if (_sampleRate != context.SampleRate)
+                throw new NotImplementedException($"Sample rate {_sampleRate} does not match AudioContext sample rate {context.SampleRate}.");
+            // TODO: implement Stereo.
+            if (_channels != 1)
+                throw new NotImplementedException($"Channels {_channels} is not implemented.");
         }
 
         public int BuffersNeeded { get; set; }
 
         public int DynamicPlatformGetPendingBufferCount()
         {
-            throw new NotImplementedException();
+            return _tmpBuffers.Count + _pendingBuffers;
         }
 
         public override void PlatformPause()
@@ -45,7 +69,65 @@ namespace Microsoft.Xna.Platform.Audio
 
         public override void PlatformPlay(bool isLooped)
         {
-            throw new NotImplementedException();
+            AudioContext context = ConcreteAudioService.Context;
+
+            float masterVolume = SoundEffect.MasterVolume;
+            _gainNode.Gain.SetTargetAtTime(this.Volume * masterVolume, 0, 0);
+            _stereoPannerNode.Pan.SetTargetAtTime(this.Pan, 0, 0);
+
+            InitStreamSourceAsync();
+        }
+
+        private async Task InitStreamSourceAsync()
+        {
+            AudioContext context = ConcreteAudioService.Context;
+
+            await context.AudioWorklet.AddModuleAsync("js/streamProcessor.js");
+            _streamSource = context.CreateWorklet("stream-processor");
+            _streamSource.Port.Message += StreamSource_OnMessage;
+            _streamSource.Connect(_sourceTarget);
+
+            _isStreamSourceInitialized = true;
+
+            // Submit any pending buffers that were added before the AudioWorklet was initialized.
+            while (_tmpBuffers.Count > 0)
+            {
+                byte[] tmpBuffer = _tmpBuffers.Dequeue();
+                _streamSource.Port.PostMessage(tmpBuffer, 0, tmpBuffer.Length);
+                _pendingBuffers++;
+            }
+        }
+
+        private void ReleaseMicrophoneDevice()
+        {
+            if (_streamSource != null)
+            {
+                _streamSource.Disconnect(_sourceTarget);
+                _streamSource.Dispose();
+                _streamSource = null;
+            }
+            _isStreamSourceInitialized = false;
+            _pendingBuffers = 0;
+            _tmpBuffers.Clear();
+
+
+        }
+
+        private void StreamSource_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.DataByteArray != null)
+            {
+                
+            }
+            else
+            {
+                var msg = e.DataFloat64;
+                
+                if (msg == 1) // buffer is proccessed
+                {
+                    _pendingBuffers--;
+                }
+            }
         }
 
         public override void PlatformResume(bool isLooped)
@@ -55,7 +137,7 @@ namespace Microsoft.Xna.Platform.Audio
 
         public override void PlatformStop()
         {
-            throw new NotImplementedException();
+            ReleaseMicrophoneDevice();
         }
 
         public override void PlatformRelease(bool isLooped)
@@ -65,14 +147,29 @@ namespace Microsoft.Xna.Platform.Audio
             throw new NotImplementedException();
         }
 
+        private readonly Queue<byte[]> _tmpBuffers = new Queue<byte[]>();
+
         public void DynamicPlatformSubmitBuffer(byte[] buffer, int offset, int count, SoundState state)
         {
-            throw new NotImplementedException();
+            if (_isStreamSourceInitialized == false)
+            {
+                // store pending buffers until the AudioWorklet is initialized
+                byte[] tmpBuffer = new byte[count];
+                Buffer.BlockCopy(buffer, offset, tmpBuffer, 0, count);
+                _tmpBuffers.Enqueue(tmpBuffer);
+            }
+            else
+            {
+                _streamSource.Port.PostMessage(buffer, offset, count);
+                _pendingBuffers++;
+            }
         }
 
         public void DynamicPlatformClearBuffers()
         {
-            throw new NotImplementedException();
+            _pendingBuffers = 0;
+            _tmpBuffers.Clear();
+            _streamSource.Port.PostMessage(2); // 2 = clear buffers
         }
 
         public void DynamicPlatformUpdateBuffers()
