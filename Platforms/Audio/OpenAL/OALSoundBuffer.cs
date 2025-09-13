9f45dedb5da0e7658d2972e8adb028a5772d7f16
Nikos Kastellanos <nkastellanos@gmail.com>
2025-09-14 00:29:48 +0300

    refactor Dispose()

--- Platforms/Audio/OpenAL/OALSoundBuffer.cs
+++ Platforms/Audio/OpenAL/OALSoundBuffer.cs
@@ -3,60 +3,13 @@
 // file 'LICENSE.txt', which is part of this source code package.
 
 using System;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Platform.Audio.OpenAL;
 
 namespace Microsoft.Xna.Platform.Audio
 {
-    sealed internal class ALSoundBuffer : IDisposable
+    sealed internal class ALSoundBuffer
     {
-        internal AudioService _audioService;
-
-        internal int _bufferId;
-        private bool _isBufferDisposed;
-
-        public ALSoundBuffer(AudioService audioService)
+        public ALSoundBuffer()
         {
-            _audioService = audioService;
-        }
-
-        ~ALSoundBuffer()
-        {
-            Dispose(false);
-        }
-
-        internal void _audioService_Disposing(object sender, EventArgs e)
-        {
-            Dispose();
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        private void Dispose(bool disposing)
-        {
-            if (!_isBufferDisposed)
-            {
-                if (disposing)
-                {
-                    // Clean up managed objects
-                }
-
-                ConcreteAudioService concreteAudioService = ((IPlatformAudioService)_audioService).Strategy.ToConcrete<ConcreteAudioService>();
-
-                // Release unmanaged resources
-                concreteAudioService.OpenAL.DeleteBuffer(_bufferId);
-                concreteAudioService.OpenAL.CheckError("Failed to delete buffer.");
-                _bufferId = 0;
-
-                _audioService.Disposing -= _audioService_Disposing;
-                _audioService = null;
-
-                _isBufferDisposed = true;
-            }
         }
     }
 }
