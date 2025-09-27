60691627da406af4a4114b3d66e0b0e911c49f20
Nikos Kastellanos <nkastellanos@gmail.com>
2025-09-27 11:40:36 +0300

    remove old code

--- src/Xna.Framework.Graphics/Graphics/ModelBone.cs
+++ src/Xna.Framework.Graphics/Graphics/ModelBone.cs
@@ -55,31 +55,4 @@ namespace Microsoft.Xna.Framework.Graphics
             Children = new ModelBoneCollection(_children);
         }
     }
-
-    //// Summary:
-    ////     Represents bone data for a model. Reference page contains links to related
-    ////     conceptual articles.
-    //public sealed class ModelBone
-    //{
-    //    // Summary:
-    //    //     Gets a collection of bones that are children of this bone.
-    //    public ModelBoneCollection Children { get { throw new NotImplementedException(); } }
-    //    //
-    //    // Summary:
-    //    //     Gets the index of this bone in the Bones collection.
-    //    public int Index { get { throw new NotImplementedException(); } }
-    //    //
-    //    // Summary:
-    //    //     Gets the name of this bone.
-    //    public string Name { get { throw new NotImplementedException(); } }
-    //    //
-    //    // Summary:
-    //    //     Gets the parent of this bone.
-    //    public ModelBone Parent { get { throw new NotImplementedException(); } }
-    //    //
-    //    // Summary:
-    //    //     Gets or sets the matrix used to transform this bone relative to its parent
-    //    //     bone.
-    //    public Matrix Transform { get { throw new NotImplementedException(); } set { throw new NotImplementedException(); } }
-    //}
 }
