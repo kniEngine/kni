ef701d31dc523bab3c9f588439d1a3baad9b50a9
Nikos Kastellanos <nkastellanos@gmail.com>
2025-09-27 11:42:34 +0300

    update comments

--- src/Xna.Framework.Graphics/Graphics/ModelBone.cs
+++ src/Xna.Framework.Graphics/Graphics/ModelBone.cs
@@ -4,35 +4,40 @@ using System.Collections.Generic;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
-    // Summary:
-    //     Represents bone data for a model. Reference page contains links to related
-    //     conceptual articles.
+    /// <summary>
+    /// Represents bone data for a model.
+    /// </summary>
     public sealed class ModelBone
     {
         private List<ModelBone> _children = new List<ModelBone>();
         
         private List<ModelMesh> _meshes = new List<ModelMesh>();
 
-        // Summary:
-        //     Gets a collection of bones that are children of this bone.
-        public ModelBoneCollection Children { get; private set; }
-        //
-        // Summary:
-        //     Gets the index of this bone in the Bones collection.
-        public int Index { get; internal set; }
-        //
-        // Summary:
-        //     Gets the name of this bone.
-        public string Name { get; internal set; }
-        //
-        // Summary:
-        //     Gets the parent of this bone.
-        public ModelBone Parent { get; internal set; }
-        //
-        // Summary:
-        //     Gets or sets the matrix used to transform this bone relative to its parent
-        //     bone.
         internal Matrix transform;
+
+        /// <summary>
+        /// Gets a collection of bones that are children of this bone.
+        /// </summary>
+        public ModelBoneCollection Children { get; private set; }
+
+        /// <summary>
+        /// Gets the index of this bone in the Bones collection.
+        /// </summary>
+        public int Index { get; internal set; }
+
+        /// <summary>
+        /// Gets the name of this bone.
+        /// </summary>
+        public string Name { get; internal set; }
+
+        /// <summary>
+        /// Gets the parent of this bone.
+        /// </summary>
+        public ModelBone Parent { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets the matrix used to transform this bone relative to its parent bone.
+        /// </summary>
         public Matrix Transform 
         { 
             get { return this.transform; } 
