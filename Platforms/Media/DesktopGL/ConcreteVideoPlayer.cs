3b2b2384fafcafce4cd70b475c6cdef24c5e669d
Nikos Kastellanos <nkastellanos@gmail.com>
2025-10-11 05:47:41 +0300

    mkv Decoder

--- Platforms/Media/DesktopGL/ConcreteVideoPlayer.cs
+++ Platforms/Media/DesktopGL/ConcreteVideoPlayer.cs
@@ -3,7 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Threading;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
@@ -14,9 +14,13 @@ namespace Microsoft.Xna.Platform.Media
 {
     internal sealed class ConcreteVideoPlayerStrategy : VideoPlayerStrategy
     {
+        private VideoDecoderProcess _decoderProcess;
         private Texture2D _lastFrame;
+        private TimeSpan _lastFrameTime;
 
-        private DynamicSoundEffectInstance _soundPlayer;
+        internal DynamicSoundEffectInstance _soundPlayer;
+
+        DecoderThread _decoderThread;
 
         public override MediaState State
         {
@@ -42,7 +46,7 @@ namespace Microsoft.Xna.Platform.Media
 
         public override TimeSpan PlayPosition
         {
-            get { throw new NotImplementedException(); }
+            get { return _decoderThread.Watch.Elapsed; }
         }
 
         public override float Volume
@@ -73,7 +77,21 @@ namespace Microsoft.Xna.Platform.Media
             if (_lastFrame == null)
                 _lastFrame = new Texture2D(((IPlatformVideo)base.Video).Strategy.GraphicsDevice, base.Video.Width, base.Video.Height, false, SurfaceFormat.Color);
 
-            throw new NotImplementedException();
+            if (_decoderProcess != null)
+            {
+                if (_decoderProcess.TryPeekVideoFrame(out VideoDecoderProcess.TrackData frameData))
+                {
+                    if (frameData.TrackTime < _decoderThread.Watch.Elapsed)
+                    {
+                        _lastFrameTime = frameData.TrackTime;
+                        _lastFrame.SetData(frameData.Data);
+                        _decoderProcess._videoFrameQueue.Dequeue();
+                        _decoderProcess._videoFramePool.Return(frameData.Data);
+                    }
+                }
+            }
+
+            return _lastFrame;
         }
 
         protected override void PlatformUpdateState(ref MediaState state)
@@ -85,7 +103,50 @@ namespace Microsoft.Xna.Platform.Media
         {
             base.Video = video;
 
-            throw new NotImplementedException();
+            // Cleanup the last video first.
+            if (State != MediaState.Stopped)
+            {
+                _decoderThread.Stop();
+                _decoderThread = null;
+                _decoderProcess.Dispose();
+                _decoderProcess = null;
+                _soundPlayer.Dispose();
+                _soundPlayer = null;
+            }
+
+            string ffmpegPath = "x64\\ffmpeg";
+            string inputFile = "" + ((IPlatformVideo)base.Video).Strategy.FileName;
+
+            _decoderProcess = new VideoDecoderProcess(base.Video, ffmpegPath, inputFile);
+
+            _decoderProcess.ParseMKV();
+
+            VideoDecoderProcess.MKVTrack atrack = _decoderProcess._tracks.Values.First((t) => (t.Type == VideoDecoderProcess.MKVTrackType.Audio));
+            VideoDecoderProcess.MKVAudioTrack audioTrack = (VideoDecoderProcess.MKVAudioTrack)atrack;
+            VideoDecoderProcess.MKVAudioSettings audioSettings = audioTrack.AudioSettings;
+
+            VideoDecoderProcess.MKVTrack vtrack = _decoderProcess._tracks.Values.First((t) => (t.Type == VideoDecoderProcess.MKVTrackType.Video));
+            VideoDecoderProcess.MKVVideoTrack videoTrack = (VideoDecoderProcess.MKVVideoTrack)vtrack;
+            VideoDecoderProcess.MKVVideoSettings videoSettings = videoTrack.VideoSettings;
+
+            int SampleRate = (int)audioSettings.SamplingFrequency;
+            AudioChannels channels = (AudioChannels)audioSettings.Channels;
+            _soundPlayer = new DynamicSoundEffectInstance(SampleRate, channels);
+            _soundPlayer.BufferNeeded += (s, e) =>
+            {
+                Debug.WriteLine("_soundPlayer.PendingBufferCount: " + _soundPlayer.PendingBufferCount);
+                Debug.WriteLine("_videoFrameQueue.Count: " + _decoderProcess._videoFrameQueue.Count);
+            };
+            int samples = (SampleRate * (int)channels) / 2;
+            PlatformSetVolume();
+            _soundPlayer.Play();
+
+            _lastFrameTime = TimeSpan.Zero;
+            this.State = MediaState.Playing;
+
+            _decoderThread = new DecoderThread(this, _decoderProcess);
+            _decoderThread.Stopped += OnDecoderThreadStopped;
+            _decoderThread.Start();
         }
 
         public override void PlatformPause()
@@ -100,30 +161,68 @@ namespace Microsoft.Xna.Platform.Media
 
         public override void PlatformStop()
         {
+            if (_decoderThread != null)
+            {
+                _decoderThread.Stop();
+                _decoderThread = null;
+            }
+            if (_decoderProcess != null)
+            {
+                _decoderProcess.Dispose();
+                _decoderProcess = null;
+            }
 
-            throw new NotImplementedException();
+            State = MediaState.Stopped;
+            _soundPlayer.Stop();
+            _soundPlayer.Dispose();
+            _soundPlayer = null;
+        }
+
+        private void OnDecoderThreadStopped(object sender, EventArgs e)
+        {
+            // TODO: event comes from another thread, so we need to sync base.State, and clean up. 
+            base.State = MediaState.Stopped;
+
+            _decoderThread.Stop();
+            _decoderThread = null;
+            _decoderProcess.Dispose();
+            _decoderProcess = null;
+            _soundPlayer.Dispose();
+            _soundPlayer = null;
         }
 
         private void PlatformSetVolume()
         {
-           float volume = base.Volume;
-           if (IsMuted)
+            float volume = base.Volume;
+            if (IsMuted)
                 volume = 0.0f;
 
-           if (_soundPlayer != null)
-              _soundPlayer.Volume = volume;
+            if (_soundPlayer != null)
+                _soundPlayer.Volume = volume;
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
+                if (_decoderThread != null)
+                {
+                    _decoderThread.Stop();
+                    _decoderThread = null;
+                }
+                if (_decoderProcess != null)
+                {
+                    _decoderProcess.Dispose();
+                    _decoderProcess = null;
+                }
                 if (_soundPlayer != null)
                 {
                     _soundPlayer.Dispose();
                     _soundPlayer = null;
                 }
+
             }
+
 
             base.Dispose(disposing);
         }
---
+++ Platforms/Media/DesktopGL/VideoDecoder/FrameBufferPool.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (C)2024 Nick Kastellanos
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Xna.Platform.Media
+{
+    public class FrameBufferPool : IComparer<byte[]>
+    {
+        private const int MinimumBufferSize = 0;
+
+        private static readonly FrameBufferPool _current = new FrameBufferPool();
+
+        private SortedSet<byte[]> _bufferSet;
+
+        public FrameBufferPool()
+        {
+            _bufferSet = new SortedSet<byte[]>(this);
+        }
+
+        public byte[] Get(int size)
+        {
+            lock (_bufferSet)
+            {
+                foreach (byte[] buffer in _bufferSet)
+                {
+                    if (buffer.Length == size)
+                    {
+                        _bufferSet.Remove(buffer);
+                        return buffer;
+                    }
+                }
+
+                if (_bufferSet.Count >= 1)
+                    _bufferSet.Remove(_bufferSet.Max);
+
+                int dataSize = Math.Max(MinimumBufferSize, size);
+
+#if NET8_0_OR_GREATER
+                return GC.AllocateUninitializedArray<byte>(dataSize);
+#else
+                return new byte[dataSize];
+#endif
+            }
+        }
+
+        public void Return(byte[] buffer)
+        {
+            lock (_bufferSet)
+            {
+                _bufferSet.Add(buffer);
+            }
+        }
+
+        int IComparer<byte[]>.Compare(byte[] x, byte[] y)
+        {
+            return (x.Length - y.Length);
+        }
+    }
+}
