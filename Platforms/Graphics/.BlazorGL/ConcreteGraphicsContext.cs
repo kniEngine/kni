kniEngine/kni

commit 24bdec3df93025e1ede13548d12ba89a568ff9b7

fix GL Clear(...) (#2354)
When Scissor is enabled in RasterizerState,
clear() was affecting only the Viewport.

This fix implements the correct XNA behavior.
Clear will set the entire Backbuffer/RenderTarget and ignores the
Viewport & Scissor.

Platforms/Graphics/.BlazorGL/ConcreteGraphicsContext.cs
@@ -108,16 +108,21 @@ namespace Microsoft.Xna.Platform.Graphics
             // So overwrite these states with what is needed to perform
             // the clear correctly and restore it afterwards.
             //
-            Rectangle prevScissorRect = this.ScissorRectangle;
             DepthStencilState prevDepthStencilState = this.DepthStencilState;
             BlendState prevBlendState = this.BlendState;
-            this.ScissorRectangle = _viewport.Bounds;
             // DepthStencilState.Default has the Stencil Test disabled; 
             // make sure stencil test is enabled before we clear since
             // some drivers won't clear with stencil test disabled
             this.DepthStencilState = _clearDepthStencilState;
             this.BlendState = BlendState.Opaque;
             PlatformApplyState();
+
+            if (_lastRasterizerState.ScissorTestEnable)
+            {
+                GL.Disable(WebGLCapability.SCISSOR_TEST);
+                GL.CheckGLError();
+                _lastRasterizerState.ScissorTestEnable = false;
+            }
 
             WebGLBufferBits bb = default(WebGLBufferBits);
             if ((options & ClearOptions.Target) != 0)
@@ -145,7 +150,6 @@ namespace Microsoft.Xna.Platform.Graphics
             base.Metrics_AddClearCount();
 
             // Restore the previous render state.
-            ScissorRectangle = prevScissorRect;
             DepthStencilState = prevDepthStencilState;
             BlendState = prevBlendState;
         }

Platforms/Graphics/.GL/ConcreteGraphicsContext.cs
@@ -158,16 +158,21 @@ namespace Microsoft.Xna.Platform.Graphics
             // So overwrite these states with what is needed to perform
             // the clear correctly and restore it afterwards.
             //
-            Rectangle prevScissorRect = this.ScissorRectangle;
             DepthStencilState prevDepthStencilState = this.DepthStencilState;
             BlendState prevBlendState = this.BlendState;
-            this.ScissorRectangle = _viewport.Bounds;
             // DepthStencilState.Default has the Stencil Test disabled; 
             // make sure stencil test is enabled before we clear since
             // some drivers won't clear with stencil test disabled
             this.DepthStencilState = _clearDepthStencilState;
             this.BlendState = BlendState.Opaque;
             PlatformApplyState();
+
+            if (_lastRasterizerState.ScissorTestEnable)
+            {
+                GL.Disable(EnableCap.ScissorTest);
+                GL.CheckGLError();
+                _lastRasterizerState.ScissorTestEnable = false;
+            }
 
             ClearBufferMask bufferMask = 0;
             if ((options & ClearOptions.Target) == ClearOptions.Target)
@@ -215,7 +220,6 @@ namespace Microsoft.Xna.Platform.Graphics
             base.Metrics_AddClearCount();
 
             // Restore the previous render state.
-            ScissorRectangle = prevScissorRect;
             DepthStencilState = prevDepthStencilState;
             BlendState = prevBlendState;
         }

commit 16ddcece00e7ca6e60a4843de1be999c42e823b6

fix GL ScissorRectangle
fixes #2549

bug introduced in #2354

Platforms/Graphics/.BlazorGL/ConcreteGraphicsContext.cs
@@ -122,6 +122,7 @@ namespace Microsoft.Xna.Platform.Graphics
                 GL.Disable(WebGLCapability.SCISSOR_TEST);
                 GL.CheckGLError();
                 _lastRasterizerState.ScissorTestEnable = false;
+                _rasterizerStateDirty = true;
             }
 
             WebGLBufferBits bb = default(WebGLBufferBits);

Platforms/Graphics/.GL/ConcreteGraphicsContext.cs
@@ -172,6 +172,7 @@ namespace Microsoft.Xna.Platform.Graphics
                 GL.Disable(EnableCap.ScissorTest);
                 GL.CheckGLError();
                 _lastRasterizerState.ScissorTestEnable = false;
+                _rasterizerStateDirty = true;
             }
 
             ClearBufferMask bufferMask = 0;