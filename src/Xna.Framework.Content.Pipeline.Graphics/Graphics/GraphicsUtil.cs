48267b097a0be26943bd833a51cfdd81744d282f
Nikos Kastellanos <nkastellanos@gmail.com>
2025-12-30 20:18:17 +0200

    remove obvious comments

--- src/Xna.Framework.Content.Pipeline.Graphics/Graphics/GraphicsUtil.cs
+++ src/Xna.Framework.Content.Pipeline.Graphics/Graphics/GraphicsUtil.cs
@@ -565,7 +565,6 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
 
         internal static bool RequiresPowerOfTwo(TextureProcessorOutputFormat format, TargetPlatform targetPlatform, GraphicsProfile targetProfile)
         {
-            // Does it require POT textures?
             switch (format)
             {
                 case TextureProcessorOutputFormat.DxtCompressed:
@@ -583,7 +582,6 @@ namespace Microsoft.Xna.Framework.Content.Pipeline.Graphics
 
         internal static bool RequiresSquare(TextureProcessorOutputFormat format, TargetPlatform targetPlatform)
         {
-            // Does it require square textures?
             switch (format)
             {
                 case TextureProcessorOutputFormat.PvrCompressed:
