052f1b0ea7eaa7782e70af3039eeb3e6ed066d0c
Nikos Kastellanos <nkastellanos@gmail.com>
2026-01-06 11:15:27 +0200

    order folders first in the Editor

---
+++ Tools/Content.Pipeline.Editor.WinForms/Forms/TreeViewNodeSorter.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (C)2026 Nick Kastellanos
+
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Content.Pipeline.Editor
+{
+    internal class TreeViewNodeSorter : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            TreeNode a = (TreeNode)x;
+            TreeNode b = (TreeNode)y;
+
+            if (a.Tag != null && b.Tag != null)
+            {
+                if ((a.Tag is FolderItem) && !(b.Tag is FolderItem))
+                    return -1;
+                if (!(a.Tag is FolderItem) && (b.Tag is FolderItem))
+                    return +1;
+            }
+
+            return a.Text.CompareTo(b.Text);
+        }
+    }
+}
