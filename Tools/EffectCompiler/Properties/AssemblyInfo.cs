6b2b222ad9e00a3de198b24d6a2075ed3611170b
Nikos Kastellanos <nkastellanos@gmail.com>
2026-06-11 19:56:18 +0300

    remove CLSCompliant from tools

--- Tools/EffectCompiler/Properties/AssemblyInfo.cs
+++ Tools/EffectCompiler/Properties/AssemblyInfo.cs
@@ -15,9 +15,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Mark the assembly as CLS compliant so it can be safely used in other .NET languages
-[assembly: CLSCompliant(true)]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
