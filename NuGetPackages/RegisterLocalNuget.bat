87e6c7710e87d53bd59b6bcdd08d01386071261e
Nikos Kastellanos <nkastellanos@gmail.com>
2026-07-13 17:42:14 +0300

    fix RegisterLocalNuget paths (#2677)

--- NuGetPackages/RegisterLocalNuget.bat
+++ NuGetPackages/RegisterLocalNuget.bat
@@ -30,34 +30,34 @@ set username=username
 "C:\Program Files (x86)\nuget\nuget.exe" delete nkast.Kni.Platform.WinForms.DX11.OculusOVR    4.2.9001 -Source "C:\Users\%username%\.nuget\localPackages" -NonInteractive
 
 
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.4.2.9001.nupkg                           -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Design.4.2.9001.nupkg                    -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.4.2.9001.nupkg                   -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Audio.4.2.9001.nupkg                     -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Graphics.4.2.9001.nupkg                  -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Media.4.2.9001.nupkg                     -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Input.4.2.9001.nupkg                     -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Game.4.2.9001.nupkg                      -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Devices.4.2.9001.nupkg                   -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Storage.4.2.9001.nupkg                   -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.XR.4.2.9001.nupkg                        -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.4.2.9001.nupkg            -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Design.4.2.9001.nupkg                    -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Content.4.2.9001.nupkg                   -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Audio.4.2.9001.nupkg                     -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Graphics.4.2.9001.nupkg                  -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Media.4.2.9001.nupkg                     -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Input.4.2.9001.nupkg                     -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Game.4.2.9001.nupkg                      -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Devices.4.2.9001.nupkg                   -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Storage.4.2.9001.nupkg                   -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.XR.4.2.9001.nupkg                        -Source "C:\Users\%username%\.nuget\localPackages"
 
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.4.2.9001.nupkg          -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Audio.4.2.9001.nupkg    -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Graphics.4.2.9001.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Media.4.2.9001.nupkg    -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Content.Pipeline.4.2.9001.nupkg          -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Content.Pipeline.Audio.4.2.9001.nupkg    -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Content.Pipeline.Graphics.4.2.9001.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Content.Pipeline.Media.4.2.9001.nupkg    -Source "C:\Users\%username%\.nuget\localPackages"
 
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Builder.4.2.9001.nupkg  -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Xna.Framework.Content.Pipeline.Builder.Windows.4.2.9001.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Content.Pipeline.Builder.4.2.9001.nupkg  -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Xna.Framework.Content.Pipeline.Builder.Windows.4.2.9001.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
 
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Kni.Platform.Blazor.GL.4.2.9001.nupkg             -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Kni.Platform.WinForms.DX11.4.2.9001.nupkg         -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Kni.Platform.UAP.DX11.4.2.9001.nupkg              -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Kni.Platform.SDL2.GL.4.2.9001.nupkg               -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Kni.Platform.Android.GL.4.2.9001.nupkg            -Source "C:\Users\%username%\.nuget\localPackages"
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Kni.Platform.Oculus.GL.4.2.9001.nupkg             -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Kni.Platform.Blazor.GL.4.2.9001.nupkg             -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Kni.Platform.WinForms.DX11.4.2.9001.nupkg         -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Kni.Platform.UAP.DX11.4.2.9001.nupkg              -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Kni.Platform.SDL2.GL.4.2.9001.nupkg               -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Kni.Platform.Android.GL.4.2.9001.nupkg            -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Kni.Platform.Oculus.GL.4.2.9001.nupkg             -Source "C:\Users\%username%\.nuget\localPackages"
 
-"C:\Program Files (x86)\nuget\nuget.exe" add output\nkast.Kni.Platform.WinForms.DX11.OculusOVR.4.2.9001.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add ..\Artifacts\Packages\nkast.Kni.Platform.WinForms.DX11.OculusOVR.4.2.9001.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
 
 
 @pause
