59fdead4a7bffb917a1c7aefc6046b898cc04a18
squarebananas <37810292+squarebananas@users.noreply.github.com>
2026-07-25 20:30:12 +0100

    Texture3D compressed format support for DX11 (#2642)
    * Texture3D.SetData compressed format support for DX11
    
    * Texture3D.GetData compressed format support for DX11

--- Platforms/Graphics/.DX11/ConcreteTexture3D.cs
+++ Platforms/Graphics/.DX11/ConcreteTexture3D.cs
@@ -58,7 +58,7 @@ namespace Microsoft.Xna.Platform.Graphics
         public int Depth { get { return _depth; } }
 
         public unsafe void SetData<T>(int level, int left, int top, int right, int bottom, int front, int back,
-                               T[] data, int startIndex, int elementCount)
+                                      T[] data, int startIndex, int elementCount)
             where T : struct
         {
             int width = right - left;
@@ -72,7 +72,8 @@ namespace Microsoft.Xna.Platform.Graphics
                 dataPtr = dataPtr + startIndex * elementSizeInByte;
 
                 int rowPitch = this.Format.GetPitch(width);
-                int slicePitch = rowPitch * height; // For 3D texture: Size of 2D image.
+                int rowCount = this.Format.IsCompressedFormat() ? ((height + 3) / 4) : height;
+                int slicePitch = rowPitch * rowCount; // For 3D texture: Size of 2D image.
                 DX.DataBox dataBox = new DX.DataBox(dataPtr, rowPitch, slicePitch);
 
                 int subresourceIndex = level;
@@ -88,16 +89,21 @@ namespace Microsoft.Xna.Platform.Graphics
             }
         }
 
-        public void GetData<T>(int level, int left, int top, int right, int bottom, int front, int back,
-                               T[] data, int startIndex, int elementCount)
+        public unsafe void GetData<T>(int level, int left, int top, int right, int bottom, int front, int back,
+                                      T[] data, int startIndex, int elementCount)
              where T : struct
         {
             // Create a temp staging resource for copying the data.
             //
+            int min = this.Format.IsCompressedFormat() ? 4 : 1;
+            int levelWidth = Math.Max(this.Width >> level, min);
+            int levelHeight = Math.Max(this.Height >> level, min);
+            int levelDepth = Math.Max(this.Depth >> level, min);
+
             D3D11.Texture3DDescription texture3DDesc = new D3D11.Texture3DDescription();
-            texture3DDesc.Width = this.Width;
-            texture3DDesc.Height = this.Height;
-            texture3DDesc.Depth = this.Depth;
+            texture3DDesc.Width = levelWidth;
+            texture3DDesc.Height = levelHeight;
+            texture3DDesc.Depth = levelDepth;
             texture3DDesc.MipLevels = 1;
             texture3DDesc.Format = this.Format.ToDXFormat();
             texture3DDesc.BindFlags = D3D11.BindFlags.None;
@@ -119,21 +125,70 @@ namespace Microsoft.Xna.Platform.Graphics
                     DX.DataBox dataBox = d3dContext.MapSubresource(stagingTexture, 0, D3D11.MapMode.Read, D3D11.MapFlags.None, out stream);
                     try
                     {
-                        // Some drivers may add pitch to rows or slices.
-                        // We need to copy each row separatly and skip trailing zeros.
-                        int currentIndex = startIndex;
                         int elementSize = this.Format.GetSize();
                         int elementsInRow = right - left;
                         int rowsInSlice = bottom - top;
-                        for (int slice = front; slice < back; slice++)
+                        int slices = back - front;
+                        if (this.Format.IsCompressedFormat())
                         {
-                            for (int row = top; row < bottom; row++)
+                            // for 4x4 block compression formats an element is one block, so elementsInRow
+                            // and number of rows are 1/4 of number of pixels in width and height
+                            elementsInRow /= 4;
+                            rowsInSlice /= 4;
+                        }
+                        int rowSize = elementSize * elementsInRow;
+                        int sliceSize = rowSize * rowsInSlice;
+                        if ((rowSize == dataBox.RowPitch) && (sliceSize == dataBox.SlicePitch))
+                            stream.ReadRange(data, startIndex, elementCount);
+                        else if (left == 0 && top == 0 && front == 0 &&
+                                 right == levelWidth && bottom == levelHeight && back == levelDepth &&
+                                 startIndex == 0 && elementCount == data.Length)
+                        {
+                            // Optimized PlatformGetData() that reads multiple elements in a row when texture has rowPitch
+                            int elementSize2 = sizeof(T);
+                            if (elementSize2 == 1) // byte[]
+                                elementsInRow = elementsInRow * elementSize;
+
+                            int currentIndex = 0;
+                            for (int slice = front; slice < back; slice++)
                             {
-                                stream.ReadRange(data, currentIndex, elementsInRow);
-                                stream.Seek(dataBox.RowPitch - (elementSize * elementsInRow), SeekOrigin.Current);
-                                currentIndex += elementsInRow;
+                                for (int row = 0; row < rowsInSlice; row++)
+                                {
+                                    stream.ReadRange(data, currentIndex, elementsInRow);
+                                    stream.Seek(dataBox.RowPitch - rowSize, SeekOrigin.Current);
+                                    currentIndex += elementsInRow;
+                                }
+                                stream.Seek(dataBox.SlicePitch - (dataBox.RowPitch * rowsInSlice), SeekOrigin.Current);
                             }
-                            stream.Seek(dataBox.SlicePitch - (dataBox.RowPitch * rowsInSlice), SeekOrigin.Current);
+                        }
+                        else
+                        {
+                            // Some drivers may add pitch to rows or slices.
+                            // We need to copy each row separately and skip trailing zeros.
+                            stream.Seek(0, SeekOrigin.Begin);
+
+                            int elementSizeInByte = sizeof(T);
+                            for (int slice = 0; slice < slices; slice++)
+                            {
+                                int dataSliceOffset = slice * sliceSize / elementSizeInByte;
+                                for (int row = 0; row < rowsInSlice; row++)
+                                {
+                                    int i;
+                                    int maxElements = (row + 1) * rowSize / elementSizeInByte;
+                                    for (i = row * rowSize / elementSizeInByte; i < maxElements; i++)
+                                        data[i + dataSliceOffset + startIndex] = stream.Read<T>();
+
+                                    if ((i + dataSliceOffset) >= elementCount)
+                                        break;
+
+                                    stream.Seek(dataBox.RowPitch - rowSize, SeekOrigin.Current);
+                                }
+
+                                if ((dataSliceOffset + (rowsInSlice * elementsInRow)) >= elementCount)
+                                    break;
+
+                                stream.Seek(dataBox.SlicePitch - (dataBox.RowPitch * rowsInSlice), SeekOrigin.Current);
+                            }
                         }
                     }
                     finally
@@ -144,7 +199,29 @@ namespace Microsoft.Xna.Platform.Graphics
                     }
                 }
             }
+        }
 
+        public int GetCompressedDataByteSize(int fSize, int left, int top, int right, int bottom, int front, int back,
+                                             int textureBoundsWidth, int textureBoundsHeight, int textureBoundsDepth,
+                                             out int checkedLeft, out int checkedTop, out int checkedRight, out int checkedBottom,
+                                             out int checkedFront, out int checkedBack)
+        {
+            int width = right - left;
+            int height = bottom - top;
+            int depth = back - front;
+            Format.GetBlockSize(out int blockWidth, out int blockHeight);
+            int blockWidthMinusOne = blockWidth - 1;
+            int blockHeightMinusOne = blockHeight - 1;
+            // round x and y down to next multiple of block size; width and height up to next multiple of block size
+            int roundedWidth = (width + blockWidthMinusOne) & ~blockWidthMinusOne;
+            int roundedHeight = (height + blockHeightMinusOne) & ~blockHeightMinusOne;
+            checkedLeft = left & ~blockWidthMinusOne;
+            checkedTop = top & ~blockHeightMinusOne;
+            checkedRight = checkedLeft + roundedWidth;
+            checkedBottom = checkedTop + roundedHeight;
+            checkedFront = front;
+            checkedBack = back;
+            return roundedWidth * roundedHeight * fSize / (blockWidth * blockHeight) * depth;
         }
 
         public int GetCompressedDataByteSize(int fSize, int left, int top, int right, int bottom, int front, int back,
