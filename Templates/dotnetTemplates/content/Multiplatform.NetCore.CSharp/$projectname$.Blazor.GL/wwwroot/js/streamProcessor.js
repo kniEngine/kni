74297b1717a8d246b290b6d8cc8cfdb1fbaf092a
Nikos Kastellanos <nkastellanos@gmail.com>
2026-07-28 10:01:23 +0300

    sync streamProcessor.js in templates
    related to #2615

--- Templates/dotnetTemplates/content/Multiplatform.NetCore.CSharp/$projectname$.Blazor.GL/wwwroot/js/streamProcessor.js
+++ Templates/dotnetTemplates/content/Multiplatform.NetCore.CSharp/$projectname$.Blazor.GL/wwwroot/js/streamProcessor.js
@@ -5,6 +5,12 @@ class StreamProcessor extends AudioWorkletProcessor
     {
         super();
         this.queue = [];
+        this.samplePosition = 0;
+        this.paused = false;
+        this.pitchPlaybackRate = 1;
+        this.formatSampleRate = 44100;
+        this.formatChannels = 1;
+        this.receivingValueForMessageType = -1;
 
         this.port.onmessage = (event) =>
         {
@@ -12,15 +18,40 @@ class StreamProcessor extends AudioWorkletProcessor
 
             if (typeof data === 'number')
             {
-                if (data === 2)
-                {                    
-                    this.queue = [];
+                if (this.receivingValueForMessageType === -1)
+                {
+                    switch (data)
+                    {
+                        case 2:
+                            this.queue = []; break;
+                        case 3:
+                            this.paused = true; break;
+                        case 4:
+                            this.paused = false; break;
+                        case 5:
+                        case 6:
+                        case 7:
+                            this.receivingValueForMessageType = data;
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (this.receivingValueForMessageType)
+                    {
+                        case 5:
+                            this.pitchPlaybackRate = data; break;
+                        case 6:
+                            this.formatSampleRate = data; break;
+                        case 7:
+                            this.formatChannels = data; break;
+                    }
+                    this.receivingValueForMessageType = -1;
                 }
             }
             if (data instanceof Uint8Array)
             {
                 const buffer = new Int16Array(data.buffer, data.byteOffset, data.length / 2);
-                buffer.offset = 0;
                 this.queue.push(buffer);
             }
         };
@@ -29,53 +60,61 @@ class StreamProcessor extends AudioWorkletProcessor
 
     process(inputs, outputs, parameters)
     {
+        const inputChannelCount = this.formatChannels;
+        const formatPlaybackRate = this.formatSampleRate / sampleRate;
+        const playbackRate = formatPlaybackRate * this.pitchPlaybackRate;
+
         const output = outputs[0];
-        
-        const channelCount = output.length;
-        const sampleCount = output[0].length;
+        const outputChannelCount = output.length;
+        const outputSampleCount = output[0].length;
 
         let written = 0;
 
-        while (written < sampleCount && this.queue.length > 0)
+        while (written < outputSampleCount && this.queue.length > 0 && !this.paused)
         {
             const buffer = this.queue[0];
-            const offset = buffer.offset;
+            const samplesInBuffer = buffer.length / inputChannelCount;
+            let nextSamplePosition = this.samplePosition;
 
-            const available = buffer.length - offset;
-            const needed = sampleCount - written;
-            const copyCount = Math.min(available, needed);
-
-            for (let i = 0; i < copyCount; i++)
+            while (written < outputSampleCount && nextSamplePosition < samplesInBuffer)
             {
-                for (let c = 0; c < channelCount; c++)
+                let offset = Math.floor(nextSamplePosition) * inputChannelCount;
+                for (let outputChannel = 0; outputChannel < outputChannelCount; outputChannel++)
                 {
-                    const channel = output[c];
-                    let value = (buffer[offset+i] / 32767);
-                    channel[written+i] = value;
+                    let inputChannel = outputChannel % inputChannelCount;
+                    let value1 = buffer[offset + inputChannel];
+                    let value2;
+                    if (nextSamplePosition < samplesInBuffer - 1)
+                        value2 = buffer[offset + inputChannelCount + inputChannel];
+                    else if (this.queue.length > 1)
+                        value2 = this.queue[1][inputChannel];
+                    else
+                        value2 = value1;
+                    let value = (value1 + ((value2 - value1) * (nextSamplePosition % 1))) / 32768;
+                    output[outputChannel][written] = value;
                 }
+                written++;
+                nextSamplePosition += playbackRate;
             }
-            
-            written += copyCount;
-            buffer.offset += copyCount;
 
-            if (buffer.offset >= buffer.length)
+            if (nextSamplePosition >= samplesInBuffer)
             {
+                nextSamplePosition -= samplesInBuffer;
                 this.queue.shift();
                 this.port.postMessage(1);
             }
+
+            this.samplePosition = nextSamplePosition;
         }
         
         // Fill remaining samples with silence
-        if (written < sampleCount)
+        if (written < outputSampleCount)
         {
-            for (let c = 0; c < channelCount; c++)
+            for (let outputChannel = 0; outputChannel < outputChannelCount; outputChannel++)
             {
-                const channel = output[c];
-
-                for (let i = written; i < sampleCount; i++)
+                for (let i = written; i < outputSampleCount; i++)
                 {
-                    let value = 0;
-                    channel[i] = value;
+                    output[outputChannel][i] = 0;
                 }
             }
         }
