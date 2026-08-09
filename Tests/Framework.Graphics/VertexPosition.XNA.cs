611d195772ad703633a3e6fdef9c777db7ba4983
Nikos Kastellanos <nkastellanos@gmail.com>
2026-08-09 11:37:17 +0300

    Update xn atests (#2696)
    * VertexPosition struct
    
    * build XNA effects

---
+++ Tests/Framework.Graphics/VertexPosition.XNA.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Microsoft.Xna.Framework.Graphics
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct VertexPosition : IVertexType
+    {
+        public Vector3 Position;
+
+        public static readonly VertexDeclaration VertexDeclaration;
+
+        public VertexPosition(Vector3 position)
+        {
+            Position = position;
+        }
+
+        VertexDeclaration IVertexType.VertexDeclaration
+        {
+            get { return VertexDeclaration; }
+        }
+
+        static VertexPosition()
+        {
+            VertexElement[] elements = { new VertexElement(0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0) };
+            VertexDeclaration = new VertexDeclaration(elements);
+        }
+    }
+}
