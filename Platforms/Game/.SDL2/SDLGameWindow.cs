a72079a9903ef548127c38579d2cff8913263231
Nikos Kastellanos <nkastellanos@gmail.com>
2026-08-23 21:54:06 +0300

    refactor EndScreenDeviceChange (#2707)
    * rename _isExclusiveFullScreen
    
    * pass willBeExclusiveFullScreen from ConcreteGraphicsDeviceManager

--- Platforms/Game/.SDL2/SDLGameWindow.cs
+++ Platforms/Game/.SDL2/SDLGameWindow.cs
@@ -107,7 +107,7 @@ namespace Microsoft.Xna.Framework
 
         private bool _disposed;
         private bool _isResizable, _isBorderless;
-        private bool _isFullScreen, _hardwareSwitch;
+        private bool _isFullScreen, _isExclusiveFullScreen;
         private bool _mouseVisible;
         private string _screenDeviceName;
         private int _width, _height;
@@ -513,7 +513,7 @@ namespace Microsoft.Xna.Framework
             SDL.MOUSE.ShowCursor(visible ? 1 : 0);
         }
 
-        internal void EndScreenDeviceChange(string screenDeviceName, int clientWidth, int clientHeight, bool willBeFullScreen)
+        internal void EndScreenDeviceChange(string screenDeviceName, int clientWidth, int clientHeight, bool willBeFullScreen, bool willBeExclusiveFullScreen)
         {
             _screenDeviceName = screenDeviceName;
 
@@ -522,24 +522,22 @@ namespace Microsoft.Xna.Framework
             int displayIndex = SDL.WINDOW.GetDisplayIndex(Handle);
             SDL.DISPLAY.GetBounds(displayIndex, out Sdl.Rectangle displayRect);
 
-            GraphicsDeviceManager gdm = ((IPlatformGame)_game).GetStrategy<GameStrategy>().GraphicsDeviceManager;
-
             if (willBeFullScreen == true)
             {
-                if (!_isFullScreen || _hardwareSwitch != gdm.HardwareModeSwitch)
+                if (!_isFullScreen || _isExclusiveFullScreen != willBeExclusiveFullScreen)
                 {
-                    Sdl.Window.State fullscreenFlag = gdm.HardwareModeSwitch 
-                                                    ? Sdl.Window.State.Fullscreen 
+                    Sdl.Window.State fullscreenFlag = willBeExclusiveFullScreen
+                                                    ? Sdl.Window.State.Fullscreen
                                                     : Sdl.Window.State.FullscreenDesktop;
                     SDL.WINDOW.SetFullscreen(Handle, fullscreenFlag);
-                    _hardwareSwitch = gdm.HardwareModeSwitch;
+                    _isExclusiveFullScreen = willBeExclusiveFullScreen;
                 }
 
                 // If going to exclusive full-screen mode, force the window to minimize on focus loss (Windows only)
                 if (CurrentPlatform.OS == OS.Windows)
-                    SDL.SetHint("SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS", _hardwareSwitch ? "1" : "0");
+                    SDL.SetHint("SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS", _isExclusiveFullScreen ? "1" : "0");
 
-                if (!gdm.HardwareModeSwitch)
+                if (!willBeExclusiveFullScreen)
                 {
                     _width = displayRect.Width;
                     _height = displayRect.Height;
@@ -568,10 +566,10 @@ namespace Microsoft.Xna.Framework
             }
             else // (willBeFullScreen == false)
             {
-                if (_isFullScreen || _hardwareSwitch != gdm.HardwareModeSwitch)
+                if (_isFullScreen || _isExclusiveFullScreen != willBeExclusiveFullScreen)
                 {
                     SDL.WINDOW.SetFullscreen(Handle, (Sdl.Window.State)0);
-                    _hardwareSwitch = gdm.HardwareModeSwitch;
+                    _isExclusiveFullScreen = willBeExclusiveFullScreen;
                 }
 
                 // If going to exclusive full-screen mode, force the window to minimize on focus loss (Windows only)
@@ -616,7 +614,7 @@ namespace Microsoft.Xna.Framework
             _supressMoved = true;
         }
 
-        internal void EndCreateDevice(string screenDeviceName, int clientWidth, int clientHeight, bool willBeFullScreen)
+        internal void EndCreateDevice(string screenDeviceName, int clientWidth, int clientHeight, bool willBeFullScreen, bool willBeExclusiveFullScreen)
         {
             _screenDeviceName = screenDeviceName;
 
@@ -625,24 +623,22 @@ namespace Microsoft.Xna.Framework
             int displayIndex = SDL.WINDOW.GetDisplayIndex(Handle);
             SDL.DISPLAY.GetBounds(displayIndex, out Sdl.Rectangle displayRect);
 
-            GraphicsDeviceManager gdm = ((IPlatformGame)_game).GetStrategy<GameStrategy>().GraphicsDeviceManager;
-
             if (willBeFullScreen == true)
             {
-                if (!_isFullScreen || _hardwareSwitch != gdm.HardwareModeSwitch)
+                if (!_isFullScreen || _isExclusiveFullScreen != willBeExclusiveFullScreen)
                 {
-                    Sdl.Window.State fullscreenFlag = gdm.HardwareModeSwitch
+                    Sdl.Window.State fullscreenFlag = willBeExclusiveFullScreen
                                                     ? Sdl.Window.State.Fullscreen
                                                     : Sdl.Window.State.FullscreenDesktop;
                     SDL.WINDOW.SetFullscreen(Handle, fullscreenFlag);
-                    _hardwareSwitch = gdm.HardwareModeSwitch;
+                    _isExclusiveFullScreen = willBeExclusiveFullScreen;
                 }
 
                 // If going to exclusive full-screen mode, force the window to minimize on focus loss (Windows only)
                 if (CurrentPlatform.OS == OS.Windows)
-                    SDL.SetHint("SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS", _hardwareSwitch ? "1" : "0");
+                    SDL.SetHint("SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS", _isExclusiveFullScreen ? "1" : "0");
 
-                if (!gdm.HardwareModeSwitch)
+                if (!willBeExclusiveFullScreen)
                 {
                     _width = displayRect.Width;
                     _height = displayRect.Height;
@@ -671,10 +667,10 @@ namespace Microsoft.Xna.Framework
             }
             else // (willBeFullScreen == false)
             {
-                if (_isFullScreen || _hardwareSwitch != gdm.HardwareModeSwitch)
+                if (_isFullScreen || _isExclusiveFullScreen != willBeExclusiveFullScreen)
                 {
                     SDL.WINDOW.SetFullscreen(Handle, (Sdl.Window.State)0);
-                    _hardwareSwitch = gdm.HardwareModeSwitch;
+                    _isExclusiveFullScreen = willBeExclusiveFullScreen;
                 }
 
                 // If going to exclusive full-screen mode, force the window to minimize on focus loss (Windows only)
